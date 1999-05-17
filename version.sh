74f1ef06d302e2f5b0c048e3249bd6f3fc7e5922
tomo <tomo>
1999-05-17 09:41:39 +0000

    XEmacs 21.2.6

--- version.sh
+++ version.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 emacs_major_version=21
 emacs_minor_version=2
-emacs_beta_version=5
-xemacs_codename="Aphrodite"
+emacs_beta_version=6
+xemacs_codename="Apollo"
 infodock_major_version=4
 infodock_minor_version=0
 infodock_build_version=1
