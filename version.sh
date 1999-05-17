6933777238ca175987e62b3495df987ecccc1168
tomo <tomo>
1999-05-17 09:41:59 +0000

    This commit was manufactured by cvs2svn to create branch 'chise-r21-4-19'.

--- version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-emacs_major_version=21
-emacs_minor_version=2
-emacs_beta_version=11
-xemacs_codename="Calliope"
-infodock_major_version=4
-infodock_minor_version=0
-infodock_build_version=1
