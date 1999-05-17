533234d88bd92bfe340ff6b55b16574bf5428fa8
tomo <tomo>
1999-05-17 09:41:58 +0000

    This commit was manufactured by cvs2svn to create branch 'chise-r21-4-18'.

--- src/gpmevent.h
+++
@@ -1,7 +0,0 @@
-#ifndef _HAVE_GPM
-#define _HAVE_GPM
-
-int handle_gpm_read (struct Lisp_Event *event, struct console *con, int fd);
-void connect_to_gpm (struct console *con);
-
-#endif
--- src/s/irix6-0.h
+++
@@ -1,6 +0,0 @@
-/* Synched up with: FSF 19.31. */
-
-#include "irix5-3.h"
-
-/* Irix 6.2 doesn't need -lw */
-#undef NEED_LIBW
