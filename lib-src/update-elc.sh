be1aa469293700c1a79701e20df4831b458e678f
tomo <tomo>
1999-05-17 09:43:52 +0000

    XEmacs 21.2.14.

--- lib-src/update-elc.sh
+++ lib-src/update-elc.sh
@@ -106,7 +106,7 @@ lisp_prog='(princ (featurep (quote mule)))'
 mule_p="`$EMACS -batch -vanilla -eval \"$lisp_prog\"`"
 if test "$mule_p" = nil ; then
 	echo No
-	ignore_dirs="$ignore=dirs mule"
+	ignore_dirs="$ignore_dirs mule"
 else
   echo Yes
 fi
@@ -176,7 +176,6 @@ ignore_pattern="$ignore_pattern"'
 \!/site-init.el$!d
 \!/version.el$!d
 \!/very-early-lisp.el$!d
-\!/Installation.el$!d
 '
 
 echo "Compiling files without .elc..."
--- nt/inc/sys/file.h
+++ nt/inc/sys/file.h
@@ -3,6 +3,9 @@
  */
 
 #define F_OK 0
+#ifdef X_OK
+#undef X_OK
+#endif
 #define X_OK 1
 #define W_OK 2
 #define R_OK 4
