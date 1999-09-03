0508e898c39759a4426f55f2f3aefc7e14c6d3e7
tomo <tomo>
1999-09-03 22:55:06 +0000

    (Charset_ID): Moved from buffer.h. (MIN_LEADING_BYTE): Likewise. (LEADING_BYTE_ASCII): Likewise. (NUM_LEADING_BYTES): Likewise. (CHARSETP): Likewise. (CHARSET_BY_LEADING_BYTE): Likewise. (XCHARSET_LEADING_BYTE): Likewise. (XCHARSET_GRAPHIC): Likewise. (XCHARSET_COLUMNS): Likewise. (XCHARSET_DIMENSION): Likewise. (CHAR_CHARSET): Likewise. (CHAR_LEADING_BYTE): Likewise. (BREAKUP_CHAR): Likewise. (Vcharset_ascii): Likewise.

--- src/char-1byte.h
+++ src/char-1byte.h
@@ -26,4 +26,48 @@ Boston, MA 02111-1307, USA.  */
 
 #define valid_char_p(ch) ((unsigned int) (ch) <= 0xff)
 
+
+/************************************************************************/
+/*                    Definition of leading bytes                       */
+/************************************************************************/
+
+typedef int Charset_ID;
+
+#define MIN_LEADING_BYTE 0x80
+#define LEADING_BYTE_ASCII 0x80
+#define NUM_LEADING_BYTES 1
+
+
+/************************************************************************/
+/*            Information about a particular character set              */
+/************************************************************************/
+
+#define CHARSETP(cs) 1
+#define CHARSET_BY_LEADING_BYTE(lb) Vcharset_ascii
+#define XCHARSET_LEADING_BYTE(cs) LEADING_BYTE_ASCII
+#define XCHARSET_GRAPHIC(cs) -1
+#define XCHARSET_COLUMNS(cs) 1
+#define XCHARSET_DIMENSION(cs) 1
+
+
+/************************************************************************/
+/*                        Dealing with characters                       */
+/************************************************************************/
+
+#define CHAR_CHARSET(ch) Vcharset_ascii
+#define CHAR_LEADING_BYTE(ch) LEADING_BYTE_ASCII
+
+#define BREAKUP_CHAR(ch, charset, byte1, byte2) do {	\
+  (charset) = Vcharset_ascii;				\
+  (byte1) = (ch);					\
+  (byte2) = 0;						\
+} while (0)
+
+
+/************************************************************************/
+/*                            Exported functions                        */
+/************************************************************************/
+
+#define Vcharset_ascii Qnil
+
 #endif /* _XEMACS_CHAR_1BYTE_H */
