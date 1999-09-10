fc0e06c20f21c1ade61a43b857a3e138e0bf9579
tomo <tomo>
1999-09-10 10:44:47 +0000

    (vars_of_mule_charset): Update `utf-2000-version' to 0.7 (Hirano).

--- src/mule-charset.c
+++ src/mule-charset.c
@@ -2847,7 +2847,7 @@ Leading-code of private TYPE9N charset of column-width 1.
 #endif
 
 #ifdef UTF2000
-  Vutf_2000_version = build_string("0.6 (Tōbushijō-mae)");
+  Vutf_2000_version = build_string("0.7 (Hirano)");
   DEFVAR_LISP ("utf-2000-version", &Vutf_2000_version /*
 Version number of UTF-2000.
 */ );
