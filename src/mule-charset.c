97083168752d478f62853aa509e2154724a30fc8
tomo <tomo>
1999-09-14 16:04:13 +0000

    Update `utf-2000-version' to 0.8 (Kami).

--- src/mule-charset.c
+++ src/mule-charset.c
@@ -2591,7 +2591,7 @@ Leading-code of private TYPE9N charset of column-width 1.
 #endif
 
 #ifdef UTF2000
-  Vutf_2000_version = build_string("0.7 (Hirano)");
+  Vutf_2000_version = build_string("0.8 (Kami)");
   DEFVAR_LISP ("utf-2000-version", &Vutf_2000_version /*
 Version number of UTF-2000.
 */ );
