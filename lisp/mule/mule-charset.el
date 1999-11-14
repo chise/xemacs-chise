fe4a0869e62e39dbf706f830dc739f2bb9f80721
tomo <tomo>
1999-11-14 12:57:48 +0000

    (default-coded-charset-priority-list): Add `chinese-cns11643-*'.

--- lisp/mule/mule-charset.el
+++ lisp/mule/mule-charset.el
@@ -273,6 +273,13 @@ DESCRIPTION (string) is the description string of the charset."
           ;; katakana-jisx0208
 	  japanese-jisx0208
 	  japanese-jisx0212
+	  chinese-cns11643-1
+	  chinese-cns11643-2
+	  chinese-cns11643-3
+	  chinese-cns11643-4
+	  chinese-cns11643-5
+	  chinese-cns11643-6
+	  chinese-cns11643-7
 	  korean-ksc5601
 	  hebrew-iso8859-8
 	  latin-viscii
