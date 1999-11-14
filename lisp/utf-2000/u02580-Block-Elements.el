df2a167b9592e9efcda0c26c390e0a9b7054e809
tomo <tomo>
1999-11-14 14:48:40 +0000

    New files.

---
+++ lisp/utf-2000/u02580-Block-Elements.el
@@ -0,0 +1,172 @@
+(define-char
+  '((name		. "UPPER HALF BLOCK")
+    (ucs		. #x2580)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    ))
+(define-char
+  '((name		. "LOWER ONE EIGHTH BLOCK")
+    (ucs		. #x2581)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x24)	; $(G#$(B
+    ))
+(define-char
+  '((name		. "LOWER ONE QUARTER BLOCK")
+    (ucs		. #x2582)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x25)	; $(G#%(B
+    ))
+(define-char
+  '((name		. "LOWER THREE EIGHTHS BLOCK")
+    (ucs		. #x2583)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x26)	; $(G#&(B
+    ))
+(define-char
+  '((name		. "LOWER HALF BLOCK")
+    (ucs		. #x2584)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x27)	; $(G#'(B
+    ))
+(define-char
+  '((name		. "LOWER FIVE EIGHTHS BLOCK")
+    (ucs		. #x2585)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x28)	; $(G#((B
+    ))
+(define-char
+  '((name		. "LOWER THREE QUARTERS BLOCK")
+    (ucs		. #x2586)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x29)	; $(G#)(B
+    ))
+(define-char
+  '((name		. "LOWER SEVEN EIGHTHS BLOCK")
+    (ucs		. #x2587)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x2A)	; $(G#*(B
+    ))
+(define-char
+  '((name		. "FULL BLOCK")
+    (ucs		. #x2588)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x2B)	; $(G#+(B
+    ))
+(define-char
+  '((name		. "LEFT SEVEN EIGHTHS BLOCK")
+    (ucs		. #x2589)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x32)	; $(G#2(B
+    ))
+(define-char
+  '((name		. "LEFT THREE QUARTERS BLOCK")
+    (ucs		. #x258A)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x31)	; $(G#1(B
+    ))
+(define-char
+  '((name		. "LEFT FIVE EIGHTHS BLOCK")
+    (ucs		. #x258B)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x30)	; $(G#0(B
+    ))
+(define-char
+  '((name		. "LEFT HALF BLOCK")
+    (ucs		. #x258C)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x2F)	; $(G#/(B
+    ))
+(define-char
+  '((name		. "LEFT THREE EIGHTHS BLOCK")
+    (ucs		. #x258D)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x2E)	; $(G#.(B
+    ))
+(define-char
+  '((name		. "LEFT ONE QUARTER BLOCK")
+    (ucs		. #x258E)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x2D)	; $(G#-(B
+    ))
+(define-char
+  '((name		. "LEFT ONE EIGHTH BLOCK")
+    (ucs		. #x258F)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x2C)	; $(G#,(B
+    ))
+(define-char
+  '((name		. "RIGHT HALF BLOCK")
+    (ucs		. #x2590)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    ))
+(define-char
+  '((name		. "LIGHT SHADE")
+    (ucs		. #x2591)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    ))
+(define-char
+  '((name		. "MEDIUM SHADE")
+    (ucs		. #x2592)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (korean-ksc5601	#x22 #x46)	; $(C"F(B
+    ))
+(define-char
+  '((name		. "DARK SHADE")
+    (ucs		. #x2593)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    ))
+(define-char
+  '((name		. "UPPER ONE EIGHTH BLOCK")
+    (ucs		. #x2594)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x38)	; $(G#8(B
+    ))
+(define-char
+  '((name		. "RIGHT ONE EIGHTH BLOCK")
+    (ucs		. #x2595)
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (chinese-cns11643-1 #x23 #x3B)	; $(G#;(B
+    ))
