0bf5dffc50e38c2722db974be19890302b58401a
tomo <tomo>
2000-06-02 10:20:38 +0000

    (vars_of_mule_charset): Update `utf-2000-version' to 0.16.

--- src/mule-charset.c
+++ src/mule-charset.c
@@ -3199,7 +3199,7 @@ Leading-code of private TYPE9N charset of column-width 1.
 #endif
 
 #ifdef UTF2000
-  Vutf_2000_version = build_string("0.15 (Sangō)");
+  Vutf_2000_version = build_string("0.16 (Ōji)");
   DEFVAR_LISP ("utf-2000-version", &Vutf_2000_version /*
 Version number of UTF-2000.
 */ );
