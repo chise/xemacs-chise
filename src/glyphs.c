1f160a30eaa0a391343d023ab7dd2351690247d6
tomo <tomo>
2000-06-07 09:13:20 +0000

    (query_string_geometry): Use Charset_ID instead of unsigned char. (query_string_font): Likewise.

--- src/glyphs.c
+++ src/glyphs.c
@@ -2165,7 +2165,7 @@ query_string_geometry (Lisp_Object string, Lisp_Object face,
 		       unsigned int* descent, Lisp_Object domain)
 {
   struct font_metric_info fm;
-  unsigned char charsets[NUM_LEADING_BYTES];
+  Charset_ID charsets[NUM_LEADING_BYTES];
   struct face_cachel frame_cachel;
   struct face_cachel *cachel;
   Lisp_Object frame = DOMAIN_FRAME (domain);
@@ -2217,7 +2217,7 @@ query_string_geometry (Lisp_Object string, Lisp_Object face,
 Lisp_Object
 query_string_font (Lisp_Object string, Lisp_Object face, Lisp_Object domain)
 {
-  unsigned char charsets[NUM_LEADING_BYTES];
+  Charset_ID charsets[NUM_LEADING_BYTES];
   struct face_cachel frame_cachel;
   struct face_cachel *cachel;
   int i;
