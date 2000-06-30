9518568dd974c09ec08227e42d09a239dd867440
tomo <tomo>
2000-06-30 08:57:27 +0000

    Use `HAVE_GGC' instead of `UTF2000' for `make_older_vector' and `make_vector_newer'.

--- src/lisp.h
+++ src/lisp.h
@@ -1988,7 +1988,7 @@ typedef unsigned long uintptr_t;
 void release_breathing_space (void);
 Lisp_Object noseeum_cons (Lisp_Object, Lisp_Object);
 Lisp_Object make_vector (size_t, Lisp_Object);
-#ifdef UTF2000
+#ifdef HAVE_GGC
 Lisp_Object make_older_vector (size_t, Lisp_Object);
 void make_vector_newer (Lisp_Object);
 #endif
