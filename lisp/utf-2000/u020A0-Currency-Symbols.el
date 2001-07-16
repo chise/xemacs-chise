ff30d3936c73a92a01b850bee3d3c2c5d44e5b44
tomo <tomo>
2001-07-16 16:41:37 +0000

    (EURO SIGN): Unify BIG5-A3E1.

--- lisp/utf-2000/u020A0-Currency-Symbols.el
+++ lisp/utf-2000/u020A0-Currency-Symbols.el
@@ -1,114 +1,115 @@
 (define-char
   '((name		. "EURO-CURRENCY SIGN")
-    (ucs		. #x20A0)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (ucs		. #x20A0)	; ₠
     ))
 (define-char
   '((name		. "COLON SIGN")
-    (ucs		. #x20A1)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (ucs		. #x20A1)	; ₡
     ))
 (define-char
   '((name		. "CRUZEIRO SIGN")
-    (ucs		. #x20A2)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (ucs		. #x20A2)	; ₢
     ))
 (define-char
   '((name		. "FRENCH FRANC SIGN")
-    (ucs		. #x20A3)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (ucs		. #x20A3)	; ₣
     ))
 (define-char
   '((name		. "LIRA SIGN")
-    (ucs		. #x20A4)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (ucs		. #x20A4)	; ₤
     ))
 (define-char
   '((name		. "MILL SIGN")
-    (ucs		. #x20A5)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (ucs		. #x20A5)	; ₥
     ))
 (define-char
   '((name		. "NAIRA SIGN")
-    (ucs		. #x20A6)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (ucs		. #x20A6)	; ₦
     ))
 (define-char
   '((name		. "PESETA SIGN")
-    (ucs		. #x20A7)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (ucs		. #x20A7)	; ₧
     ))
 (define-char
   '((name		. "RUPEE SIGN")
-    (ucs		. #x20A8)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
     (->decomposition	compat #x0052 #x0073)
+    (ucs		. #x20A8)	; ₨
     ))
 (define-char
   '((name		. "WON SIGN")
-    (ucs		. #x20A9)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (ucs		. #x20A9)	; ₩
     ))
 (define-char
   '((name		. "NEW SHEQEL SIGN")
-    (ucs		. #x20AA)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (ucs		. #x20AA)	; ₪
     ))
 (define-char
   '((name		. "DONG SIGN")
-    (ucs		. #x20AB)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (ucs		. #x20AB)	; ₫
     ))
 (define-char
   '((name		. "EURO SIGN")
-    (ucs		. #x20AC)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
-    (japanese-jisx0213-1	#x29 #x21)	; €
+    (japanese-jisx0213-1	. #x2921)	; �����
+    (chinese-big5		. #xA3E1)	; �����
+    (ucs			. #x20AC)	; €
     ))
 (define-char
   '((name		. "KIP SIGN")
-    (ucs		. #x20AD)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (ucs		. #x20AD)	; ₭
     ))
 (define-char
   '((name		. "TUGRIK SIGN")
-    (ucs		. #x20AE)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (ucs		. #x20AE)	; ₮
     ))
 (define-char
   '((name		. "DRACHMA SIGN")
-    (ucs		. #x20AF)
     (general-category	symbol currency) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (ucs		. #x20AF)	; ₯
     ))
