66fa0fd7af19d578e3fb3e35e906af6187c0bbce
tomo <tomo>
2001-07-26 16:56:58 +0000

    Reformatted.

--- lisp/utf-2000/u020D0-Combining-Diacritical-Marks-for-Symbols.el
+++ lisp/utf-2000/u020D0-Combining-Diacritical-Marks-for-Symbols.el
@@ -1,140 +1,140 @@
 (define-char
   '((name		. "COMBINING LEFT HARPOON ABOVE")
-    (ucs		. #x20D0)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20D0)	; ⃐
     ))
 (define-char
   '((name		. "COMBINING RIGHT HARPOON ABOVE")
-    (ucs		. #x20D1)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20D1)	; ⃑
     ))
 (define-char
   '((name		. "COMBINING LONG VERTICAL LINE OVERLAY")
-    (ucs		. #x20D2)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20D2)	; ⃒
     ))
 (define-char
   '((name		. "COMBINING SHORT VERTICAL LINE OVERLAY")
-    (ucs		. #x20D3)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20D3)	; ⃓
     ))
 (define-char
   '((name		. "COMBINING ANTICLOCKWISE ARROW ABOVE")
-    (ucs		. #x20D4)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20D4)	; ⃔
     ))
 (define-char
   '((name		. "COMBINING CLOCKWISE ARROW ABOVE")
-    (ucs		. #x20D5)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20D5)	; ⃕
     ))
 (define-char
   '((name		. "COMBINING LEFT ARROW ABOVE")
-    (ucs		. #x20D6)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20D6)	; ⃖
     ))
 (define-char
   '((name		. "COMBINING RIGHT ARROW ABOVE")
-    (ucs		. #x20D7)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20D7)	; ⃗
     ))
 (define-char
   '((name		. "COMBINING RING OVERLAY")
-    (ucs		. #x20D8)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20D8)	; ⃘
     ))
 (define-char
   '((name		. "COMBINING CLOCKWISE RING OVERLAY")
-    (ucs		. #x20D9)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20D9)	; ⃙
     ))
 (define-char
   '((name		. "COMBINING ANTICLOCKWISE RING OVERLAY")
-    (ucs		. #x20DA)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20DA)	; ⃚
     ))
 (define-char
   '((name		. "COMBINING THREE DOTS ABOVE")
-    (ucs		. #x20DB)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20DB)	; ⃛
     ))
 (define-char
   '((name		. "COMBINING FOUR DOTS ABOVE")
-    (ucs		. #x20DC)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20DC)	; ⃜
     ))
 (define-char
   '((name		. "COMBINING ENCLOSING CIRCLE")
-    (ucs		. #x20DD)
     (general-category	mark enclosing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20DD)	; ⃝
     ))
 (define-char
   '((name		. "COMBINING ENCLOSING SQUARE")
-    (ucs		. #x20DE)
     (general-category	mark enclosing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20DE)	; ⃞
     ))
 (define-char
   '((name		. "COMBINING ENCLOSING DIAMOND")
-    (ucs		. #x20DF)
     (general-category	mark enclosing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20DF)	; ⃟
     ))
 (define-char
   '((name		. "COMBINING ENCLOSING CIRCLE BACKSLASH")
-    (ucs		. #x20E0)
     (general-category	mark enclosing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20E0)	; ⃠
     ))
 (define-char
   '((name		. "COMBINING LEFT RIGHT ARROW ABOVE")
-    (ucs		. #x20E1)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20E1)	; ⃡
     ))
 (define-char
   '((name		. "COMBINING ENCLOSING SCREEN")
-    (ucs		. #x20E2)
     (general-category	mark enclosing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20E2)	; ⃢
     ))
 (define-char
   '((name		. "COMBINING ENCLOSING KEYCAP")
-    (ucs		. #x20E3)
     (general-category	mark enclosing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x20E3)	; ⃣
     ))
