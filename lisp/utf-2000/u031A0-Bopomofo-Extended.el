835bcc33f657913ff0aa95a64569ad5aa0f4142d
tomo <tomo>
2001-07-29 16:33:18 +0000

    Rename u031A0-Extended-Bopomofo.el to u031A0-Bopomofo-Extended.el.

--- lisp/utf-2000/u031A0-Bopomofo-Extended.el
+++ lisp/utf-2000/u031A0-Bopomofo-Extended.el
@@ -1,168 +1,168 @@
 (define-char
   '((name		. "BOPOMOFO LETTER BU")
-    (ucs		. #x31A0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31A0)	; ㆠ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER ZI")
-    (ucs		. #x31A1)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31A1)	; ㆡ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER JI")
-    (ucs		. #x31A2)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31A2)	; ㆢ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER GU")
-    (ucs		. #x31A3)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31A3)	; ㆣ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER EE")
-    (ucs		. #x31A4)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31A4)	; ㆤ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER ENN")
-    (ucs		. #x31A5)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31A5)	; ㆥ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER OO")
-    (ucs		. #x31A6)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31A6)	; ㆦ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER ONN")
-    (ucs		. #x31A7)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31A7)	; ㆧ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER IR")
-    (ucs		. #x31A8)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31A8)	; ㆨ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER ANN")
-    (ucs		. #x31A9)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31A9)	; ㆩ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER INN")
-    (ucs		. #x31AA)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31AA)	; ㆪ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER UNN")
-    (ucs		. #x31AB)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31AB)	; ㆫ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER IM")
-    (ucs		. #x31AC)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31AC)	; ㆬ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER NGG")
-    (ucs		. #x31AD)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31AD)	; ㆭ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER AINN")
-    (ucs		. #x31AE)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31AE)	; ㆮ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER AUNN")
-    (ucs		. #x31AF)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31AF)	; ㆯ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER AM")
-    (ucs		. #x31B0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31B0)	; ㆰ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER OM")
-    (ucs		. #x31B1)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31B1)	; ㆱ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER ONG")
-    (ucs		. #x31B2)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31B2)	; ㆲ
     ))
 (define-char
   '((name		. "BOPOMOFO LETTER INNN")
-    (ucs		. #x31B3)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31B3)	; ㆳ
     ))
 (define-char
   '((name		. "BOPOMOFO FINAL LETTER P")
-    (ucs		. #x31B4)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31B4)	; ㆴ
     ))
 (define-char
   '((name		. "BOPOMOFO FINAL LETTER T")
-    (ucs		. #x31B5)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31B5)	; ㆵ
     ))
 (define-char
   '((name		. "BOPOMOFO FINAL LETTER K")
-    (ucs		. #x31B6)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31B6)	; ㆶ
     ))
 (define-char
   '((name		. "BOPOMOFO FINAL LETTER H")
-    (ucs		. #x31B7)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x31B7)	; ㆷ
     ))
