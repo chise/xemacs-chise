c58f996c40e93bc12710c963526102b589fedeae
tomo <tomo>
2001-07-29 08:34:39 +0000

    Reformatted.

--- lisp/utf-2000/u0FB00-Alphabetic-Presentation-Forms.el
+++ lisp/utf-2000/u0FB00-Alphabetic-Presentation-Forms.el
@@ -1,463 +1,463 @@
 (define-char
   '((name		. "LATIN SMALL LIGATURE FF")
-    (ucs		. #xFB00)
     (general-category	letter lowercase) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	compat #x0066 #x0066)
     (mirrored		. nil)
+    (->decomposition	compat #x0066 #x0066)
+    (ucs		. #xFB00)	; ﬀ
     ))
 (define-char
   '((name		. "LATIN SMALL LIGATURE FI")
-    (ucs		. #xFB01)
     (general-category	letter lowercase) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	compat #x0066 #x0069)
     (mirrored		. nil)
+    (->decomposition	compat #x0066 #x0069)
+    (ucs		. #xFB01)	; ﬁ
     ))
 (define-char
   '((name		. "LATIN SMALL LIGATURE FL")
-    (ucs		. #xFB02)
     (general-category	letter lowercase) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	compat #x0066 #x006C)
     (mirrored		. nil)
+    (->decomposition	compat #x0066 #x006C)
+    (ucs		. #xFB02)	; ﬂ
     ))
 (define-char
   '((name		. "LATIN SMALL LIGATURE FFI")
-    (ucs		. #xFB03)
     (general-category	letter lowercase) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	compat #x0066 #x0066 #x0069)
     (mirrored		. nil)
+    (->decomposition	compat #x0066 #x0066 #x0069)
+    (ucs		. #xFB03)	; ﬃ
     ))
 (define-char
   '((name		. "LATIN SMALL LIGATURE FFL")
-    (ucs		. #xFB04)
     (general-category	letter lowercase) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	compat #x0066 #x0066 #x006C)
     (mirrored		. nil)
+    (->decomposition	compat #x0066 #x0066 #x006C)
+    (ucs		. #xFB04)	; ﬄ
     ))
 (define-char
   '((name		. "LATIN SMALL LIGATURE LONG S T")
-    (ucs		. #xFB05)
     (general-category	letter lowercase) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	compat #x017F #x0074)
     (mirrored		. nil)
+    (->decomposition	compat #x017F #x0074)
+    (ucs		. #xFB05)	; ﬅ
     ))
 (define-char
   '((name		. "LATIN SMALL LIGATURE ST")
-    (ucs		. #xFB06)
     (general-category	letter lowercase) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	compat #x0073 #x0074)
     (mirrored		. nil)
+    (->decomposition	compat #x0073 #x0074)
+    (ucs		. #xFB06)	; ﬆ
     ))
 (define-char
   '((name		. "ARMENIAN SMALL LIGATURE MEN NOW")
-    (ucs		. #xFB13)
     (general-category	letter lowercase) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	compat #x0574 #x0576)
     (mirrored		. nil)
+    (->decomposition	compat #x0574 #x0576)
+    (ucs		. #xFB13)	; ﬓ
     ))
 (define-char
   '((name		. "ARMENIAN SMALL LIGATURE MEN ECH")
-    (ucs		. #xFB14)
     (general-category	letter lowercase) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	compat #x0574 #x0565)
     (mirrored		. nil)
+    (->decomposition	compat #x0574 #x0565)
+    (ucs		. #xFB14)	; ﬔ
     ))
 (define-char
   '((name		. "ARMENIAN SMALL LIGATURE MEN INI")
-    (ucs		. #xFB15)
     (general-category	letter lowercase) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	compat #x0574 #x056B)
     (mirrored		. nil)
+    (->decomposition	compat #x0574 #x056B)
+    (ucs		. #xFB15)	; ﬕ
     ))
 (define-char
   '((name		. "ARMENIAN SMALL LIGATURE VEW NOW")
-    (ucs		. #xFB16)
     (general-category	letter lowercase) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	compat #x057E #x0576)
     (mirrored		. nil)
+    (->decomposition	compat #x057E #x0576)
+    (ucs		. #xFB16)	; ﬖ
     ))
 (define-char
   '((name		. "ARMENIAN SMALL LIGATURE MEN XEH")
-    (ucs		. #xFB17)
     (general-category	letter lowercase) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	compat #x0574 #x056D)
     (mirrored		. nil)
+    (->decomposition	compat #x0574 #x056D)
+    (ucs		. #xFB17)	; ﬗ
     ))
 (define-char
   '((name		. "HEBREW LETTER YOD WITH HIRIQ")
-    (ucs		. #xFB1D)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05D9 #x05B4)
     (mirrored		. nil)
+    (->decomposition	#x05D9 #x05B4)
+    (ucs		. #xFB1D)	; יִ
     ))
 (define-char
   '((name		. "HEBREW POINT JUDEO-SPANISH VARIKA")
-    (ucs		. #xFB1E)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #xFB1E)	; ﬞ
     ))
 (define-char
   '((name		. "HEBREW LIGATURE YIDDISH YOD YOD PATAH")
-    (ucs		. #xFB1F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05F2 #x05B7)
     (mirrored		. nil)
+    (->decomposition	#x05F2 #x05B7)
+    (ucs		. #xFB1F)	; ײַ
     ))
 (define-char
   '((name		. "HEBREW LETTER ALTERNATIVE AYIN")
-    (ucs		. #xFB20)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	font #x05E2)
     (mirrored		. nil)
+    (->decomposition	font #x05E2)
+    (ucs		. #xFB20)	; ﬠ
     ))
 (define-char
   '((name		. "HEBREW LETTER WIDE ALEF")
-    (ucs		. #xFB21)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	font #x05D0)
     (mirrored		. nil)
+    (->decomposition	font #x05D0)
+    (ucs		. #xFB21)	; ﬡ
     ))
 (define-char
   '((name		. "HEBREW LETTER WIDE DALET")
-    (ucs		. #xFB22)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	font #x05D3)
     (mirrored		. nil)
+    (->decomposition	font #x05D3)
+    (ucs		. #xFB22)	; ﬢ
     ))
 (define-char
   '((name		. "HEBREW LETTER WIDE HE")
-    (ucs		. #xFB23)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	font #x05D4)
     (mirrored		. nil)
+    (->decomposition	font #x05D4)
+    (ucs		. #xFB23)	; ﬣ
     ))
 (define-char
   '((name		. "HEBREW LETTER WIDE KAF")
-    (ucs		. #xFB24)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	font #x05DB)
     (mirrored		. nil)
+    (->decomposition	font #x05DB)
+    (ucs		. #xFB24)	; ﬤ
     ))
 (define-char
   '((name		. "HEBREW LETTER WIDE LAMED")
-    (ucs		. #xFB25)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	font #x05DC)
     (mirrored		. nil)
+    (->decomposition	font #x05DC)
+    (ucs		. #xFB25)	; ﬥ
     ))
 (define-char
   '((name		. "HEBREW LETTER WIDE FINAL MEM")
-    (ucs		. #xFB26)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	font #x05DD)
     (mirrored		. nil)
+    (->decomposition	font #x05DD)
+    (ucs		. #xFB26)	; ﬦ
     ))
 (define-char
   '((name		. "HEBREW LETTER WIDE RESH")
-    (ucs		. #xFB27)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	font #x05E8)
     (mirrored		. nil)
+    (->decomposition	font #x05E8)
+    (ucs		. #xFB27)	; ﬧ
     ))
 (define-char
   '((name		. "HEBREW LETTER WIDE TAV")
-    (ucs		. #xFB28)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	font #x05EA)
     (mirrored		. nil)
+    (->decomposition	font #x05EA)
+    (ucs		. #xFB28)	; ﬨ
     ))
 (define-char
   '((name		. "HEBREW LETTER ALTERNATIVE PLUS SIGN")
-    (ucs		. #xFB29)
     (general-category	symbol math) ; Informative Category
     (bidi-category	. "ET")
-    (->decomposition	font #x002B)
     (mirrored		. nil)
+    (->decomposition	font #x002B)
+    (ucs		. #xFB29)	; ﬩
     ))
 (define-char
   '((name		. "HEBREW LETTER SHIN WITH SHIN DOT")
-    (ucs		. #xFB2A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05E9 #x05C1)
     (mirrored		. nil)
+    (->decomposition	#x05E9 #x05C1)
+    (ucs		. #xFB2A)	; שׁ
     ))
 (define-char
   '((name		. "HEBREW LETTER SHIN WITH SIN DOT")
-    (ucs		. #xFB2B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05E9 #x05C2)
     (mirrored		. nil)
+    (->decomposition	#x05E9 #x05C2)
+    (ucs		. #xFB2B)	; שׂ
     ))
 (define-char
   '((name		. "HEBREW LETTER SHIN WITH DAGESH AND SHIN DOT")
-    (ucs		. #xFB2C)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#xFB49 #x05C1)
     (mirrored		. nil)
+    (->decomposition	#xFB49 #x05C1)
+    (ucs		. #xFB2C)	; שּׁ
     ))
 (define-char
   '((name		. "HEBREW LETTER SHIN WITH DAGESH AND SIN DOT")
-    (ucs		. #xFB2D)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#xFB49 #x05C2)
     (mirrored		. nil)
+    (->decomposition	#xFB49 #x05C2)
+    (ucs		. #xFB2D)	; שּׂ
     ))
 (define-char
   '((name		. "HEBREW LETTER ALEF WITH PATAH")
-    (ucs		. #xFB2E)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05D0 #x05B7)
     (mirrored		. nil)
+    (->decomposition	#x05D0 #x05B7)
+    (ucs		. #xFB2E)	; אַ
     ))
 (define-char
   '((name		. "HEBREW LETTER ALEF WITH QAMATS")
-    (ucs		. #xFB2F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05D0 #x05B8)
     (mirrored		. nil)
+    (->decomposition	#x05D0 #x05B8)
+    (ucs		. #xFB2F)	; אָ
     ))
 (define-char
   '((name		. "HEBREW LETTER ALEF WITH MAPIQ")
-    (ucs		. #xFB30)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05D0 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05D0 #x05BC)
+    (ucs		. #xFB30)	; אּ
     ))
 (define-char
   '((name		. "HEBREW LETTER BET WITH DAGESH")
-    (ucs		. #xFB31)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05D1 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05D1 #x05BC)
+    (ucs		. #xFB31)	; בּ
     ))
 (define-char
   '((name		. "HEBREW LETTER GIMEL WITH DAGESH")
-    (ucs		. #xFB32)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05D2 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05D2 #x05BC)
+    (ucs		. #xFB32)	; גּ
     ))
 (define-char
   '((name		. "HEBREW LETTER DALET WITH DAGESH")
-    (ucs		. #xFB33)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05D3 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05D3 #x05BC)
+    (ucs		. #xFB33)	; דּ
     ))
 (define-char
   '((name		. "HEBREW LETTER HE WITH MAPIQ")
-    (ucs		. #xFB34)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05D4 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05D4 #x05BC)
+    (ucs		. #xFB34)	; הּ
     ))
 (define-char
   '((name		. "HEBREW LETTER VAV WITH DAGESH")
-    (ucs		. #xFB35)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05D5 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05D5 #x05BC)
+    (ucs		. #xFB35)	; וּ
     ))
 (define-char
   '((name		. "HEBREW LETTER ZAYIN WITH DAGESH")
-    (ucs		. #xFB36)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05D6 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05D6 #x05BC)
+    (ucs		. #xFB36)	; זּ
     ))
 (define-char
   '((name		. "HEBREW LETTER TET WITH DAGESH")
-    (ucs		. #xFB38)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05D8 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05D8 #x05BC)
+    (ucs		. #xFB38)	; טּ
     ))
 (define-char
   '((name		. "HEBREW LETTER YOD WITH DAGESH")
-    (ucs		. #xFB39)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05D9 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05D9 #x05BC)
+    (ucs		. #xFB39)	; יּ
     ))
 (define-char
   '((name		. "HEBREW LETTER FINAL KAF WITH DAGESH")
-    (ucs		. #xFB3A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05DA #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05DA #x05BC)
+    (ucs		. #xFB3A)	; ךּ
     ))
 (define-char
   '((name		. "HEBREW LETTER KAF WITH DAGESH")
-    (ucs		. #xFB3B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05DB #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05DB #x05BC)
+    (ucs		. #xFB3B)	; כּ
     ))
 (define-char
   '((name		. "HEBREW LETTER LAMED WITH DAGESH")
-    (ucs		. #xFB3C)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05DC #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05DC #x05BC)
+    (ucs		. #xFB3C)	; לּ
     ))
 (define-char
   '((name		. "HEBREW LETTER MEM WITH DAGESH")
-    (ucs		. #xFB3E)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05DE #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05DE #x05BC)
+    (ucs		. #xFB3E)	; מּ
     ))
 (define-char
   '((name		. "HEBREW LETTER NUN WITH DAGESH")
-    (ucs		. #xFB40)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05E0 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05E0 #x05BC)
+    (ucs		. #xFB40)	; נּ
     ))
 (define-char
   '((name		. "HEBREW LETTER SAMEKH WITH DAGESH")
-    (ucs		. #xFB41)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05E1 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05E1 #x05BC)
+    (ucs		. #xFB41)	; סּ
     ))
 (define-char
   '((name		. "HEBREW LETTER FINAL PE WITH DAGESH")
-    (ucs		. #xFB43)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05E3 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05E3 #x05BC)
+    (ucs		. #xFB43)	; ףּ
     ))
 (define-char
   '((name		. "HEBREW LETTER PE WITH DAGESH")
-    (ucs		. #xFB44)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05E4 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05E4 #x05BC)
+    (ucs		. #xFB44)	; פּ
     ))
 (define-char
   '((name		. "HEBREW LETTER TSADI WITH DAGESH")
-    (ucs		. #xFB46)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05E6 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05E6 #x05BC)
+    (ucs		. #xFB46)	; צּ
     ))
 (define-char
   '((name		. "HEBREW LETTER QOF WITH DAGESH")
-    (ucs		. #xFB47)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05E7 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05E7 #x05BC)
+    (ucs		. #xFB47)	; קּ
     ))
 (define-char
   '((name		. "HEBREW LETTER RESH WITH DAGESH")
-    (ucs		. #xFB48)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05E8 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05E8 #x05BC)
+    (ucs		. #xFB48)	; רּ
     ))
 (define-char
   '((name		. "HEBREW LETTER SHIN WITH DAGESH")
-    (ucs		. #xFB49)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05E9 #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05E9 #x05BC)
+    (ucs		. #xFB49)	; שּ
     ))
 (define-char
   '((name		. "HEBREW LETTER TAV WITH DAGESH")
-    (ucs		. #xFB4A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05EA #x05BC)
     (mirrored		. nil)
+    (->decomposition	#x05EA #x05BC)
+    (ucs		. #xFB4A)	; תּ
     ))
 (define-char
   '((name		. "HEBREW LETTER VAV WITH HOLAM")
-    (ucs		. #xFB4B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05D5 #x05B9)
     (mirrored		. nil)
+    (->decomposition	#x05D5 #x05B9)
+    (ucs		. #xFB4B)	; וֹ
     ))
 (define-char
   '((name		. "HEBREW LETTER BET WITH RAFE")
-    (ucs		. #xFB4C)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05D1 #x05BF)
     (mirrored		. nil)
+    (->decomposition	#x05D1 #x05BF)
+    (ucs		. #xFB4C)	; בֿ
     ))
 (define-char
   '((name		. "HEBREW LETTER KAF WITH RAFE")
-    (ucs		. #xFB4D)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05DB #x05BF)
     (mirrored		. nil)
+    (->decomposition	#x05DB #x05BF)
+    (ucs		. #xFB4D)	; כֿ
     ))
 (define-char
   '((name		. "HEBREW LETTER PE WITH RAFE")
-    (ucs		. #xFB4E)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	#x05E4 #x05BF)
     (mirrored		. nil)
+    (->decomposition	#x05E4 #x05BF)
+    (ucs		. #xFB4E)	; פֿ
     ))
 (define-char
   '((name		. "HEBREW LIGATURE ALEF LAMED")
-    (ucs		. #xFB4F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "R")
-    (->decomposition	compat #x05D0 #x05DC)
     (mirrored		. nil)
+    (->decomposition	compat #x05D0 #x05DC)
+    (ucs		. #xFB4F)	; ﭏ
     ))
--- lisp/utf-2000/u0FE20-Combining-Half-Marks.el
+++ lisp/utf-2000/u0FE20-Combining-Half-Marks.el
@@ -1,28 +1,28 @@
 (define-char
   '((name		. "COMBINING LIGATURE LEFT HALF")
-    (ucs		. #xFE20)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #xFE20)	; ︠
     ))
 (define-char
   '((name		. "COMBINING LIGATURE RIGHT HALF")
-    (ucs		. #xFE21)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #xFE21)	; ︡
     ))
 (define-char
   '((name		. "COMBINING DOUBLE TILDE LEFT HALF")
-    (ucs		. #xFE22)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #xFE22)	; ︢
     ))
 (define-char
   '((name		. "COMBINING DOUBLE TILDE RIGHT HALF")
-    (ucs		. #xFE23)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #xFE23)	; ︣
     ))
