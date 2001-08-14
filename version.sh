994b4d838612d3226d6bc0f525d7f2bbaff8dd4f
tomo <tomo>
2001-08-14 14:55:51 +0000

    XEmacs 21.2.40 "Persephone".

--- version.sh
+++ version.sh
@@ -2,8 +2,8 @@
 emacs_is_beta=t
 emacs_major_version=21
 emacs_minor_version=2
-emacs_beta_version=39
-xemacs_codename="Millennium"
+emacs_beta_version=40
+xemacs_codename="Persephone"
 infodock_major_version=4
 infodock_minor_version=0
 infodock_build_version=8
