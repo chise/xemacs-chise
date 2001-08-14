5f839f2732a2c7ad80fc36a7d7d490053954c90d
tomo <tomo>
2001-08-14 11:20:16 +0000

    Sync up with r21-2-39.

--- version.sh
+++ version.sh
@@ -2,8 +2,8 @@
 emacs_is_beta=t
 emacs_major_version=21
 emacs_minor_version=2
-emacs_beta_version=38
-xemacs_codename="Peisinoë"
+emacs_beta_version=39
+xemacs_codename="Millennium"
 infodock_major_version=4
 infodock_minor_version=0
 infodock_build_version=8
