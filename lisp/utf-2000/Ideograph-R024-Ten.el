fe3ae76660b64a30115c468b9cc94998ac522310
tomo <tomo>
2001-10-03 10:20:11 +0000

    (J0-4234): Use <IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO MIDDLE AND BELOW> instead of <IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW> in `ideographic-structure'. (U+5352): Likewise.

--- lisp/utf-2000/Ideograph-R024-Ten.el
+++ lisp/utf-2000/Ideograph-R024-Ten.el
@@ -552,8 +552,8 @@
     (total-strokes	 . 8)
     (ideographic-structure
      (:cdp-combinator	2
-      :char	((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW")
-		 (ucs		     . #x2FF1)	; â¿±
+      :char	((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO MIDDLE AND BELOW")
+		 (ucs		     . #x2FF3)	; â¿³
 		 )
       )
      ((japanese-jisx0208-1978	  . #x5035)	; ø»ƒ–
@@ -582,8 +582,8 @@
     (total-strokes	 . 8)
     (ideographic-structure
      (:cdp-combinator	2
-      :char	((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW")
-		 (ucs		     . #x2FF1)	; â¿±
+      :char	((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO MIDDLE AND BELOW")
+		 (ucs		     . #x2FF3)	; â¿³
 		 )
       )
      ((ucs		  . #x4EA0)	; äº 
