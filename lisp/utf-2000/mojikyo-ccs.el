8e84dc5104f2bfef43ecd73153bdbe470a431e50
tomo <tomo>
2002-01-07 12:10:20 +0000

    Delete setting about `default-coded-charset-priority-list' for `mojikyo-pj-*'.

--- lisp/utf-2000/mojikyo-ccs.el
+++ lisp/utf-2000/mojikyo-ccs.el
@@ -1,3 +1,29 @@
+;;; mojikyo-ccs.el.el --- Definition of Mojikyo related coded-charsets
+
+;; Copyright (C) 2001,2002 MORIOKA Tomohiko.
+
+;; Author: MORIOKA Tomohiko <tomo@kanji.zinbun.kyoto-u.ac.jp>
+;; Keywords: UTF-2000, ISO/IEC 10646, Unicode, UCS-4, MULE.
+
+;; This file is part of XEmacs UTF-2000.
+
+;; XEmacs UTF-2000 is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; XEmacs UTF-2000 is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with XEmacs UTF-2000; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Code:
+
 (make-charset 'mojikyo
 	      "Mojikyo"
 	      `(long-name "Mojikyo"
@@ -42,8 +68,6 @@
 			      max-code ,(* 94 60 i)
 			      code-offset ,(1+ (* 94 60 (1- i)))
 			      conversion 94x60))
-    (unless (memq name default-coded-charset-priority-list)
-      (setq default-coded-charset-priority-list
-	    (append default-coded-charset-priority-list
-		    (list name))))
     (setq i (1+ i))))
+
+;;; mojikyo-ccs.el.el ends here
