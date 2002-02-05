0451b1f165bc9e1d7e53708c56c27ac651709940
tomo <tomo>
2002-02-05 17:53:41 +0000

    Reformatted.

--- lisp/utf-2000/u00A80-Gujarati.el
+++ lisp/utf-2000/u00A80-Gujarati.el
@@ -1,576 +1,576 @@
 (define-char
   '((name		. "GUJARATI SIGN CANDRABINDU")
-    (ucs		. #x0A81)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0A81)	; ઁ
     ))
 (define-char
   '((name		. "GUJARATI SIGN ANUSVARA")
-    (ucs		. #x0A82)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0A82)	; ં
     ))
 (define-char
   '((name		. "GUJARATI SIGN VISARGA")
-    (ucs		. #x0A83)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A83)	; ઃ
     ))
 (define-char
   '((name		. "GUJARATI LETTER A")
-    (ucs		. #x0A85)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A85)	; અ
     ))
 (define-char
   '((name		. "GUJARATI LETTER AA")
-    (ucs		. #x0A86)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A86)	; આ
     ))
 (define-char
   '((name		. "GUJARATI LETTER I")
-    (ucs		. #x0A87)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A87)	; ઇ
     ))
 (define-char
   '((name		. "GUJARATI LETTER II")
-    (ucs		. #x0A88)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A88)	; ઈ
     ))
 (define-char
   '((name		. "GUJARATI LETTER U")
-    (ucs		. #x0A89)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A89)	; ઉ
     ))
 (define-char
   '((name		. "GUJARATI LETTER UU")
-    (ucs		. #x0A8A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A8A)	; ઊ
     ))
 (define-char
   '((name		. "GUJARATI LETTER VOCALIC R")
-    (ucs		. #x0A8B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A8B)	; ઋ
     ))
 (define-char
   '((name		. "GUJARATI VOWEL CANDRA E")
-    (ucs		. #x0A8D)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A8D)	; ઍ
     ))
 (define-char
   '((name		. "GUJARATI LETTER E")
-    (ucs		. #x0A8F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A8F)	; એ
     ))
 (define-char
   '((name		. "GUJARATI LETTER AI")
-    (ucs		. #x0A90)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A90)	; ઐ
     ))
 (define-char
   '((name		. "GUJARATI VOWEL CANDRA O")
-    (ucs		. #x0A91)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A91)	; ઑ
     ))
 (define-char
   '((name		. "GUJARATI LETTER O")
-    (ucs		. #x0A93)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A93)	; ઓ
     ))
 (define-char
   '((name		. "GUJARATI LETTER AU")
-    (ucs		. #x0A94)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A94)	; ઔ
     ))
 (define-char
   '((name		. "GUJARATI LETTER KA")
-    (ucs		. #x0A95)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A95)	; ક
     ))
 (define-char
   '((name		. "GUJARATI LETTER KHA")
-    (ucs		. #x0A96)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A96)	; ખ
     ))
 (define-char
   '((name		. "GUJARATI LETTER GA")
-    (ucs		. #x0A97)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A97)	; ગ
     ))
 (define-char
   '((name		. "GUJARATI LETTER GHA")
-    (ucs		. #x0A98)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A98)	; ઘ
     ))
 (define-char
   '((name		. "GUJARATI LETTER NGA")
-    (ucs		. #x0A99)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A99)	; ઙ
     ))
 (define-char
   '((name		. "GUJARATI LETTER CA")
-    (ucs		. #x0A9A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A9A)	; ચ
     ))
 (define-char
   '((name		. "GUJARATI LETTER CHA")
-    (ucs		. #x0A9B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A9B)	; છ
     ))
 (define-char
   '((name		. "GUJARATI LETTER JA")
-    (ucs		. #x0A9C)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A9C)	; જ
     ))
 (define-char
   '((name		. "GUJARATI LETTER JHA")
-    (ucs		. #x0A9D)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A9D)	; ઝ
     ))
 (define-char
   '((name		. "GUJARATI LETTER NYA")
-    (ucs		. #x0A9E)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A9E)	; ઞ
     ))
 (define-char
   '((name		. "GUJARATI LETTER TTA")
-    (ucs		. #x0A9F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A9F)	; ટ
     ))
 (define-char
   '((name		. "GUJARATI LETTER TTHA")
-    (ucs		. #x0AA0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AA0)	; ઠ
     ))
 (define-char
   '((name		. "GUJARATI LETTER DDA")
-    (ucs		. #x0AA1)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AA1)	; ડ
     ))
 (define-char
   '((name		. "GUJARATI LETTER DDHA")
-    (ucs		. #x0AA2)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AA2)	; ઢ
     ))
 (define-char
   '((name		. "GUJARATI LETTER NNA")
-    (ucs		. #x0AA3)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AA3)	; ણ
     ))
 (define-char
   '((name		. "GUJARATI LETTER TA")
-    (ucs		. #x0AA4)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AA4)	; ત
     ))
 (define-char
   '((name		. "GUJARATI LETTER THA")
-    (ucs		. #x0AA5)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AA5)	; થ
     ))
 (define-char
   '((name		. "GUJARATI LETTER DA")
-    (ucs		. #x0AA6)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AA6)	; દ
     ))
 (define-char
   '((name		. "GUJARATI LETTER DHA")
-    (ucs		. #x0AA7)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AA7)	; ધ
     ))
 (define-char
   '((name		. "GUJARATI LETTER NA")
-    (ucs		. #x0AA8)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AA8)	; ન
     ))
 (define-char
   '((name		. "GUJARATI LETTER PA")
-    (ucs		. #x0AAA)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AAA)	; પ
     ))
 (define-char
   '((name		. "GUJARATI LETTER PHA")
-    (ucs		. #x0AAB)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AAB)	; ફ
     ))
 (define-char
   '((name		. "GUJARATI LETTER BA")
-    (ucs		. #x0AAC)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AAC)	; બ
     ))
 (define-char
   '((name		. "GUJARATI LETTER BHA")
-    (ucs		. #x0AAD)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AAD)	; ભ
     ))
 (define-char
   '((name		. "GUJARATI LETTER MA")
-    (ucs		. #x0AAE)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AAE)	; મ
     ))
 (define-char
   '((name		. "GUJARATI LETTER YA")
-    (ucs		. #x0AAF)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AAF)	; ય
     ))
 (define-char
   '((name		. "GUJARATI LETTER RA")
-    (ucs		. #x0AB0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AB0)	; ર
     ))
 (define-char
   '((name		. "GUJARATI LETTER LA")
-    (ucs		. #x0AB2)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AB2)	; લ
     ))
 (define-char
   '((name		. "GUJARATI LETTER LLA")
-    (ucs		. #x0AB3)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AB3)	; ળ
     ))
 (define-char
   '((name		. "GUJARATI LETTER VA")
-    (ucs		. #x0AB5)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AB5)	; વ
     ))
 (define-char
   '((name		. "GUJARATI LETTER SHA")
-    (ucs		. #x0AB6)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AB6)	; શ
     ))
 (define-char
   '((name		. "GUJARATI LETTER SSA")
-    (ucs		. #x0AB7)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AB7)	; ષ
     ))
 (define-char
   '((name		. "GUJARATI LETTER SA")
-    (ucs		. #x0AB8)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AB8)	; સ
     ))
 (define-char
   '((name		. "GUJARATI LETTER HA")
-    (ucs		. #x0AB9)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AB9)	; હ
     ))
 (define-char
   '((name		. "GUJARATI SIGN NUKTA")
-    (ucs		. #x0ABC)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0ABC)	; ઼
     ))
 (define-char
   '((name		. "GUJARATI SIGN AVAGRAHA")
-    (ucs		. #x0ABD)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0ABD)	; ઽ
     ))
 (define-char
   '((name		. "GUJARATI VOWEL SIGN AA")
-    (ucs		. #x0ABE)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0ABE)	; ા
     ))
 (define-char
   '((name		. "GUJARATI VOWEL SIGN I")
-    (ucs		. #x0ABF)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0ABF)	; િ
     ))
 (define-char
   '((name		. "GUJARATI VOWEL SIGN II")
-    (ucs		. #x0AC0)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AC0)	; ી
     ))
 (define-char
   '((name		. "GUJARATI VOWEL SIGN U")
-    (ucs		. #x0AC1)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0AC1)	; ુ
     ))
 (define-char
   '((name		. "GUJARATI VOWEL SIGN UU")
-    (ucs		. #x0AC2)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0AC2)	; ૂ
     ))
 (define-char
   '((name		. "GUJARATI VOWEL SIGN VOCALIC R")
-    (ucs		. #x0AC3)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0AC3)	; ૃ
     ))
 (define-char
   '((name		. "GUJARATI VOWEL SIGN VOCALIC RR")
-    (ucs		. #x0AC4)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0AC4)	; ૄ
     ))
 (define-char
   '((name		. "GUJARATI VOWEL SIGN CANDRA E")
-    (ucs		. #x0AC5)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0AC5)	; ૅ
     ))
 (define-char
   '((name		. "GUJARATI VOWEL SIGN E")
-    (ucs		. #x0AC7)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0AC7)	; ે
     ))
 (define-char
   '((name		. "GUJARATI VOWEL SIGN AI")
-    (ucs		. #x0AC8)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0AC8)	; ૈ
     ))
 (define-char
   '((name		. "GUJARATI VOWEL SIGN CANDRA O")
-    (ucs		. #x0AC9)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AC9)	; ૉ
     ))
 (define-char
   '((name		. "GUJARATI VOWEL SIGN O")
-    (ucs		. #x0ACB)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0ACB)	; ો
     ))
 (define-char
   '((name		. "GUJARATI VOWEL SIGN AU")
-    (ucs		. #x0ACC)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0ACC)	; ૌ
     ))
 (define-char
   '((name		. "GUJARATI SIGN VIRAMA")
-    (ucs		. #x0ACD)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0ACD)	; ્
     ))
 (define-char
   '((name		. "GUJARATI OM")
-    (ucs		. #x0AD0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AD0)	; ૐ
     ))
 (define-char
   '((name		. "GUJARATI LETTER VOCALIC RR")
-    (ucs		. #x0AE0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0AE0)	; ૠ
     ))
 (define-char
   '((name		. "GUJARATI DIGIT ZERO")
-    (ucs		. #x0AE6)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 0)
-    (digit-value	. 0)
-    (numeric-value	. 0)
     (mirrored		. nil)
+    (decimal-digit-value . 0)
+    (digit-value	 . 0)
+    (numeric-value	 . 0)
+    (ucs		. #x0AE6)	; ૦
     ))
 (define-char
   '((name		. "GUJARATI DIGIT ONE")
-    (ucs		. #x0AE7)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 1)
-    (digit-value	. 1)
-    (numeric-value	. 1)
     (mirrored		. nil)
+    (decimal-digit-value . 1)
+    (digit-value	 . 1)
+    (numeric-value	 . 1)
+    (ucs		. #x0AE7)	; ૧
     ))
 (define-char
   '((name		. "GUJARATI DIGIT TWO")
-    (ucs		. #x0AE8)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 2)
-    (digit-value	. 2)
-    (numeric-value	. 2)
     (mirrored		. nil)
+    (decimal-digit-value . 2)
+    (digit-value	 . 2)
+    (numeric-value	 . 2)
+    (ucs		. #x0AE8)	; ૨
     ))
 (define-char
   '((name		. "GUJARATI DIGIT THREE")
-    (ucs		. #x0AE9)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 3)
-    (digit-value	. 3)
-    (numeric-value	. 3)
     (mirrored		. nil)
+    (decimal-digit-value . 3)
+    (digit-value	 . 3)
+    (numeric-value	 . 3)
+    (ucs		. #x0AE9)	; ૩
     ))
 (define-char
   '((name		. "GUJARATI DIGIT FOUR")
-    (ucs		. #x0AEA)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 4)
-    (digit-value	. 4)
-    (numeric-value	. 4)
     (mirrored		. nil)
+    (decimal-digit-value . 4)
+    (digit-value	 . 4)
+    (numeric-value	 . 4)
+    (ucs		. #x0AEA)	; ૪
     ))
 (define-char
   '((name		. "GUJARATI DIGIT FIVE")
-    (ucs		. #x0AEB)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 5)
-    (digit-value	. 5)
-    (numeric-value	. 5)
     (mirrored		. nil)
+    (decimal-digit-value . 5)
+    (digit-value	 . 5)
+    (numeric-value	 . 5)
+    (ucs		. #x0AEB)	; ૫
     ))
 (define-char
   '((name		. "GUJARATI DIGIT SIX")
-    (ucs		. #x0AEC)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 6)
-    (digit-value	. 6)
-    (numeric-value	. 6)
     (mirrored		. nil)
+    (decimal-digit-value . 6)
+    (digit-value	 . 6)
+    (numeric-value	 . 6)
+    (ucs		. #x0AEC)	; ૬
     ))
 (define-char
   '((name		. "GUJARATI DIGIT SEVEN")
-    (ucs		. #x0AED)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 7)
-    (digit-value	. 7)
-    (numeric-value	. 7)
     (mirrored		. nil)
+    (decimal-digit-value . 7)
+    (digit-value	 . 7)
+    (numeric-value	 . 7)
+    (ucs		. #x0AED)	; ૭
     ))
 (define-char
   '((name		. "GUJARATI DIGIT EIGHT")
-    (ucs		. #x0AEE)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 8)
-    (digit-value	. 8)
-    (numeric-value	. 8)
     (mirrored		. nil)
+    (decimal-digit-value . 8)
+    (digit-value	 . 8)
+    (numeric-value	 . 8)
+    (ucs		. #x0AEE)	; ૮
     ))
 (define-char
   '((name		. "GUJARATI DIGIT NINE")
-    (ucs		. #x0AEF)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 9)
-    (digit-value	. 9)
-    (numeric-value	. 9)
     (mirrored		. nil)
+    (decimal-digit-value . 9)
+    (digit-value	 . 9)
+    (numeric-value	 . 9)
+    (ucs		. #x0AEF)	; ૯
     ))
