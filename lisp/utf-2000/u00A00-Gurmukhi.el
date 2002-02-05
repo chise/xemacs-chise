2d29cd624f50220cde4201c5bfc00eaa1e8ed97d
tomo <tomo>
2002-02-05 17:27:27 +0000

    Reformatted.

--- lisp/utf-2000/u00A00-Gurmukhi.el
+++ lisp/utf-2000/u00A00-Gurmukhi.el
@@ -1,561 +1,561 @@
 (define-char
   '((name		. "GURMUKHI SIGN BINDI")
-    (ucs		. #x0A02)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0A02)	; ਂ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER A")
-    (ucs		. #x0A05)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A05)	; ਅ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER AA")
-    (ucs		. #x0A06)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A06)	; ਆ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER I")
-    (ucs		. #x0A07)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A07)	; ਇ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER II")
-    (ucs		. #x0A08)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A08)	; ਈ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER U")
-    (ucs		. #x0A09)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A09)	; ਉ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER UU")
-    (ucs		. #x0A0A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A0A)	; ਊ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER EE")
-    (ucs		. #x0A0F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A0F)	; ਏ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER AI")
-    (ucs		. #x0A10)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A10)	; ਐ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER OO")
-    (ucs		. #x0A13)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A13)	; ਓ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER AU")
-    (ucs		. #x0A14)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A14)	; ਔ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER KA")
-    (ucs		. #x0A15)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A15)	; ਕ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER KHA")
-    (ucs		. #x0A16)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A16)	; ਖ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER GA")
-    (ucs		. #x0A17)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A17)	; ਗ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER GHA")
-    (ucs		. #x0A18)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A18)	; ਘ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER NGA")
-    (ucs		. #x0A19)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A19)	; ਙ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER CA")
-    (ucs		. #x0A1A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A1A)	; ਚ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER CHA")
-    (ucs		. #x0A1B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A1B)	; ਛ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER JA")
-    (ucs		. #x0A1C)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A1C)	; ਜ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER JHA")
-    (ucs		. #x0A1D)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A1D)	; ਝ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER NYA")
-    (ucs		. #x0A1E)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A1E)	; ਞ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER TTA")
-    (ucs		. #x0A1F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A1F)	; ਟ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER TTHA")
-    (ucs		. #x0A20)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A20)	; ਠ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER DDA")
-    (ucs		. #x0A21)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A21)	; ਡ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER DDHA")
-    (ucs		. #x0A22)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A22)	; ਢ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER NNA")
-    (ucs		. #x0A23)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A23)	; ਣ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER TA")
-    (ucs		. #x0A24)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A24)	; ਤ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER THA")
-    (ucs		. #x0A25)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A25)	; ਥ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER DA")
-    (ucs		. #x0A26)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A26)	; ਦ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER DHA")
-    (ucs		. #x0A27)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A27)	; ਧ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER NA")
-    (ucs		. #x0A28)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A28)	; ਨ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER PA")
-    (ucs		. #x0A2A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A2A)	; ਪ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER PHA")
-    (ucs		. #x0A2B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A2B)	; ਫ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER BA")
-    (ucs		. #x0A2C)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A2C)	; ਬ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER BHA")
-    (ucs		. #x0A2D)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A2D)	; ਭ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER MA")
-    (ucs		. #x0A2E)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A2E)	; ਮ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER YA")
-    (ucs		. #x0A2F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A2F)	; ਯ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER RA")
-    (ucs		. #x0A30)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A30)	; ਰ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER LA")
-    (ucs		. #x0A32)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A32)	; ਲ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER LLA")
-    (ucs		. #x0A33)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
-    (->decomposition	#x0A32 #x0A3C)
     (mirrored		. nil)
+    (->decomposition	#x0A32 #x0A3C)
+    (ucs		. #x0A33)	; ਲ਼
     ))
 (define-char
   '((name		. "GURMUKHI LETTER VA")
-    (ucs		. #x0A35)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A35)	; ਵ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER SHA")
-    (ucs		. #x0A36)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
-    (->decomposition	#x0A38 #x0A3C)
     (mirrored		. nil)
+    (->decomposition	#x0A38 #x0A3C)
+    (ucs		. #x0A36)	; ਸ਼
     ))
 (define-char
   '((name		. "GURMUKHI LETTER SA")
-    (ucs		. #x0A38)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A38)	; ਸ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER HA")
-    (ucs		. #x0A39)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A39)	; ਹ
     ))
 (define-char
   '((name		. "GURMUKHI SIGN NUKTA")
-    (ucs		. #x0A3C)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0A3C)	; ਼
     ))
 (define-char
   '((name		. "GURMUKHI VOWEL SIGN AA")
-    (ucs		. #x0A3E)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A3E)	; ਾ
     ))
 (define-char
   '((name		. "GURMUKHI VOWEL SIGN I")
-    (ucs		. #x0A3F)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A3F)	; ਿ
     ))
 (define-char
   '((name		. "GURMUKHI VOWEL SIGN II")
-    (ucs		. #x0A40)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A40)	; ੀ
     ))
 (define-char
   '((name		. "GURMUKHI VOWEL SIGN U")
-    (ucs		. #x0A41)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0A41)	; ੁ
     ))
 (define-char
   '((name		. "GURMUKHI VOWEL SIGN UU")
-    (ucs		. #x0A42)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0A42)	; ੂ
     ))
 (define-char
   '((name		. "GURMUKHI VOWEL SIGN EE")
-    (ucs		. #x0A47)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0A47)	; ੇ
     ))
 (define-char
   '((name		. "GURMUKHI VOWEL SIGN AI")
-    (ucs		. #x0A48)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0A48)	; ੈ
     ))
 (define-char
   '((name		. "GURMUKHI VOWEL SIGN OO")
-    (ucs		. #x0A4B)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0A4B)	; ੋ
     ))
 (define-char
   '((name		. "GURMUKHI VOWEL SIGN AU")
-    (ucs		. #x0A4C)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0A4C)	; ੌ
     ))
 (define-char
   '((name		. "GURMUKHI SIGN VIRAMA")
-    (ucs		. #x0A4D)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0A4D)	; ੍
     ))
 (define-char
   '((name		. "GURMUKHI LETTER KHHA")
-    (ucs		. #x0A59)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
-    (->decomposition	#x0A16 #x0A3C)
     (mirrored		. nil)
+    (->decomposition	#x0A16 #x0A3C)
+    (ucs		. #x0A59)	; ਖ਼
     ))
 (define-char
   '((name		. "GURMUKHI LETTER GHHA")
-    (ucs		. #x0A5A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
-    (->decomposition	#x0A17 #x0A3C)
     (mirrored		. nil)
+    (->decomposition	#x0A17 #x0A3C)
+    (ucs		. #x0A5A)	; ਗ਼
     ))
 (define-char
   '((name		. "GURMUKHI LETTER ZA")
-    (ucs		. #x0A5B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
-    (->decomposition	#x0A1C #x0A3C)
     (mirrored		. nil)
+    (->decomposition	#x0A1C #x0A3C)
+    (ucs		. #x0A5B)	; ਜ਼
     ))
 (define-char
   '((name		. "GURMUKHI LETTER RRA")
-    (ucs		. #x0A5C)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A5C)	; ੜ
     ))
 (define-char
   '((name		. "GURMUKHI LETTER FA")
-    (ucs		. #x0A5E)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
-    (->decomposition	#x0A2B #x0A3C)
     (mirrored		. nil)
+    (->decomposition	#x0A2B #x0A3C)
+    (ucs		. #x0A5E)	; ਫ਼
     ))
 (define-char
   '((name		. "GURMUKHI DIGIT ZERO")
-    (ucs		. #x0A66)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 0)
-    (digit-value	. 0)
-    (numeric-value	. 0)
     (mirrored		. nil)
+    (decimal-digit-value . 0)
+    (digit-value	 . 0)
+    (numeric-value	 . 0)
+    (ucs		. #x0A66)	; ੦
     ))
 (define-char
   '((name		. "GURMUKHI DIGIT ONE")
-    (ucs		. #x0A67)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 1)
-    (digit-value	. 1)
-    (numeric-value	. 1)
     (mirrored		. nil)
+    (decimal-digit-value . 1)
+    (digit-value	 . 1)
+    (numeric-value	 . 1)
+    (ucs		. #x0A67)	; ੧
     ))
 (define-char
   '((name		. "GURMUKHI DIGIT TWO")
-    (ucs		. #x0A68)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 2)
-    (digit-value	. 2)
-    (numeric-value	. 2)
     (mirrored		. nil)
+    (decimal-digit-value . 2)
+    (digit-value	 . 2)
+    (numeric-value	 . 2)
+    (ucs		. #x0A68)	; ੨
     ))
 (define-char
   '((name		. "GURMUKHI DIGIT THREE")
-    (ucs		. #x0A69)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 3)
-    (digit-value	. 3)
-    (numeric-value	. 3)
     (mirrored		. nil)
+    (decimal-digit-value . 3)
+    (digit-value	 . 3)
+    (numeric-value	 . 3)
+    (ucs		. #x0A69)	; ੩
     ))
 (define-char
   '((name		. "GURMUKHI DIGIT FOUR")
-    (ucs		. #x0A6A)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 4)
-    (digit-value	. 4)
-    (numeric-value	. 4)
     (mirrored		. nil)
+    (decimal-digit-value . 4)
+    (digit-value	 . 4)
+    (numeric-value	 . 4)
+    (ucs		. #x0A6A)	; ੪
     ))
 (define-char
   '((name		. "GURMUKHI DIGIT FIVE")
-    (ucs		. #x0A6B)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 5)
-    (digit-value	. 5)
-    (numeric-value	. 5)
     (mirrored		. nil)
+    (decimal-digit-value . 5)
+    (digit-value	 . 5)
+    (numeric-value	 . 5)
+    (ucs		. #x0A6B)	; ੫
     ))
 (define-char
   '((name		. "GURMUKHI DIGIT SIX")
-    (ucs		. #x0A6C)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 6)
-    (digit-value	. 6)
-    (numeric-value	. 6)
     (mirrored		. nil)
+    (decimal-digit-value . 6)
+    (digit-value	 . 6)
+    (numeric-value	 . 6)
+    (ucs		. #x0A6C)	; ੬
     ))
 (define-char
   '((name		. "GURMUKHI DIGIT SEVEN")
-    (ucs		. #x0A6D)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 7)
-    (digit-value	. 7)
-    (numeric-value	. 7)
     (mirrored		. nil)
+    (decimal-digit-value . 7)
+    (digit-value	 . 7)
+    (numeric-value	 . 7)
+    (ucs		. #x0A6D)	; ੭
     ))
 (define-char
   '((name		. "GURMUKHI DIGIT EIGHT")
-    (ucs		. #x0A6E)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 8)
-    (digit-value	. 8)
-    (numeric-value	. 8)
     (mirrored		. nil)
+    (decimal-digit-value . 8)
+    (digit-value	 . 8)
+    (numeric-value	 . 8)
+    (ucs		. #x0A6E)	; ੮
     ))
 (define-char
   '((name		. "GURMUKHI DIGIT NINE")
-    (ucs		. #x0A6F)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 9)
-    (digit-value	. 9)
-    (numeric-value	. 9)
     (mirrored		. nil)
+    (decimal-digit-value . 9)
+    (digit-value	 . 9)
+    (numeric-value	 . 9)
+    (ucs		. #x0A6F)	; ੯
     ))
 (define-char
   '((name		. "GURMUKHI TIPPI")
-    (ucs		. #x0A70)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0A70)	; ੰ
     ))
 (define-char
   '((name		. "GURMUKHI ADDAK")
-    (ucs		. #x0A71)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0A71)	; ੱ
     ))
 (define-char
   '((name		. "GURMUKHI IRI")
-    (ucs		. #x0A72)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A72)	; ੲ
     ))
 (define-char
   '((name		. "GURMUKHI URA")
-    (ucs		. #x0A73)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A73)	; ੳ
     ))
 (define-char
   '((name		. "GURMUKHI EK ONKAR")
-    (ucs		. #x0A74)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0A74)	; ੴ
     ))
