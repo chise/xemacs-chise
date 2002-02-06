cb9e23a8a478d24a9ac2c6900db5fa2ae166cf8a
tomo <tomo>
2002-02-06 08:48:50 +0000

    Reformatted.

--- lisp/utf-2000/u00C80-Kannada.el
+++ lisp/utf-2000/u00C80-Kannada.el
@@ -1,595 +1,595 @@
 (define-char
   '((name		. "KANNADA SIGN ANUSVARA")
-    (ucs		. #x0C82)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C82)	; ಂ
     ))
 (define-char
   '((name		. "KANNADA SIGN VISARGA")
-    (ucs		. #x0C83)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C83)	; ಃ
     ))
 (define-char
   '((name		. "KANNADA LETTER A")
-    (ucs		. #x0C85)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C85)	; ಅ
     ))
 (define-char
   '((name		. "KANNADA LETTER AA")
-    (ucs		. #x0C86)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C86)	; ಆ
     ))
 (define-char
   '((name		. "KANNADA LETTER I")
-    (ucs		. #x0C87)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C87)	; ಇ
     ))
 (define-char
   '((name		. "KANNADA LETTER II")
-    (ucs		. #x0C88)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C88)	; ಈ
     ))
 (define-char
   '((name		. "KANNADA LETTER U")
-    (ucs		. #x0C89)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C89)	; ಉ
     ))
 (define-char
   '((name		. "KANNADA LETTER UU")
-    (ucs		. #x0C8A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C8A)	; ಊ
     ))
 (define-char
   '((name		. "KANNADA LETTER VOCALIC R")
-    (ucs		. #x0C8B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C8B)	; ಋ
     ))
 (define-char
   '((name		. "KANNADA LETTER VOCALIC L")
-    (ucs		. #x0C8C)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C8C)	; ಌ
     ))
 (define-char
   '((name		. "KANNADA LETTER E")
-    (ucs		. #x0C8E)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C8E)	; ಎ
     ))
 (define-char
   '((name		. "KANNADA LETTER EE")
-    (ucs		. #x0C8F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C8F)	; ಏ
     ))
 (define-char
   '((name		. "KANNADA LETTER AI")
-    (ucs		. #x0C90)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C90)	; ಐ
     ))
 (define-char
   '((name		. "KANNADA LETTER O")
-    (ucs		. #x0C92)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C92)	; ಒ
     ))
 (define-char
   '((name		. "KANNADA LETTER OO")
-    (ucs		. #x0C93)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C93)	; ಓ
     ))
 (define-char
   '((name		. "KANNADA LETTER AU")
-    (ucs		. #x0C94)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C94)	; ಔ
     ))
 (define-char
   '((name		. "KANNADA LETTER KA")
-    (ucs		. #x0C95)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C95)	; ಕ
     ))
 (define-char
   '((name		. "KANNADA LETTER KHA")
-    (ucs		. #x0C96)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C96)	; ಖ
     ))
 (define-char
   '((name		. "KANNADA LETTER GA")
-    (ucs		. #x0C97)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C97)	; ಗ
     ))
 (define-char
   '((name		. "KANNADA LETTER GHA")
-    (ucs		. #x0C98)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C98)	; ಘ
     ))
 (define-char
   '((name		. "KANNADA LETTER NGA")
-    (ucs		. #x0C99)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C99)	; ಙ
     ))
 (define-char
   '((name		. "KANNADA LETTER CA")
-    (ucs		. #x0C9A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C9A)	; ಚ
     ))
 (define-char
   '((name		. "KANNADA LETTER CHA")
-    (ucs		. #x0C9B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C9B)	; ಛ
     ))
 (define-char
   '((name		. "KANNADA LETTER JA")
-    (ucs		. #x0C9C)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C9C)	; ಜ
     ))
 (define-char
   '((name		. "KANNADA LETTER JHA")
-    (ucs		. #x0C9D)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C9D)	; ಝ
     ))
 (define-char
   '((name		. "KANNADA LETTER NYA")
-    (ucs		. #x0C9E)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C9E)	; ಞ
     ))
 (define-char
   '((name		. "KANNADA LETTER TTA")
-    (ucs		. #x0C9F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C9F)	; ಟ
     ))
 (define-char
   '((name		. "KANNADA LETTER TTHA")
-    (ucs		. #x0CA0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CA0)	; ಠ
     ))
 (define-char
   '((name		. "KANNADA LETTER DDA")
-    (ucs		. #x0CA1)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CA1)	; ಡ
     ))
 (define-char
   '((name		. "KANNADA LETTER DDHA")
-    (ucs		. #x0CA2)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CA2)	; ಢ
     ))
 (define-char
   '((name		. "KANNADA LETTER NNA")
-    (ucs		. #x0CA3)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CA3)	; ಣ
     ))
 (define-char
   '((name		. "KANNADA LETTER TA")
-    (ucs		. #x0CA4)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CA4)	; ತ
     ))
 (define-char
   '((name		. "KANNADA LETTER THA")
-    (ucs		. #x0CA5)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CA5)	; ಥ
     ))
 (define-char
   '((name		. "KANNADA LETTER DA")
-    (ucs		. #x0CA6)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CA6)	; ದ
     ))
 (define-char
   '((name		. "KANNADA LETTER DHA")
-    (ucs		. #x0CA7)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CA7)	; ಧ
     ))
 (define-char
   '((name		. "KANNADA LETTER NA")
-    (ucs		. #x0CA8)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CA8)	; ನ
     ))
 (define-char
   '((name		. "KANNADA LETTER PA")
-    (ucs		. #x0CAA)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CAA)	; ಪ
     ))
 (define-char
   '((name		. "KANNADA LETTER PHA")
-    (ucs		. #x0CAB)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CAB)	; ಫ
     ))
 (define-char
   '((name		. "KANNADA LETTER BA")
-    (ucs		. #x0CAC)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CAC)	; ಬ
     ))
 (define-char
   '((name		. "KANNADA LETTER BHA")
-    (ucs		. #x0CAD)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CAD)	; ಭ
     ))
 (define-char
   '((name		. "KANNADA LETTER MA")
-    (ucs		. #x0CAE)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CAE)	; ಮ
     ))
 (define-char
   '((name		. "KANNADA LETTER YA")
-    (ucs		. #x0CAF)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CAF)	; ಯ
     ))
 (define-char
   '((name		. "KANNADA LETTER RA")
-    (ucs		. #x0CB0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CB0)	; ರ
     ))
 (define-char
   '((name		. "KANNADA LETTER RRA")
-    (ucs		. #x0CB1)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CB1)	; ಱ
     ))
 (define-char
   '((name		. "KANNADA LETTER LA")
-    (ucs		. #x0CB2)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CB2)	; ಲ
     ))
 (define-char
   '((name		. "KANNADA LETTER LLA")
-    (ucs		. #x0CB3)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CB3)	; ಳ
     ))
 (define-char
   '((name		. "KANNADA LETTER VA")
-    (ucs		. #x0CB5)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CB5)	; ವ
     ))
 (define-char
   '((name		. "KANNADA LETTER SHA")
-    (ucs		. #x0CB6)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CB6)	; ಶ
     ))
 (define-char
   '((name		. "KANNADA LETTER SSA")
-    (ucs		. #x0CB7)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CB7)	; ಷ
     ))
 (define-char
   '((name		. "KANNADA LETTER SA")
-    (ucs		. #x0CB8)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CB8)	; ಸ
     ))
 (define-char
   '((name		. "KANNADA LETTER HA")
-    (ucs		. #x0CB9)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CB9)	; ಹ
     ))
 (define-char
   '((name		. "KANNADA VOWEL SIGN AA")
-    (ucs		. #x0CBE)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CBE)	; ಾ
     ))
 (define-char
   '((name		. "KANNADA VOWEL SIGN I")
-    (ucs		. #x0CBF)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0CBF)	; ಿ
     ))
 (define-char
   '((name		. "KANNADA VOWEL SIGN II")
-    (ucs		. #x0CC0)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	#x0CBF #x0CD5)
     (mirrored		. nil)
+    (->decomposition	#x0CBF #x0CD5)
+    (ucs		. #x0CC0)	; ೀ
     ))
 (define-char
   '((name		. "KANNADA VOWEL SIGN U")
-    (ucs		. #x0CC1)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CC1)	; ು
     ))
 (define-char
   '((name		. "KANNADA VOWEL SIGN UU")
-    (ucs		. #x0CC2)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CC2)	; ೂ
     ))
 (define-char
   '((name		. "KANNADA VOWEL SIGN VOCALIC R")
-    (ucs		. #x0CC3)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CC3)	; ೃ
     ))
 (define-char
   '((name		. "KANNADA VOWEL SIGN VOCALIC RR")
-    (ucs		. #x0CC4)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CC4)	; ೄ
     ))
 (define-char
   '((name		. "KANNADA VOWEL SIGN E")
-    (ucs		. #x0CC6)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0CC6)	; ೆ
     ))
 (define-char
   '((name		. "KANNADA VOWEL SIGN EE")
-    (ucs		. #x0CC7)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	#x0CC6 #x0CD5)
     (mirrored		. nil)
+    (->decomposition	#x0CC6 #x0CD5)
+    (ucs		. #x0CC7)	; ೇ
     ))
 (define-char
   '((name		. "KANNADA VOWEL SIGN AI")
-    (ucs		. #x0CC8)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	#x0CC6 #x0CD6)
     (mirrored		. nil)
+    (->decomposition	#x0CC6 #x0CD6)
+    (ucs		. #x0CC8)	; ೈ
     ))
 (define-char
   '((name		. "KANNADA VOWEL SIGN O")
-    (ucs		. #x0CCA)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	#x0CC6 #x0CC2)
     (mirrored		. nil)
+    (->decomposition	#x0CC6 #x0CC2)
+    (ucs		. #x0CCA)	; ೊ
     ))
 (define-char
   '((name		. "KANNADA VOWEL SIGN OO")
-    (ucs		. #x0CCB)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
-    (->decomposition	#x0CCA #x0CD5)
     (mirrored		. nil)
+    (->decomposition	#x0CCA #x0CD5)
+    (ucs		. #x0CCB)	; ೋ
     ))
 (define-char
   '((name		. "KANNADA VOWEL SIGN AU")
-    (ucs		. #x0CCC)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0CCC)	; ೌ
     ))
 (define-char
   '((name		. "KANNADA SIGN VIRAMA")
-    (ucs		. #x0CCD)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0CCD)	; ್
     ))
 (define-char
   '((name		. "KANNADA LENGTH MARK")
-    (ucs		. #x0CD5)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CD5)	; ೕ
     ))
 (define-char
   '((name		. "KANNADA AI LENGTH MARK")
-    (ucs		. #x0CD6)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CD6)	; ೖ
     ))
 (define-char
   '((name		. "KANNADA LETTER FA")
-    (ucs		. #x0CDE)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CDE)	; ೞ
     ))
 (define-char
   '((name		. "KANNADA LETTER VOCALIC RR")
-    (ucs		. #x0CE0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CE0)	; ೠ
     ))
 (define-char
   '((name		. "KANNADA LETTER VOCALIC LL")
-    (ucs		. #x0CE1)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0CE1)	; ೡ
     ))
 (define-char
   '((name		. "KANNADA DIGIT ZERO")
-    (ucs		. #x0CE6)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 0)
-    (digit-value	. 0)
-    (numeric-value	. 0)
     (mirrored		. nil)
+    (decimal-digit-value . 0)
+    (digit-value	 . 0)
+    (numeric-value	 . 0)
+    (ucs		. #x0CE6)	; ೦
     ))
 (define-char
   '((name		. "KANNADA DIGIT ONE")
-    (ucs		. #x0CE7)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 1)
-    (digit-value	. 1)
-    (numeric-value	. 1)
     (mirrored		. nil)
+    (decimal-digit-value . 1)
+    (digit-value	 . 1)
+    (numeric-value	 . 1)
+    (ucs		. #x0CE7)	; ೧
     ))
 (define-char
   '((name		. "KANNADA DIGIT TWO")
-    (ucs		. #x0CE8)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 2)
-    (digit-value	. 2)
-    (numeric-value	. 2)
     (mirrored		. nil)
+    (decimal-digit-value . 2)
+    (digit-value	 . 2)
+    (numeric-value	 . 2)
+    (ucs		. #x0CE8)	; ೨
     ))
 (define-char
   '((name		. "KANNADA DIGIT THREE")
-    (ucs		. #x0CE9)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 3)
-    (digit-value	. 3)
-    (numeric-value	. 3)
     (mirrored		. nil)
+    (decimal-digit-value . 3)
+    (digit-value	 . 3)
+    (numeric-value	 . 3)
+    (ucs		. #x0CE9)	; ೩
     ))
 (define-char
   '((name		. "KANNADA DIGIT FOUR")
-    (ucs		. #x0CEA)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 4)
-    (digit-value	. 4)
-    (numeric-value	. 4)
     (mirrored		. nil)
+    (decimal-digit-value . 4)
+    (digit-value	 . 4)
+    (numeric-value	 . 4)
+    (ucs		. #x0CEA)	; ೪
     ))
 (define-char
   '((name		. "KANNADA DIGIT FIVE")
-    (ucs		. #x0CEB)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 5)
-    (digit-value	. 5)
-    (numeric-value	. 5)
     (mirrored		. nil)
+    (decimal-digit-value . 5)
+    (digit-value	 . 5)
+    (numeric-value	 . 5)
+    (ucs		. #x0CEB)	; ೫
     ))
 (define-char
   '((name		. "KANNADA DIGIT SIX")
-    (ucs		. #x0CEC)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 6)
-    (digit-value	. 6)
-    (numeric-value	. 6)
     (mirrored		. nil)
+    (decimal-digit-value . 6)
+    (digit-value	 . 6)
+    (numeric-value	 . 6)
+    (ucs		. #x0CEC)	; ೬
     ))
 (define-char
   '((name		. "KANNADA DIGIT SEVEN")
-    (ucs		. #x0CED)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 7)
-    (digit-value	. 7)
-    (numeric-value	. 7)
     (mirrored		. nil)
+    (decimal-digit-value . 7)
+    (digit-value	 . 7)
+    (numeric-value	 . 7)
+    (ucs		. #x0CED)	; ೭
     ))
 (define-char
   '((name		. "KANNADA DIGIT EIGHT")
-    (ucs		. #x0CEE)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 8)
-    (digit-value	. 8)
-    (numeric-value	. 8)
     (mirrored		. nil)
+    (decimal-digit-value . 8)
+    (digit-value	 . 8)
+    (numeric-value	 . 8)
+    (ucs		. #x0CEE)	; ೮
     ))
 (define-char
   '((name		. "KANNADA DIGIT NINE")
-    (ucs		. #x0CEF)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 9)
-    (digit-value	. 9)
-    (numeric-value	. 9)
     (mirrored		. nil)
+    (decimal-digit-value . 9)
+    (digit-value	 . 9)
+    (numeric-value	 . 9)
+    (ucs		. #x0CEF)	; ೯
     ))
