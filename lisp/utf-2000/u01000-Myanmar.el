cabdee4162d481cbc39c073cbaf36d943038765c
tomo <tomo>
2002-02-13 10:04:59 +0000

    Reformatted.

--- lisp/utf-2000/u01000-Myanmar.el
+++ lisp/utf-2000/u01000-Myanmar.el
@@ -1,577 +1,577 @@
 (define-char
   '((name		. "MYANMAR LETTER KA")
-    (ucs		. #x1000)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1000)	; က
     ))
 (define-char
   '((name		. "MYANMAR LETTER KHA")
-    (ucs		. #x1001)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1001)	; ခ
     ))
 (define-char
   '((name		. "MYANMAR LETTER GA")
-    (ucs		. #x1002)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1002)	; ဂ
     ))
 (define-char
   '((name		. "MYANMAR LETTER GHA")
-    (ucs		. #x1003)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1003)	; ဃ
     ))
 (define-char
   '((name		. "MYANMAR LETTER NGA")
-    (ucs		. #x1004)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1004)	; င
     ))
 (define-char
   '((name		. "MYANMAR LETTER CA")
-    (ucs		. #x1005)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1005)	; စ
     ))
 (define-char
   '((name		. "MYANMAR LETTER CHA")
-    (ucs		. #x1006)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1006)	; ဆ
     ))
 (define-char
   '((name		. "MYANMAR LETTER JA")
-    (ucs		. #x1007)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1007)	; ဇ
     ))
 (define-char
   '((name		. "MYANMAR LETTER JHA")
-    (ucs		. #x1008)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1008)	; ဈ
     ))
 (define-char
   '((name		. "MYANMAR LETTER NYA")
-    (ucs		. #x1009)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1009)	; ဉ
     ))
 (define-char
   '((name		. "MYANMAR LETTER NNYA")
-    (ucs		. #x100A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x100A)	; ည
     ))
 (define-char
   '((name		. "MYANMAR LETTER TTA")
-    (ucs		. #x100B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x100B)	; ဋ
     ))
 (define-char
   '((name		. "MYANMAR LETTER TTHA")
-    (ucs		. #x100C)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x100C)	; ဌ
     ))
 (define-char
   '((name		. "MYANMAR LETTER DDA")
-    (ucs		. #x100D)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x100D)	; ဍ
     ))
 (define-char
   '((name		. "MYANMAR LETTER DDHA")
-    (ucs		. #x100E)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x100E)	; ဎ
     ))
 (define-char
   '((name		. "MYANMAR LETTER NNA")
-    (ucs		. #x100F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x100F)	; ဏ
     ))
 (define-char
   '((name		. "MYANMAR LETTER TA")
-    (ucs		. #x1010)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1010)	; တ
     ))
 (define-char
   '((name		. "MYANMAR LETTER THA")
-    (ucs		. #x1011)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1011)	; ထ
     ))
 (define-char
   '((name		. "MYANMAR LETTER DA")
-    (ucs		. #x1012)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1012)	; ဒ
     ))
 (define-char
   '((name		. "MYANMAR LETTER DHA")
-    (ucs		. #x1013)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1013)	; ဓ
     ))
 (define-char
   '((name		. "MYANMAR LETTER NA")
-    (ucs		. #x1014)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1014)	; န
     ))
 (define-char
   '((name		. "MYANMAR LETTER PA")
-    (ucs		. #x1015)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1015)	; ပ
     ))
 (define-char
   '((name		. "MYANMAR LETTER PHA")
-    (ucs		. #x1016)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1016)	; ဖ
     ))
 (define-char
   '((name		. "MYANMAR LETTER BA")
-    (ucs		. #x1017)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1017)	; ဗ
     ))
 (define-char
   '((name		. "MYANMAR LETTER BHA")
-    (ucs		. #x1018)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1018)	; ဘ
     ))
 (define-char
   '((name		. "MYANMAR LETTER MA")
-    (ucs		. #x1019)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1019)	; မ
     ))
 (define-char
   '((name		. "MYANMAR LETTER YA")
-    (ucs		. #x101A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x101A)	; ယ
     ))
 (define-char
   '((name		. "MYANMAR LETTER RA")
-    (ucs		. #x101B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x101B)	; ရ
     ))
 (define-char
   '((name		. "MYANMAR LETTER LA")
-    (ucs		. #x101C)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x101C)	; လ
     ))
 (define-char
   '((name		. "MYANMAR LETTER WA")
-    (ucs		. #x101D)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x101D)	; ဝ
     ))
 (define-char
   '((name		. "MYANMAR LETTER SA")
-    (ucs		. #x101E)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x101E)	; သ
     ))
 (define-char
   '((name		. "MYANMAR LETTER HA")
-    (ucs		. #x101F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x101F)	; ဟ
     ))
 (define-char
   '((name		. "MYANMAR LETTER LLA")
-    (ucs		. #x1020)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1020)	; ဠ
     ))
 (define-char
   '((name		. "MYANMAR LETTER A")
-    (ucs		. #x1021)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1021)	; အ
     ))
 (define-char
   '((name		. "MYANMAR LETTER I")
-    (ucs		. #x1023)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1023)	; ဣ
     ))
 (define-char
   '((name		. "MYANMAR LETTER II")
-    (ucs		. #x1024)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1024)	; ဤ
     ))
 (define-char
   '((name		. "MYANMAR LETTER U")
-    (ucs		. #x1025)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1025)	; ဥ
     ))
 (define-char
   '((name		. "MYANMAR LETTER UU")
-    (ucs		. #x1026)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
-    (->decomposition	#x1025 #x102E)
     (mirrored		. nil)
+    (->decomposition	#x1025 #x102E)
+    (ucs		. #x1026)	; ဦ
     ))
 (define-char
   '((name		. "MYANMAR LETTER E")
-    (ucs		. #x1027)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1027)	; ဧ
     ))
 (define-char
   '((name		. "MYANMAR LETTER O")
-    (ucs		. #x1029)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1029)	; ဩ
     ))
 (define-char
   '((name		. "MYANMAR LETTER AU")
-    (ucs		. #x102A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x102A)	; ဪ
     ))
 (define-char
   '((name		. "MYANMAR VOWEL SIGN AA")
-    (ucs		. #x102C)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x102C)	; ာ
     ))
 (define-char
   '((name		. "MYANMAR VOWEL SIGN I")
-    (ucs		. #x102D)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x102D)	; ိ
     ))
 (define-char
   '((name		. "MYANMAR VOWEL SIGN II")
-    (ucs		. #x102E)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x102E)	; ီ
     ))
 (define-char
   '((name		. "MYANMAR VOWEL SIGN U")
-    (ucs		. #x102F)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x102F)	; ု
     ))
 (define-char
   '((name		. "MYANMAR VOWEL SIGN UU")
-    (ucs		. #x1030)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x1030)	; ူ
     ))
 (define-char
   '((name		. "MYANMAR VOWEL SIGN E")
-    (ucs		. #x1031)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1031)	; ေ
     ))
 (define-char
   '((name		. "MYANMAR VOWEL SIGN AI")
-    (ucs		. #x1032)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x1032)	; ဲ
     ))
 (define-char
   '((name		. "MYANMAR SIGN ANUSVARA")
-    (ucs		. #x1036)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x1036)	; ံ
     ))
 (define-char
   '((name		. "MYANMAR SIGN DOT BELOW")
-    (ucs		. #x1037)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x1037)	; ့
     ))
 (define-char
   '((name		. "MYANMAR SIGN VISARGA")
-    (ucs		. #x1038)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1038)	; း
     ))
 (define-char
   '((name		. "MYANMAR SIGN VIRAMA")
-    (ucs		. #x1039)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x1039)	; ္
     ))
 (define-char
   '((name		. "MYANMAR DIGIT ZERO")
-    (ucs		. #x1040)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 0)
-    (digit-value	. 0)
-    (numeric-value	. 0)
     (mirrored		. nil)
+    (decimal-digit-value . 0)
+    (digit-value	 . 0)
+    (numeric-value	 . 0)
+    (ucs		. #x1040)	; ၀
     ))
 (define-char
   '((name		. "MYANMAR DIGIT ONE")
-    (ucs		. #x1041)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 1)
-    (digit-value	. 1)
-    (numeric-value	. 1)
     (mirrored		. nil)
+    (decimal-digit-value . 1)
+    (digit-value	 . 1)
+    (numeric-value	 . 1)
+    (ucs		. #x1041)	; ၁
     ))
 (define-char
   '((name		. "MYANMAR DIGIT TWO")
-    (ucs		. #x1042)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 2)
-    (digit-value	. 2)
-    (numeric-value	. 2)
     (mirrored		. nil)
+    (decimal-digit-value . 2)
+    (digit-value	 . 2)
+    (numeric-value	 . 2)
+    (ucs		. #x1042)	; ၂
     ))
 (define-char
   '((name		. "MYANMAR DIGIT THREE")
-    (ucs		. #x1043)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 3)
-    (digit-value	. 3)
-    (numeric-value	. 3)
     (mirrored		. nil)
+    (decimal-digit-value . 3)
+    (digit-value	 . 3)
+    (numeric-value	 . 3)
+    (ucs		. #x1043)	; ၃
     ))
 (define-char
   '((name		. "MYANMAR DIGIT FOUR")
-    (ucs		. #x1044)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 4)
-    (digit-value	. 4)
-    (numeric-value	. 4)
     (mirrored		. nil)
+    (decimal-digit-value . 4)
+    (digit-value	 . 4)
+    (numeric-value	 . 4)
+    (ucs		. #x1044)	; ၄
     ))
 (define-char
   '((name		. "MYANMAR DIGIT FIVE")
-    (ucs		. #x1045)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 5)
-    (digit-value	. 5)
-    (numeric-value	. 5)
     (mirrored		. nil)
+    (decimal-digit-value . 5)
+    (digit-value	 . 5)
+    (numeric-value	 . 5)
+    (ucs		. #x1045)	; ၅
     ))
 (define-char
   '((name		. "MYANMAR DIGIT SIX")
-    (ucs		. #x1046)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 6)
-    (digit-value	. 6)
-    (numeric-value	. 6)
     (mirrored		. nil)
+    (decimal-digit-value . 6)
+    (digit-value	 . 6)
+    (numeric-value	 . 6)
+    (ucs		. #x1046)	; ၆
     ))
 (define-char
   '((name		. "MYANMAR DIGIT SEVEN")
-    (ucs		. #x1047)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 7)
-    (digit-value	. 7)
-    (numeric-value	. 7)
     (mirrored		. nil)
+    (decimal-digit-value . 7)
+    (digit-value	 . 7)
+    (numeric-value	 . 7)
+    (ucs		. #x1047)	; ၇
     ))
 (define-char
   '((name		. "MYANMAR DIGIT EIGHT")
-    (ucs		. #x1048)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 8)
-    (digit-value	. 8)
-    (numeric-value	. 8)
     (mirrored		. nil)
+    (decimal-digit-value . 8)
+    (digit-value	 . 8)
+    (numeric-value	 . 8)
+    (ucs		. #x1048)	; ၈
     ))
 (define-char
   '((name		. "MYANMAR DIGIT NINE")
-    (ucs		. #x1049)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 9)
-    (digit-value	. 9)
-    (numeric-value	. 9)
     (mirrored		. nil)
+    (decimal-digit-value . 9)
+    (digit-value	 . 9)
+    (numeric-value	 . 9)
+    (ucs		. #x1049)	; ၉
     ))
 (define-char
   '((name		. "MYANMAR SIGN LITTLE SECTION")
-    (ucs		. #x104A)
     (general-category	punctuation other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x104A)	; ၊
     ))
 (define-char
   '((name		. "MYANMAR SIGN SECTION")
-    (ucs		. #x104B)
     (general-category	punctuation other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x104B)	; ။
     ))
 (define-char
   '((name		. "MYANMAR SYMBOL LOCATIVE")
-    (ucs		. #x104C)
     (general-category	punctuation other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x104C)	; ၌
     ))
 (define-char
   '((name		. "MYANMAR SYMBOL COMPLETED")
-    (ucs		. #x104D)
     (general-category	punctuation other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x104D)	; ၍
     ))
 (define-char
   '((name		. "MYANMAR SYMBOL AFOREMENTIONED")
-    (ucs		. #x104E)
     (general-category	punctuation other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x104E)	; ၎
     ))
 (define-char
   '((name		. "MYANMAR SYMBOL GENITIVE")
-    (ucs		. #x104F)
     (general-category	punctuation other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x104F)	; ၏
     ))
 (define-char
   '((name		. "MYANMAR LETTER SHA")
-    (ucs		. #x1050)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1050)	; ၐ
     ))
 (define-char
   '((name		. "MYANMAR LETTER SSA")
-    (ucs		. #x1051)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1051)	; ၑ
     ))
 (define-char
   '((name		. "MYANMAR LETTER VOCALIC R")
-    (ucs		. #x1052)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1052)	; ၒ
     ))
 (define-char
   '((name		. "MYANMAR LETTER VOCALIC RR")
-    (ucs		. #x1053)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1053)	; ၓ
     ))
 (define-char
   '((name		. "MYANMAR LETTER VOCALIC L")
-    (ucs		. #x1054)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1054)	; ၔ
     ))
 (define-char
   '((name		. "MYANMAR LETTER VOCALIC LL")
-    (ucs		. #x1055)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1055)	; ၕ
     ))
 (define-char
   '((name		. "MYANMAR VOWEL SIGN VOCALIC R")
-    (ucs		. #x1056)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1056)	; ၖ
     ))
 (define-char
   '((name		. "MYANMAR VOWEL SIGN VOCALIC RR")
-    (ucs		. #x1057)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1057)	; ၗ
     ))
 (define-char
   '((name		. "MYANMAR VOWEL SIGN VOCALIC L")
-    (ucs		. #x1058)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x1058)	; ၘ
     ))
 (define-char
   '((name		. "MYANMAR VOWEL SIGN VOCALIC LL")
-    (ucs		. #x1059)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x1059)	; ၙ
     ))
