e9743897927573f4053363022196f2abef655a3c
tomo <tomo>
2002-03-11 03:39:28 +0000

    Reformatted.

--- lisp/utf-2000/u00C00-Telugu.el
+++ lisp/utf-2000/u00C00-Telugu.el
@@ -1,591 +1,591 @@
 (define-char
   '((name		. "TELUGU SIGN CANDRABINDU")
-    (ucs		. #x0C01)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C01)	; ఁ
     ))
 (define-char
   '((name		. "TELUGU SIGN ANUSVARA")
-    (ucs		. #x0C02)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C02)	; ం
     ))
 (define-char
   '((name		. "TELUGU SIGN VISARGA")
-    (ucs		. #x0C03)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C03)	; ః
     ))
 (define-char
   '((name		. "TELUGU LETTER A")
-    (ucs		. #x0C05)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C05)	; అ
     ))
 (define-char
   '((name		. "TELUGU LETTER AA")
-    (ucs		. #x0C06)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C06)	; ఆ
     ))
 (define-char
   '((name		. "TELUGU LETTER I")
-    (ucs		. #x0C07)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C07)	; ఇ
     ))
 (define-char
   '((name		. "TELUGU LETTER II")
-    (ucs		. #x0C08)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C08)	; ఈ
     ))
 (define-char
   '((name		. "TELUGU LETTER U")
-    (ucs		. #x0C09)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C09)	; ఉ
     ))
 (define-char
   '((name		. "TELUGU LETTER UU")
-    (ucs		. #x0C0A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C0A)	; ఊ
     ))
 (define-char
   '((name		. "TELUGU LETTER VOCALIC R")
-    (ucs		. #x0C0B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C0B)	; ఋ
     ))
 (define-char
   '((name		. "TELUGU LETTER VOCALIC L")
-    (ucs		. #x0C0C)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C0C)	; ఌ
     ))
 (define-char
   '((name		. "TELUGU LETTER E")
-    (ucs		. #x0C0E)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C0E)	; ఎ
     ))
 (define-char
   '((name		. "TELUGU LETTER EE")
-    (ucs		. #x0C0F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C0F)	; ఏ
     ))
 (define-char
   '((name		. "TELUGU LETTER AI")
-    (ucs		. #x0C10)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C10)	; ఐ
     ))
 (define-char
   '((name		. "TELUGU LETTER O")
-    (ucs		. #x0C12)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C12)	; ఒ
     ))
 (define-char
   '((name		. "TELUGU LETTER OO")
-    (ucs		. #x0C13)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C13)	; ఓ
     ))
 (define-char
   '((name		. "TELUGU LETTER AU")
-    (ucs		. #x0C14)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C14)	; ఔ
     ))
 (define-char
   '((name		. "TELUGU LETTER KA")
-    (ucs		. #x0C15)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C15)	; క
     ))
 (define-char
   '((name		. "TELUGU LETTER KHA")
-    (ucs		. #x0C16)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C16)	; ఖ
     ))
 (define-char
   '((name		. "TELUGU LETTER GA")
-    (ucs		. #x0C17)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C17)	; గ
     ))
 (define-char
   '((name		. "TELUGU LETTER GHA")
-    (ucs		. #x0C18)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C18)	; ఘ
     ))
 (define-char
   '((name		. "TELUGU LETTER NGA")
-    (ucs		. #x0C19)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C19)	; ఙ
     ))
 (define-char
   '((name		. "TELUGU LETTER CA")
-    (ucs		. #x0C1A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C1A)	; చ
     ))
 (define-char
   '((name		. "TELUGU LETTER CHA")
-    (ucs		. #x0C1B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C1B)	; ఛ
     ))
 (define-char
   '((name		. "TELUGU LETTER JA")
-    (ucs		. #x0C1C)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C1C)	; జ
     ))
 (define-char
   '((name		. "TELUGU LETTER JHA")
-    (ucs		. #x0C1D)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C1D)	; ఝ
     ))
 (define-char
   '((name		. "TELUGU LETTER NYA")
-    (ucs		. #x0C1E)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C1E)	; ఞ
     ))
 (define-char
   '((name		. "TELUGU LETTER TTA")
-    (ucs		. #x0C1F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C1F)	; ట
     ))
 (define-char
   '((name		. "TELUGU LETTER TTHA")
-    (ucs		. #x0C20)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C20)	; ఠ
     ))
 (define-char
   '((name		. "TELUGU LETTER DDA")
-    (ucs		. #x0C21)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C21)	; డ
     ))
 (define-char
   '((name		. "TELUGU LETTER DDHA")
-    (ucs		. #x0C22)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C22)	; ఢ
     ))
 (define-char
   '((name		. "TELUGU LETTER NNA")
-    (ucs		. #x0C23)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C23)	; ణ
     ))
 (define-char
   '((name		. "TELUGU LETTER TA")
-    (ucs		. #x0C24)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C24)	; త
     ))
 (define-char
   '((name		. "TELUGU LETTER THA")
-    (ucs		. #x0C25)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C25)	; థ
     ))
 (define-char
   '((name		. "TELUGU LETTER DA")
-    (ucs		. #x0C26)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C26)	; ద
     ))
 (define-char
   '((name		. "TELUGU LETTER DHA")
-    (ucs		. #x0C27)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C27)	; ధ
     ))
 (define-char
   '((name		. "TELUGU LETTER NA")
-    (ucs		. #x0C28)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C28)	; న
     ))
 (define-char
   '((name		. "TELUGU LETTER PA")
-    (ucs		. #x0C2A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C2A)	; ప
     ))
 (define-char
   '((name		. "TELUGU LETTER PHA")
-    (ucs		. #x0C2B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C2B)	; ఫ
     ))
 (define-char
   '((name		. "TELUGU LETTER BA")
-    (ucs		. #x0C2C)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C2C)	; బ
     ))
 (define-char
   '((name		. "TELUGU LETTER BHA")
-    (ucs		. #x0C2D)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C2D)	; భ
     ))
 (define-char
   '((name		. "TELUGU LETTER MA")
-    (ucs		. #x0C2E)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C2E)	; మ
     ))
 (define-char
   '((name		. "TELUGU LETTER YA")
-    (ucs		. #x0C2F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C2F)	; య
     ))
 (define-char
   '((name		. "TELUGU LETTER RA")
-    (ucs		. #x0C30)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C30)	; ర
     ))
 (define-char
   '((name		. "TELUGU LETTER RRA")
-    (ucs		. #x0C31)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C31)	; ఱ
     ))
 (define-char
   '((name		. "TELUGU LETTER LA")
-    (ucs		. #x0C32)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C32)	; ల
     ))
 (define-char
   '((name		. "TELUGU LETTER LLA")
-    (ucs		. #x0C33)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C33)	; ళ
     ))
 (define-char
   '((name		. "TELUGU LETTER VA")
-    (ucs		. #x0C35)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C35)	; వ
     ))
 (define-char
   '((name		. "TELUGU LETTER SHA")
-    (ucs		. #x0C36)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C36)	; శ
     ))
 (define-char
   '((name		. "TELUGU LETTER SSA")
-    (ucs		. #x0C37)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C37)	; ష
     ))
 (define-char
   '((name		. "TELUGU LETTER SA")
-    (ucs		. #x0C38)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C38)	; స
     ))
 (define-char
   '((name		. "TELUGU LETTER HA")
-    (ucs		. #x0C39)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C39)	; హ
     ))
 (define-char
   '((name		. "TELUGU VOWEL SIGN AA")
-    (ucs		. #x0C3E)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0C3E)	; ా
     ))
 (define-char
   '((name		. "TELUGU VOWEL SIGN I")
-    (ucs		. #x0C3F)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0C3F)	; ి
     ))
 (define-char
   '((name		. "TELUGU VOWEL SIGN II")
-    (ucs		. #x0C40)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0C40)	; ీ
     ))
 (define-char
   '((name		. "TELUGU VOWEL SIGN U")
-    (ucs		. #x0C41)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C41)	; ు
     ))
 (define-char
   '((name		. "TELUGU VOWEL SIGN UU")
-    (ucs		. #x0C42)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C42)	; ూ
     ))
 (define-char
   '((name		. "TELUGU VOWEL SIGN VOCALIC R")
-    (ucs		. #x0C43)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C43)	; ృ
     ))
 (define-char
   '((name		. "TELUGU VOWEL SIGN VOCALIC RR")
-    (ucs		. #x0C44)
     (general-category	mark spacing-combining) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C44)	; ౄ
     ))
 (define-char
   '((name		. "TELUGU VOWEL SIGN E")
-    (ucs		. #x0C46)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0C46)	; ె
     ))
 (define-char
   '((name		. "TELUGU VOWEL SIGN EE")
-    (ucs		. #x0C47)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0C47)	; ే
     ))
 (define-char
   '((name		. "TELUGU VOWEL SIGN AI")
-    (ucs		. #x0C48)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
-    (->decomposition	#x0C46 #x0C56)
     (mirrored		. nil)
+    (->decomposition	#x0C46 #x0C56)
+    (ucs		. #x0C48)	; ై
     ))
 (define-char
   '((name		. "TELUGU VOWEL SIGN O")
-    (ucs		. #x0C4A)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0C4A)	; ొ
     ))
 (define-char
   '((name		. "TELUGU VOWEL SIGN OO")
-    (ucs		. #x0C4B)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0C4B)	; ో
     ))
 (define-char
   '((name		. "TELUGU VOWEL SIGN AU")
-    (ucs		. #x0C4C)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0C4C)	; ౌ
     ))
 (define-char
   '((name		. "TELUGU SIGN VIRAMA")
-    (ucs		. #x0C4D)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0C4D)	; ్
     ))
 (define-char
   '((name		. "TELUGU LENGTH MARK")
-    (ucs		. #x0C55)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0C55)	; ౕ
     ))
 (define-char
   '((name		. "TELUGU AI LENGTH MARK")
-    (ucs		. #x0C56)
     (general-category	mark non-spacing) ; Normative Category
     (bidi-category	. "NSM")
     (mirrored		. nil)
+    (ucs		. #x0C56)	; ౖ
     ))
 (define-char
   '((name		. "TELUGU LETTER VOCALIC RR")
-    (ucs		. #x0C60)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C60)	; ౠ
     ))
 (define-char
   '((name		. "TELUGU LETTER VOCALIC LL")
-    (ucs		. #x0C61)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x0C61)	; ౡ
     ))
 (define-char
   '((name		. "TELUGU DIGIT ZERO")
-    (ucs		. #x0C66)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 0)
-    (digit-value	. 0)
-    (numeric-value	. 0)
     (mirrored		. nil)
+    (decimal-digit-value . 0)
+    (digit-value	 . 0)
+    (numeric-value	 . 0)
+    (ucs		. #x0C66)	; ౦
     ))
 (define-char
   '((name		. "TELUGU DIGIT ONE")
-    (ucs		. #x0C67)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 1)
-    (digit-value	. 1)
-    (numeric-value	. 1)
     (mirrored		. nil)
+    (decimal-digit-value . 1)
+    (digit-value	 . 1)
+    (numeric-value	 . 1)
+    (ucs		. #x0C67)	; ౧
     ))
 (define-char
   '((name		. "TELUGU DIGIT TWO")
-    (ucs		. #x0C68)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 2)
-    (digit-value	. 2)
-    (numeric-value	. 2)
     (mirrored		. nil)
+    (decimal-digit-value . 2)
+    (digit-value	 . 2)
+    (numeric-value	 . 2)
+    (ucs		. #x0C68)	; ౨
     ))
 (define-char
   '((name		. "TELUGU DIGIT THREE")
-    (ucs		. #x0C69)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 3)
-    (digit-value	. 3)
-    (numeric-value	. 3)
     (mirrored		. nil)
+    (decimal-digit-value . 3)
+    (digit-value	 . 3)
+    (numeric-value	 . 3)
+    (ucs		. #x0C69)	; ౩
     ))
 (define-char
   '((name		. "TELUGU DIGIT FOUR")
-    (ucs		. #x0C6A)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 4)
-    (digit-value	. 4)
-    (numeric-value	. 4)
     (mirrored		. nil)
+    (decimal-digit-value . 4)
+    (digit-value	 . 4)
+    (numeric-value	 . 4)
+    (ucs		. #x0C6A)	; ౪
     ))
 (define-char
   '((name		. "TELUGU DIGIT FIVE")
-    (ucs		. #x0C6B)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 5)
-    (digit-value	. 5)
-    (numeric-value	. 5)
     (mirrored		. nil)
+    (decimal-digit-value . 5)
+    (digit-value	 . 5)
+    (numeric-value	 . 5)
+    (ucs		. #x0C6B)	; ౫
     ))
 (define-char
   '((name		. "TELUGU DIGIT SIX")
-    (ucs		. #x0C6C)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 6)
-    (digit-value	. 6)
-    (numeric-value	. 6)
     (mirrored		. nil)
+    (decimal-digit-value . 6)
+    (digit-value	 . 6)
+    (numeric-value	 . 6)
+    (ucs		. #x0C6C)	; ౬
     ))
 (define-char
   '((name		. "TELUGU DIGIT SEVEN")
-    (ucs		. #x0C6D)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 7)
-    (digit-value	. 7)
-    (numeric-value	. 7)
     (mirrored		. nil)
+    (decimal-digit-value . 7)
+    (digit-value	 . 7)
+    (numeric-value	 . 7)
+    (ucs		. #x0C6D)	; ౭
     ))
 (define-char
   '((name		. "TELUGU DIGIT EIGHT")
-    (ucs		. #x0C6E)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 8)
-    (digit-value	. 8)
-    (numeric-value	. 8)
     (mirrored		. nil)
+    (decimal-digit-value . 8)
+    (digit-value	 . 8)
+    (numeric-value	 . 8)
+    (ucs		. #x0C6E)	; ౮
     ))
 (define-char
   '((name		. "TELUGU DIGIT NINE")
-    (ucs		. #x0C6F)
     (general-category	number decimal-digit) ; Normative Category
     (bidi-category	. "L")
-    (decimal-digit-value . 9)
-    (digit-value	. 9)
-    (numeric-value	. 9)
     (mirrored		. nil)
+    (decimal-digit-value . 9)
+    (digit-value	 . 9)
+    (numeric-value	 . 9)
+    (ucs		. #x0C6F)	; ౯
     ))
