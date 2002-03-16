8cfa4591da64395de1dc51205625dcaf062aafbb
tomo <tomo>
2002-03-16 15:40:10 +0000

    Reformatted.

--- lisp/utf-2000/u010A0-Georgian.el
+++ lisp/utf-2000/u010A0-Georgian.el
@@ -1,584 +1,584 @@
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER AN")
-    (ucs		. #x10A0)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10A0)	; Ⴀ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER BAN")
-    (ucs		. #x10A1)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10A1)	; Ⴁ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER GAN")
-    (ucs		. #x10A2)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10A2)	; Ⴂ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER DON")
-    (ucs		. #x10A3)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10A3)	; Ⴃ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER EN")
-    (ucs		. #x10A4)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10A4)	; Ⴄ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER VIN")
-    (ucs		. #x10A5)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10A5)	; Ⴅ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER ZEN")
-    (ucs		. #x10A6)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10A6)	; Ⴆ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER TAN")
-    (ucs		. #x10A7)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10A7)	; Ⴇ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER IN")
-    (ucs		. #x10A8)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10A8)	; Ⴈ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER KAN")
-    (ucs		. #x10A9)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10A9)	; Ⴉ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER LAS")
-    (ucs		. #x10AA)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10AA)	; Ⴊ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER MAN")
-    (ucs		. #x10AB)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10AB)	; Ⴋ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER NAR")
-    (ucs		. #x10AC)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10AC)	; Ⴌ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER ON")
-    (ucs		. #x10AD)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10AD)	; Ⴍ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER PAR")
-    (ucs		. #x10AE)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10AE)	; Ⴎ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER ZHAR")
-    (ucs		. #x10AF)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10AF)	; Ⴏ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER RAE")
-    (ucs		. #x10B0)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10B0)	; Ⴐ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER SAN")
-    (ucs		. #x10B1)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10B1)	; Ⴑ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER TAR")
-    (ucs		. #x10B2)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10B2)	; Ⴒ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER UN")
-    (ucs		. #x10B3)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10B3)	; Ⴓ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER PHAR")
-    (ucs		. #x10B4)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10B4)	; Ⴔ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER KHAR")
-    (ucs		. #x10B5)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10B5)	; Ⴕ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER GHAN")
-    (ucs		. #x10B6)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10B6)	; Ⴖ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER QAR")
-    (ucs		. #x10B7)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10B7)	; Ⴗ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER SHIN")
-    (ucs		. #x10B8)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10B8)	; Ⴘ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER CHIN")
-    (ucs		. #x10B9)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10B9)	; Ⴙ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER CAN")
-    (ucs		. #x10BA)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10BA)	; Ⴚ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER JIL")
-    (ucs		. #x10BB)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10BB)	; Ⴛ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER CIL")
-    (ucs		. #x10BC)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10BC)	; Ⴜ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER CHAR")
-    (ucs		. #x10BD)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10BD)	; Ⴝ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER XAN")
-    (ucs		. #x10BE)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10BE)	; Ⴞ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER JHAN")
-    (ucs		. #x10BF)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10BF)	; Ⴟ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER HAE")
-    (ucs		. #x10C0)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10C0)	; Ⴠ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER HE")
-    (ucs		. #x10C1)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10C1)	; Ⴡ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER HIE")
-    (ucs		. #x10C2)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10C2)	; Ⴢ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER WE")
-    (ucs		. #x10C3)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10C3)	; Ⴣ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER HAR")
-    (ucs		. #x10C4)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10C4)	; Ⴤ
     ))
 (define-char
   '((name		. "GEORGIAN CAPITAL LETTER HOE")
-    (ucs		. #x10C5)
     (general-category	letter uppercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
     (iso-10646-comment	. "Khutsuri")
+    (ucs		. #x10C5)	; Ⴥ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER AN")
-    (ucs		. #x10D0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10D0)	; ა
     ))
 (define-char
   '((name		. "GEORGIAN LETTER BAN")
-    (ucs		. #x10D1)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10D1)	; ბ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER GAN")
-    (ucs		. #x10D2)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10D2)	; გ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER DON")
-    (ucs		. #x10D3)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10D3)	; დ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER EN")
-    (ucs		. #x10D4)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10D4)	; ე
     ))
 (define-char
   '((name		. "GEORGIAN LETTER VIN")
-    (ucs		. #x10D5)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10D5)	; ვ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER ZEN")
-    (ucs		. #x10D6)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10D6)	; ზ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER TAN")
-    (ucs		. #x10D7)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10D7)	; თ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER IN")
-    (ucs		. #x10D8)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10D8)	; ი
     ))
 (define-char
   '((name		. "GEORGIAN LETTER KAN")
-    (ucs		. #x10D9)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10D9)	; კ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER LAS")
-    (ucs		. #x10DA)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10DA)	; ლ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER MAN")
-    (ucs		. #x10DB)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10DB)	; მ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER NAR")
-    (ucs		. #x10DC)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10DC)	; ნ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER ON")
-    (ucs		. #x10DD)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10DD)	; ო
     ))
 (define-char
   '((name		. "GEORGIAN LETTER PAR")
-    (ucs		. #x10DE)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10DE)	; პ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER ZHAR")
-    (ucs		. #x10DF)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10DF)	; ჟ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER RAE")
-    (ucs		. #x10E0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10E0)	; რ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER SAN")
-    (ucs		. #x10E1)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10E1)	; ს
     ))
 (define-char
   '((name		. "GEORGIAN LETTER TAR")
-    (ucs		. #x10E2)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10E2)	; ტ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER UN")
-    (ucs		. #x10E3)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10E3)	; უ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER PHAR")
-    (ucs		. #x10E4)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10E4)	; ფ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER KHAR")
-    (ucs		. #x10E5)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10E5)	; ქ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER GHAN")
-    (ucs		. #x10E6)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10E6)	; ღ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER QAR")
-    (ucs		. #x10E7)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10E7)	; ყ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER SHIN")
-    (ucs		. #x10E8)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10E8)	; შ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER CHIN")
-    (ucs		. #x10E9)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10E9)	; ჩ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER CAN")
-    (ucs		. #x10EA)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10EA)	; ც
     ))
 (define-char
   '((name		. "GEORGIAN LETTER JIL")
-    (ucs		. #x10EB)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10EB)	; ძ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER CIL")
-    (ucs		. #x10EC)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10EC)	; წ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER CHAR")
-    (ucs		. #x10ED)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10ED)	; ჭ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER XAN")
-    (ucs		. #x10EE)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10EE)	; ხ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER JHAN")
-    (ucs		. #x10EF)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10EF)	; ჯ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER HAE")
-    (ucs		. #x10F0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10F0)	; ჰ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER HE")
-    (ucs		. #x10F1)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10F1)	; ჱ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER HIE")
-    (ucs		. #x10F2)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10F2)	; ჲ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER WE")
-    (ucs		. #x10F3)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10F3)	; ჳ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER HAR")
-    (ucs		. #x10F4)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10F4)	; ჴ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER HOE")
-    (ucs		. #x10F5)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10F5)	; ჵ
     ))
 (define-char
   '((name		. "GEORGIAN LETTER FI")
-    (ucs		. #x10F6)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10F6)	; ჶ
     ))
 (define-char
   '((name		. "GEORGIAN PARAGRAPH SEPARATOR")
-    (ucs		. #x10FB)
     (general-category	punctuation other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x10FB)	; ჻
     ))
