b81ebcca1a8fa1f56fdf1dda9ee80af8551bccac
tomo <tomo>
2002-03-17 13:42:35 +0000

    Reformatted.

--- lisp/utf-2000/u01680-Ogham.el
+++ lisp/utf-2000/u01680-Ogham.el
@@ -1,203 +1,203 @@
 (define-char
   '((name		. "OGHAM SPACE MARK")
-    (ucs		. #x1680)
     (general-category	separator space) ; Normative Category
     (bidi-category	. "WS")
     (mirrored		. nil)
+    (ucs		. #x1680)	;  
     ))
 (define-char
   '((name		. "OGHAM LETTER BEITH")
-    (ucs		. #x1681)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1681)	; ᚁ
     ))
 (define-char
   '((name		. "OGHAM LETTER LUIS")
-    (ucs		. #x1682)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1682)	; ᚂ
     ))
 (define-char
   '((name		. "OGHAM LETTER FEARN")
-    (ucs		. #x1683)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1683)	; ᚃ
     ))
 (define-char
   '((name		. "OGHAM LETTER SAIL")
-    (ucs		. #x1684)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1684)	; ᚄ
     ))
 (define-char
   '((name		. "OGHAM LETTER NION")
-    (ucs		. #x1685)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1685)	; ᚅ
     ))
 (define-char
   '((name		. "OGHAM LETTER UATH")
-    (ucs		. #x1686)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1686)	; ᚆ
     ))
 (define-char
   '((name		. "OGHAM LETTER DAIR")
-    (ucs		. #x1687)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1687)	; ᚇ
     ))
 (define-char
   '((name		. "OGHAM LETTER TINNE")
-    (ucs		. #x1688)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1688)	; ᚈ
     ))
 (define-char
   '((name		. "OGHAM LETTER COLL")
-    (ucs		. #x1689)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1689)	; ᚉ
     ))
 (define-char
   '((name		. "OGHAM LETTER CEIRT")
-    (ucs		. #x168A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x168A)	; ᚊ
     ))
 (define-char
   '((name		. "OGHAM LETTER MUIN")
-    (ucs		. #x168B)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x168B)	; ᚋ
     ))
 (define-char
   '((name		. "OGHAM LETTER GORT")
-    (ucs		. #x168C)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x168C)	; ᚌ
     ))
 (define-char
   '((name		. "OGHAM LETTER NGEADAL")
-    (ucs		. #x168D)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x168D)	; ᚍ
     ))
 (define-char
   '((name		. "OGHAM LETTER STRAIF")
-    (ucs		. #x168E)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x168E)	; ᚎ
     ))
 (define-char
   '((name		. "OGHAM LETTER RUIS")
-    (ucs		. #x168F)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x168F)	; ᚏ
     ))
 (define-char
   '((name		. "OGHAM LETTER AILM")
-    (ucs		. #x1690)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1690)	; ᚐ
     ))
 (define-char
   '((name		. "OGHAM LETTER ONN")
-    (ucs		. #x1691)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1691)	; ᚑ
     ))
 (define-char
   '((name		. "OGHAM LETTER UR")
-    (ucs		. #x1692)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1692)	; ᚒ
     ))
 (define-char
   '((name		. "OGHAM LETTER EADHADH")
-    (ucs		. #x1693)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1693)	; ᚓ
     ))
 (define-char
   '((name		. "OGHAM LETTER IODHADH")
-    (ucs		. #x1694)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1694)	; ᚔ
     ))
 (define-char
   '((name		. "OGHAM LETTER EABHADH")
-    (ucs		. #x1695)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1695)	; ᚕ
     ))
 (define-char
   '((name		. "OGHAM LETTER OR")
-    (ucs		. #x1696)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1696)	; ᚖ
     ))
 (define-char
   '((name		. "OGHAM LETTER UILLEANN")
-    (ucs		. #x1697)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1697)	; ᚗ
     ))
 (define-char
   '((name		. "OGHAM LETTER IFIN")
-    (ucs		. #x1698)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1698)	; ᚘ
     ))
 (define-char
   '((name		. "OGHAM LETTER EAMHANCHOLL")
-    (ucs		. #x1699)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x1699)	; ᚙ
     ))
 (define-char
   '((name		. "OGHAM LETTER PEITH")
-    (ucs		. #x169A)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x169A)	; ᚚ
     ))
 (define-char
   '((name		. "OGHAM FEATHER MARK")
-    (ucs		. #x169B)
     (general-category	punctuation open) ; Informative Category
     (bidi-category	. "ON")
     (mirrored		. nil)
+    (ucs		. #x169B)	; ᚛
     ))
 (define-char
   '((name		. "OGHAM REVERSED FEATHER MARK")
-    (ucs		. #x169C)
     (general-category	punctuation close) ; Informative Category
     (bidi-category	. "ON")
     (mirrored		. nil)
+    (ucs		. #x169C)	; ᚜
     ))
