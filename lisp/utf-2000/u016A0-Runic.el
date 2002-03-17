a13de3ea3573de2b248a8ddbf0d88d83251af292
tomo <tomo>
2002-03-17 15:49:35 +0000

    Reformatted.

--- lisp/utf-2000/u016A0-Runic.el
+++ lisp/utf-2000/u016A0-Runic.el
@@ -1,573 +1,573 @@
 (define-char
   '((name		. "RUNIC LETTER FEHU FEOH FE F")
-    (ucs		. #x16A0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16A0)	; ᚠ
     ))
 (define-char
   '((name		. "RUNIC LETTER V")
-    (ucs		. #x16A1)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16A1)	; ᚡ
     ))
 (define-char
   '((name		. "RUNIC LETTER URUZ UR U")
-    (ucs		. #x16A2)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16A2)	; ᚢ
     ))
 (define-char
   '((name		. "RUNIC LETTER YR")
-    (ucs		. #x16A3)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16A3)	; ᚣ
     ))
 (define-char
   '((name		. "RUNIC LETTER Y")
-    (ucs		. #x16A4)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16A4)	; ᚤ
     ))
 (define-char
   '((name		. "RUNIC LETTER W")
-    (ucs		. #x16A5)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16A5)	; ᚥ
     ))
 (define-char
   '((name		. "RUNIC LETTER THURISAZ THURS THORN")
-    (ucs		. #x16A6)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16A6)	; ᚦ
     ))
 (define-char
   '((name		. "RUNIC LETTER ETH")
-    (ucs		. #x16A7)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16A7)	; ᚧ
     ))
 (define-char
   '((name		. "RUNIC LETTER ANSUZ A")
-    (ucs		. #x16A8)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16A8)	; ᚨ
     ))
 (define-char
   '((name		. "RUNIC LETTER OS O")
-    (ucs		. #x16A9)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16A9)	; ᚩ
     ))
 (define-char
   '((name		. "RUNIC LETTER AC A")
-    (ucs		. #x16AA)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16AA)	; ᚪ
     ))
 (define-char
   '((name		. "RUNIC LETTER AESC")
-    (ucs		. #x16AB)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16AB)	; ᚫ
     ))
 (define-char
   '((name		. "RUNIC LETTER LONG-BRANCH-OSS O")
-    (ucs		. #x16AC)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16AC)	; ᚬ
     ))
 (define-char
   '((name		. "RUNIC LETTER SHORT-TWIG-OSS O")
-    (ucs		. #x16AD)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16AD)	; ᚭ
     ))
 (define-char
   '((name		. "RUNIC LETTER O")
-    (ucs		. #x16AE)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16AE)	; ᚮ
     ))
 (define-char
   '((name		. "RUNIC LETTER OE")
-    (ucs		. #x16AF)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16AF)	; ᚯ
     ))
 (define-char
   '((name		. "RUNIC LETTER ON")
-    (ucs		. #x16B0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16B0)	; ᚰ
     ))
 (define-char
   '((name		. "RUNIC LETTER RAIDO RAD REID R")
-    (ucs		. #x16B1)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16B1)	; ᚱ
     ))
 (define-char
   '((name		. "RUNIC LETTER KAUNA")
-    (ucs		. #x16B2)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16B2)	; ᚲ
     ))
 (define-char
   '((name		. "RUNIC LETTER CEN")
-    (ucs		. #x16B3)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16B3)	; ᚳ
     ))
 (define-char
   '((name		. "RUNIC LETTER KAUN K")
-    (ucs		. #x16B4)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16B4)	; ᚴ
     ))
 (define-char
   '((name		. "RUNIC LETTER G")
-    (ucs		. #x16B5)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16B5)	; ᚵ
     ))
 (define-char
   '((name		. "RUNIC LETTER ENG")
-    (ucs		. #x16B6)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16B6)	; ᚶ
     ))
 (define-char
   '((name		. "RUNIC LETTER GEBO GYFU G")
-    (ucs		. #x16B7)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16B7)	; ᚷ
     ))
 (define-char
   '((name		. "RUNIC LETTER GAR")
-    (ucs		. #x16B8)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16B8)	; ᚸ
     ))
 (define-char
   '((name		. "RUNIC LETTER WUNJO WYNN W")
-    (ucs		. #x16B9)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16B9)	; ᚹ
     ))
 (define-char
   '((name		. "RUNIC LETTER HAGLAZ H")
-    (ucs		. #x16BA)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16BA)	; ᚺ
     ))
 (define-char
   '((name		. "RUNIC LETTER HAEGL H")
-    (ucs		. #x16BB)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16BB)	; ᚻ
     ))
 (define-char
   '((name		. "RUNIC LETTER LONG-BRANCH-HAGALL H")
-    (ucs		. #x16BC)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16BC)	; ᚼ
     ))
 (define-char
   '((name		. "RUNIC LETTER SHORT-TWIG-HAGALL H")
-    (ucs		. #x16BD)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16BD)	; ᚽ
     ))
 (define-char
   '((name		. "RUNIC LETTER NAUDIZ NYD NAUD N")
-    (ucs		. #x16BE)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16BE)	; ᚾ
     ))
 (define-char
   '((name		. "RUNIC LETTER SHORT-TWIG-NAUD N")
-    (ucs		. #x16BF)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16BF)	; ᚿ
     ))
 (define-char
   '((name		. "RUNIC LETTER DOTTED-N")
-    (ucs		. #x16C0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16C0)	; ᛀ
     ))
 (define-char
   '((name		. "RUNIC LETTER ISAZ IS ISS I")
-    (ucs		. #x16C1)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16C1)	; ᛁ
     ))
 (define-char
   '((name		. "RUNIC LETTER E")
-    (ucs		. #x16C2)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16C2)	; ᛂ
     ))
 (define-char
   '((name		. "RUNIC LETTER JERAN J")
-    (ucs		. #x16C3)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16C3)	; ᛃ
     ))
 (define-char
   '((name		. "RUNIC LETTER GER")
-    (ucs		. #x16C4)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16C4)	; ᛄ
     ))
 (define-char
   '((name		. "RUNIC LETTER LONG-BRANCH-AR AE")
-    (ucs		. #x16C5)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16C5)	; ᛅ
     ))
 (define-char
   '((name		. "RUNIC LETTER SHORT-TWIG-AR A")
-    (ucs		. #x16C6)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16C6)	; ᛆ
     ))
 (define-char
   '((name		. "RUNIC LETTER IWAZ EOH")
-    (ucs		. #x16C7)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16C7)	; ᛇ
     ))
 (define-char
   '((name		. "RUNIC LETTER PERTHO PEORTH P")
-    (ucs		. #x16C8)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16C8)	; ᛈ
     ))
 (define-char
   '((name		. "RUNIC LETTER ALGIZ EOLHX")
-    (ucs		. #x16C9)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16C9)	; ᛉ
     ))
 (define-char
   '((name		. "RUNIC LETTER SOWILO S")
-    (ucs		. #x16CA)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16CA)	; ᛊ
     ))
 (define-char
   '((name		. "RUNIC LETTER SIGEL LONG-BRANCH-SOL S")
-    (ucs		. #x16CB)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16CB)	; ᛋ
     ))
 (define-char
   '((name		. "RUNIC LETTER SHORT-TWIG-SOL S")
-    (ucs		. #x16CC)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16CC)	; ᛌ
     ))
 (define-char
   '((name		. "RUNIC LETTER C")
-    (ucs		. #x16CD)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16CD)	; ᛍ
     ))
 (define-char
   '((name		. "RUNIC LETTER Z")
-    (ucs		. #x16CE)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16CE)	; ᛎ
     ))
 (define-char
   '((name		. "RUNIC LETTER TIWAZ TIR TYR T")
-    (ucs		. #x16CF)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16CF)	; ᛏ
     ))
 (define-char
   '((name		. "RUNIC LETTER SHORT-TWIG-TYR T")
-    (ucs		. #x16D0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16D0)	; ᛐ
     ))
 (define-char
   '((name		. "RUNIC LETTER D")
-    (ucs		. #x16D1)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16D1)	; ᛑ
     ))
 (define-char
   '((name		. "RUNIC LETTER BERKANAN BEORC BJARKAN B")
-    (ucs		. #x16D2)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16D2)	; ᛒ
     ))
 (define-char
   '((name		. "RUNIC LETTER SHORT-TWIG-BJARKAN B")
-    (ucs		. #x16D3)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16D3)	; ᛓ
     ))
 (define-char
   '((name		. "RUNIC LETTER DOTTED-P")
-    (ucs		. #x16D4)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16D4)	; ᛔ
     ))
 (define-char
   '((name		. "RUNIC LETTER OPEN-P")
-    (ucs		. #x16D5)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16D5)	; ᛕ
     ))
 (define-char
   '((name		. "RUNIC LETTER EHWAZ EH E")
-    (ucs		. #x16D6)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16D6)	; ᛖ
     ))
 (define-char
   '((name		. "RUNIC LETTER MANNAZ MAN M")
-    (ucs		. #x16D7)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16D7)	; ᛗ
     ))
 (define-char
   '((name		. "RUNIC LETTER LONG-BRANCH-MADR M")
-    (ucs		. #x16D8)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16D8)	; ᛘ
     ))
 (define-char
   '((name		. "RUNIC LETTER SHORT-TWIG-MADR M")
-    (ucs		. #x16D9)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16D9)	; ᛙ
     ))
 (define-char
   '((name		. "RUNIC LETTER LAUKAZ LAGU LOGR L")
-    (ucs		. #x16DA)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16DA)	; ᛚ
     ))
 (define-char
   '((name		. "RUNIC LETTER DOTTED-L")
-    (ucs		. #x16DB)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16DB)	; ᛛ
     ))
 (define-char
   '((name		. "RUNIC LETTER INGWAZ")
-    (ucs		. #x16DC)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16DC)	; ᛜ
     ))
 (define-char
   '((name		. "RUNIC LETTER ING")
-    (ucs		. #x16DD)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16DD)	; ᛝ
     ))
 (define-char
   '((name		. "RUNIC LETTER DAGAZ DAEG D")
-    (ucs		. #x16DE)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16DE)	; ᛞ
     ))
 (define-char
   '((name		. "RUNIC LETTER OTHALAN ETHEL O")
-    (ucs		. #x16DF)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16DF)	; ᛟ
     ))
 (define-char
   '((name		. "RUNIC LETTER EAR")
-    (ucs		. #x16E0)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16E0)	; ᛠ
     ))
 (define-char
   '((name		. "RUNIC LETTER IOR")
-    (ucs		. #x16E1)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16E1)	; ᛡ
     ))
 (define-char
   '((name		. "RUNIC LETTER CWEORTH")
-    (ucs		. #x16E2)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16E2)	; ᛢ
     ))
 (define-char
   '((name		. "RUNIC LETTER CALC")
-    (ucs		. #x16E3)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16E3)	; ᛣ
     ))
 (define-char
   '((name		. "RUNIC LETTER CEALC")
-    (ucs		. #x16E4)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16E4)	; ᛤ
     ))
 (define-char
   '((name		. "RUNIC LETTER STAN")
-    (ucs		. #x16E5)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16E5)	; ᛥ
     ))
 (define-char
   '((name		. "RUNIC LETTER LONG-BRANCH-YR")
-    (ucs		. #x16E6)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16E6)	; ᛦ
     ))
 (define-char
   '((name		. "RUNIC LETTER SHORT-TWIG-YR")
-    (ucs		. #x16E7)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16E7)	; ᛧ
     ))
 (define-char
   '((name		. "RUNIC LETTER ICELANDIC-YR")
-    (ucs		. #x16E8)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16E8)	; ᛨ
     ))
 (define-char
   '((name		. "RUNIC LETTER Q")
-    (ucs		. #x16E9)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16E9)	; ᛩ
     ))
 (define-char
   '((name		. "RUNIC LETTER X")
-    (ucs		. #x16EA)
     (general-category	letter other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16EA)	; ᛪ
     ))
 (define-char
   '((name		. "RUNIC SINGLE PUNCTUATION")
-    (ucs		. #x16EB)
     (general-category	punctuation other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16EB)	; ᛫
     ))
 (define-char
   '((name		. "RUNIC MULTIPLE PUNCTUATION")
-    (ucs		. #x16EC)
     (general-category	punctuation other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16EC)	; ᛬
     ))
 (define-char
   '((name		. "RUNIC CROSS PUNCTUATION")
-    (ucs		. #x16ED)
     (general-category	punctuation other) ; Informative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (ucs		. #x16ED)	; ᛭
     ))
 (define-char
   '((name		. "RUNIC ARLAUG SYMBOL")
-    (ucs		. #x16EE)
     (general-category	number other) ; Normative Category
     (bidi-category	. "L")
-    (numeric-value	. 17)
     (mirrored		. nil)
+    (numeric-value	. 17)
     (iso-10646-comment	. "golden number 17")
+    (ucs		. #x16EE)	; ᛮ
     ))
 (define-char
   '((name		. "RUNIC TVIMADUR SYMBOL")
-    (ucs		. #x16EF)
     (general-category	number other) ; Normative Category
     (bidi-category	. "L")
-    (numeric-value	. 18)
     (mirrored		. nil)
+    (numeric-value	. 18)
     (iso-10646-comment	. "golden number 18")
+    (ucs		. #x16EF)	; ᛯ
     ))
 (define-char
   '((name		. "RUNIC BELGTHOR SYMBOL")
-    (ucs		. #x16F0)
     (general-category	number other) ; Normative Category
     (bidi-category	. "L")
-    (numeric-value	. 19)
     (mirrored		. nil)
+    (numeric-value	. 19)
     (iso-10646-comment	. "golden number 19")
+    (ucs		. #x16F0)	; ᛰ
     ))
