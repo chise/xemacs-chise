479093d0e21e1d10809a7e01c4b9e3d4f59f165c
tomo <tomo>
2002-04-12 05:08:34 +0000

    (decode_builtin_char): Use `decode_builtin_char' instead of `DECODE_CHAR' for mother; don't use special code for chinese-big5 to use code space of chinese-big5-1 and chinese-big5-2. (complex_vars_of_mule_charset): Use `MIN_CHAR_BIG5_CDP' and `MAX_CHAR_BIG5_CDP' for chinese-big5.

--- src/mule-charset.c
+++ src/mule-charset.c
@@ -1028,8 +1028,10 @@ decode_builtin_char (Lisp_Object charset, int code_point)
 	      + (row - (18 + 32)) * 94
 	      + cell - 33;
 	}
-      return DECODE_CHAR (mother, code + XCHARSET_CODE_OFFSET(charset));
+      return
+	decode_builtin_char (mother, code + XCHARSET_CODE_OFFSET(charset));
     }
+#if 0
   else if (EQ (charset, Vcharset_chinese_big5))
     {
       int c1 = code_point >> 8;
@@ -1056,6 +1058,7 @@ decode_builtin_char (Lisp_Object charset, int code_point)
 	  code_point = ((I / 94 + 33) << 8) | (I % 94 + 33);
 	}
     }
+#endif
   if ((final = XCHARSET_FINAL (charset)) >= '0')
     {
       if (XCHARSET_DIMENSION (charset) == 1)
@@ -2966,9 +2969,8 @@ complex_vars_of_mule_charset (void)
 		  build_string ("Big5 Chinese traditional"),
 		  build_string ("big5-0"),
 		  Qnil,
-		  0 /* MIN_CHAR_BIG5_CDP */,
-		  0 /* MAX_CHAR_BIG5_CDP */, 0, 0,
-		  Qnil, CONVERSION_IDENTICAL);
+		  MIN_CHAR_BIG5_CDP, MAX_CHAR_BIG5_CDP,
+		  MIN_CHAR_BIG5_CDP, 0, Qnil, CONVERSION_IDENTICAL);
 #if 0
   staticpro (&Vcharset_chinese_big5_cdp);
   Vcharset_chinese_big5_cdp =
