be7ee0f566927ad36564b29f30c2f0f3a7f6fdd6
tomo <tomo>
2002-10-24 18:44:41 +0000

    (M-44029): Map to U-00020A78; add `->same-ideograph' for M-02979. (M044029): Relate to M-44029.

--- lisp/utf-2000/Ideograph-R184-Eat.el
+++ lisp/utf-2000/Ideograph-R184-Eat.el
@@ -239,10 +239,22 @@
     (chinese-cns11643-6 . #x3C77)	; ø»œ°
     ))
 (define-char
-  '((ideographic-radical . 184)	; â¾·
+  '((=>ucs		. #x20A78)	; ð ©¸
+    (ideographic-radical . 184)	; â¾·
     (ideographic-strokes . 2)
     (total-strokes	 . 11)
+    (->same-ideograph
+     ((ideograph-daikanwa . 02979)	; ø¸€®£
+      ))
     (ideograph-daikanwa . 44029)	; ø¸Š¯½
+    ))
+(define-char
+  '((=>ucs		. #x20A78)	; ð ©¸
+    (morohashi-daikanwa	44029 0 0)
+    (ideographic-radical . 184)	; â¾·
+    (ideographic-strokes . 2)
+    (total-strokes	 . 11)
+    (mojikyo		. 044029)	; ý €Š¯½
     ))
 (define-char
   '((ideographic-radical . 184)	; â¾·
