b30286bb931e32c857587ba61f25263a7f8ede86
tomo <tomo>
2002-11-05 19:05:52 +0000

    (M-48286): Add mapping for U-0002FA19. (C3-5C2F): Unify U-0002FA19.

--- lisp/utf-2000/Ideograph-R205-Frog.el
+++ lisp/utf-2000/Ideograph-R205-Frog.el
@@ -323,7 +323,8 @@
     (ucs		. #x9F05)	; é¼…
     ))
 (define-char
-  '((ideographic-radical . 205)	; â¿Œ
+  '((=>ucs		. #x2FA19)	; ð¯¨™
+    (ideographic-radical . 205)	; â¿Œ
     (ideographic-strokes . 8)
     (total-strokes	 . 19)
     (ideograph-daikanwa . 48286)	; ø¸‹²ž
@@ -334,6 +335,7 @@
     (ideographic-strokes . 8)
     (total-strokes	 . 19)
     (chinese-cns11643-3 . #x5C2F)	; ø»–­œ
+    (ucs		. #x2FA19)	; ð¯¨™
     ))
 (define-char
   '((ideographic-radical . 205)	; â¿Œ
