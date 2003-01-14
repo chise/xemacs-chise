e57c41ec47de14e16dbeafb4a8a7ee3499054459
tomo <tomo>
2003-01-14 11:14:22 +0000

    Reformatted. (U-00020915): Delete `hanyu-dazidian-{vol|page|char}'.

--- lisp/utf-2000/Ideograph-R021-Spoon.el
+++ lisp/utf-2000/Ideograph-R021-Spoon.el
@@ -210,7 +210,7 @@
      ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW")
       (ucs		  . #x2FF1)	; â¿±
       )
-     ((ideograph-daikanwa . 02571)	; ø¸€¨‹
+     ((ucs		  . #x2090E)	; ð ¤Ž
       )
      ((ucs		  . #x77E2)	; çŸ¢
       ))
@@ -221,9 +221,6 @@
     (ideographic-radical . 21)	; â¼”
     (ideographic-strokes . 5)
     (total-strokes	 . 7)
-    (hanyu-dazidian-vol	 . 1)
-    (hanyu-dazidian-page . 262)
-    (hanyu-dazidian-char . 5)
     (ideographic-structure
      ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW")
       (ucs		  . #x2FF1)	; â¿±
