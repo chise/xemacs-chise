f2823b3acf047b304d074d6ab1baab862b323b05
tomo <tomo>
2003-02-09 08:29:31 +0000

    New file.

---
+++ lisp/utf-2000/u031F0-Katakana-Phonetic-Extensions.el
@@ -0,0 +1,80 @@
+(define-char
+  '((name		. "KATAKANA LETTER SMALL KU")
+    (japanese-jisx0213-1	. #x266E)	; ø»¢›Ÿ
+    (ucs			. #x31F0)	; ã‡°
+    ))
+(define-char
+  '((name		. "KATAKANA LETTER SMALL SI")
+    (japanese-jisx0213-1	. #x266F)	; ø»¢› 
+    (ucs			. #x31F1)	; ã‡±
+    ))
+(define-char
+  '((name		. "KATAKANA LETTER SMALL SU")
+    (japanese-jisx0213-1	. #x2670)	; ø»¢›¡
+    (ucs			. #x31F2)	; ã‡²
+    ))
+(define-char
+  '((name		. "KATAKANA LETTER SMALL TO")
+    (japanese-jisx0213-1	. #x2671)	; ø»¢›¢
+    (ucs			. #x31F3)	; ã‡³
+    ))
+(define-char
+  '((name		. "KATAKANA LETTER SMALL NU")
+    (japanese-jisx0213-1	. #x2672)	; ø»¢›£
+    (ucs			. #x31F4)	; ã‡´
+    ))
+(define-char
+  '((name		. "KATAKANA LETTER SMALL HA")
+    (japanese-jisx0213-1	. #x2673)	; ø»¢›¤
+    (ucs			. #x31F5)	; ã‡µ
+    ))
+(define-char
+  '((name		. "KATAKANA LETTER SMALL HI")
+    (japanese-jisx0213-1	. #x2674)	; ø»¢›¥
+    (ucs			. #x31F6)	; ã‡¶
+    ))
+(define-char
+  '((name		. "KATAKANA LETTER SMALL HU")
+    (japanese-jisx0213-1	. #x2675)	; ø»¢›¦
+    (ucs			. #x31F7)	; ã‡·
+    ))
+(define-char
+  '((name		. "KATAKANA LETTER SMALL HE")
+    (japanese-jisx0213-1	. #x2676)	; ø»¢›§
+    (ucs			. #x31F8)	; ã‡¸
+    ))
+(define-char
+  '((name		. "KATAKANA LETTER SMALL HO")
+    (japanese-jisx0213-1	. #x2677)	; ø»¢›¨
+    (ucs			. #x31F9)	; ã‡¹
+    ))
+(define-char
+  '((name		. "KATAKANA LETTER SMALL MU")
+    (japanese-jisx0213-1	. #x2679)	; ø»¢›ª
+    (ucs			. #x31FA)	; ã‡º
+    ))
+(define-char
+  '((name		. "KATAKANA LETTER SMALL RA")
+    (japanese-jisx0213-1	. #x267A)	; ø»¢›«
+    (ucs			. #x31FB)	; ã‡»
+    ))
+(define-char
+  '((name		. "KATAKANA LETTER SMALL RI")
+    (japanese-jisx0213-1	. #x267B)	; ø»¢›¬
+    (ucs			. #x31FC)	; ã‡¼
+    ))
+(define-char
+  '((name		. "KATAKANA LETTER SMALL RU")
+    (japanese-jisx0213-1	. #x267C)	; ø»¢›­
+    (ucs			. #x31FD)	; ã‡½
+    ))
+(define-char
+  '((name		. "KATAKANA LETTER SMALL RE")
+    (japanese-jisx0213-1	. #x267D)	; ø»¢›®
+    (ucs			. #x31FE)	; ã‡¾
+    ))
+(define-char
+  '((name		. "KATAKANA LETTER SMALL RO")
+    (japanese-jisx0213-1	. #x267E)	; ø»¢›¯
+    (ucs			. #x31FF)	; ã‡¿
+    ))
