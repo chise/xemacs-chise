d8bdbc5aada1ec424d172605017593ffe957b8ed
tomo <tomo>
2003-02-13 10:30:48 +0000

    (M-27295): Unify JC3-5EF3 and M054342. (J90-4A36): Unify M027295.

--- lisp/utf-2000/Ideograph-R120-Silk.el
+++ lisp/utf-2000/Ideograph-R120-Silk.el
@@ -1394,6 +1394,8 @@
     (japanese-jisx0208		. #x4A36)	; ø»‡Œ«
     (korean-ksc5601		. #x5D51)	; ø»‰³„
     (ideograph-daikanwa		. 27295)	; ø¸†ªŸ
+    (=jef-china3		. #x5EF3)	; øº…»³
+    (mojikyo			. 054342)	; ý €‘†
     ))
 (define-char
   '((=>ucs		. #x7D1B)	; ç´›
@@ -1404,6 +1406,7 @@
     (shinjigen-2	. 5993)
     (japanese-jisx0208-1990	. #x4A36)	; ø½Š²Ÿ
     (ucs-jis			. #x7D1B)	; ø½Š²Ÿ
+    (mojikyo			. 027295)	; ý €†ªŸ
     ))
 (define-char
   '((morohashi-daikanwa	27295 0 1)
