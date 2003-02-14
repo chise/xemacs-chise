8146be4f14a0a3e6d6960d996175d20217852621
tomo <tomo>
2003-02-14 19:11:59 +0000

    Rename `chinese-big5-pua' to `=big5-pua'.

--- lisp/utf-2000/maps-conf.el
+++ lisp/utf-2000/maps-conf.el
@@ -12,7 +12,7 @@
       ret)
   (while (<= ucs #xF848)
     (setq chr (decode-char 'ucs ucs))
-    (when (setq big5 (get-char-attribute chr 'chinese-big5-pua))
+    (when (setq big5 (get-char-attribute chr '=big5-pua))
       (when (setq chr (decode-char 'chinese-big5-cdp big5))
 	(unless (get-char-attribute chr 'chinese-big5-cdp)
 	  (put-char-attribute chr 'chinese-big5-cdp big5))))
@@ -25,3 +25,27 @@
 ;;        (remove-char-attribute ch 'chinese-big5))
 ;;    nil)
 ;;  'chinese-big5-cdp)
+
+;; (let ((default-coded-charset-priority-list
+;;         '(ideograph-gt-pj-1
+;;           ideograph-gt-pj-2
+;;           ideograph-gt-pj-3
+;;           ideograph-gt-pj-4
+;;           ideograph-gt-pj-5
+;;           ideograph-gt-pj-6
+;;           ideograph-gt-pj-7
+;;           ideograph-gt-pj-8
+;;           ideograph-gt-pj-9
+;;           ideograph-gt-pj-10
+;;           ideograph-gt-pj-11
+;;           ideograph-gt-pj-k1
+;;           ideograph-gt-pj-k2))
+;;       (i 1)
+;;       chr ret)
+;;   (while (<= i 12000)
+;;     (when (and (setq ret (decode-char 'ideograph-gt-k i))
+;;                (setq ret (split-char ret))
+;;                (memq (car ret) default-coded-charset-priority-list))
+;;       (when (setq chr (apply (function make-char) ret))
+;;         (put-char-attribute chr 'ideograph-gt-k i)))
+;;     (setq i (1+ i))))
