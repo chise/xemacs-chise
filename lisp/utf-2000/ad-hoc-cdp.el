c01a73c5f0aa21f12217c95fe1c8266afabbf5a0
tomo <tomo>
2003-02-16 14:01:25 +0000

    Rename `chinese-big5-cdp' to `=big5-cdp'.

--- lisp/utf-2000/ad-hoc-cdp.el
+++ lisp/utf-2000/ad-hoc-cdp.el
@@ -4,9 +4,9 @@
   (while (<= ucs #xF848)
     (setq chr (decode-char 'ucs ucs))
     (when (setq big5 (get-char-attribute chr '=big5-pua))
-      (when (setq chr (decode-char 'chinese-big5-cdp big5))
-	(unless (get-char-attribute chr 'chinese-big5-cdp)
-	  (put-char-attribute chr 'chinese-big5-cdp big5))))
+      (when (setq chr (decode-char '=big5-cdp big5))
+	(unless (get-char-attribute chr '=big5-cdp)
+	  (put-char-attribute chr '=big5-cdp big5))))
     (setq ucs (1+ ucs))))
 
 ;; (map-char-attribute
@@ -15,4 +15,4 @@
 ;;             (get-char-attribute ch 'chinese-big5))
 ;;        (remove-char-attribute ch 'chinese-big5))
 ;;    nil)
-;;  'chinese-big5-cdp)
+;;  '=big5-cdp)
