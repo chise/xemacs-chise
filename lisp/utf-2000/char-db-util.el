5e7d4b493d27da17a759f5ee57c3dc1a70f9be1d
tomo <tomo>
2003-02-17 03:10:07 +0000

    Rename `ideograph-gt-k' to `=gt-k'.

--- lisp/utf-2000/char-db-util.el
+++ lisp/utf-2000/char-db-util.el
@@ -916,7 +916,7 @@
 	    (cond ((memq name '(ideograph-daikanwa-2
 				ideograph-daikanwa
 				ideograph-gt
-				ideograph-gt-k
+				=gt-k
 				ideograph-cbeta))
 		   (if has-long-ccs-name
 		       "(%-26s . %05d)\t; %c%s"
