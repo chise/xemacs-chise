00c9ac362c9224de54631f467182c830e0c2afdc
tomo <tomo>
2003-04-02 16:14:48 +0000

    (U+FA10): Rename `=>ucs-big5' to `=>ucs@big5'.

--- lisp/utf-2000/Ideograph-R032-Earth.el
+++ lisp/utf-2000/Ideograph-R032-Earth.el
@@ -7555,7 +7555,7 @@
   '((=>ucs*		. #x585A)	; å¡š
     (=>ucs@gb		. #x585A)	; å¡š
     (=>ucs@cns		. #x585A)	; ø»’¤
-    (=>ucs-big5		. #x585A)	; ø»’¤
+    (=>ucs@big5		. #x585A)	; ø»’¤
     (ideographic-radical . 32)	; â¼Ÿ
     (ideographic-strokes . 10)
     (total-strokes	 . 13)
