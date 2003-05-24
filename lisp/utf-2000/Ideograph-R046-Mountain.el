dc6e502a442a3e1da44036bb17c16a6eaed4d00a
tomo <tomo>
2003-05-24 16:57:29 +0000

    (M-08650): Unify JC3-4BE3.

--- lisp/utf-2000/Ideograph-R046-Mountain.el
+++ lisp/utf-2000/Ideograph-R046-Mountain.el
@@ -9952,6 +9952,7 @@
     (=cns11643-3	. #x6030)	; ø»–³•
     (=jis-x0213-2-2000	. #x2869)	; ø»¤¨š
     (=daikanwa		. 08650)	; ø¸‚‡Š
+    (=jef-china3	. #x4BE3)	; øº„¯£
     (=ucs@jis		. #x5DD7)	; ø»–³•
     (mojikyo		. 008650)	; ý €‚‡Š
     ))
