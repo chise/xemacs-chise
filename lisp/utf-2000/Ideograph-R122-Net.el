ba56f902999b50f38815d5cdf30d58bdd6356cc2
tomo <tomo>
2003-06-18 06:58:13 +0000

    (U+7F87): Unify JC3-64BC.

--- lisp/utf-2000/Ideograph-R122-Net.el
+++ lisp/utf-2000/Ideograph-R122-Net.el
@@ -2618,6 +2618,7 @@
     (=jis-x0208		. #x6634)	; ø½‹›¥
     (=cns11643-2	. #x6D71)	; ø»”½˜
     (=daikanwa		. 28412)	; ø¸†»¼
+    (=jef-china3	. #x64BC)	; øº†’¼
     (=ucs		. #x7F87)	; ç¾‡
     ))
 (define-char
