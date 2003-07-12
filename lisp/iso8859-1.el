de471017a37027f7afa580f2d29e3261a3a19776
tomo <tomo>
2003-07-12 17:13:58 +0000

    Sync with r21-4-11-chise-0_21-maps-c1-2.

--- lisp/iso8859-1.el
+++ lisp/iso8859-1.el
@@ -142,6 +142,7 @@
   )
 
 
+(unless (featurep 'utf-2000)
 (defconst iso8859/1-case-table nil
   "The case table for ISO-8859/1 characters.")
 
@@ -198,6 +199,7 @@
  )
 
 (set-standard-case-table (mapcar 'copy-sequence iso8859/1-case-table))
+)
 
 (setq-default ctl-arrow 'iso-8859/1)
 
