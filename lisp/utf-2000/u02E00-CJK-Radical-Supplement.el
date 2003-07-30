2e1b055849f31e4f377f5718388f4f2b31dca94e
tomo <tomo>
2003-07-30 12:32:37 +0000

    (U+2EBC): Unify GT-37857.

--- lisp/utf-2000/u02E00-CJK-Radical-Supplement.el
+++ lisp/utf-2000/u02E00-CJK-Radical-Supplement.el
@@ -622,7 +622,9 @@
     (ideographic-radical . 130)	; â¾
     (ideographic-strokes . 0)
     (total-strokes	 . 4)
+    (=gt-pj-6		. #x3879)	; ý¡€‰¡
     (=daikanwa		. 29237)	; ø¸‡ˆµ
+    (=gt		. 37857)	; ý¡€‰¡
     (=ucs		. #x2EBC)	; âº¼
     ))
 (define-char
