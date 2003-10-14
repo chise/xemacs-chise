41dfb1712ef103e580a8de0d62871a163062ff22
tomo <tomo>
2003-10-14 12:22:42 +0000

    Modify header.

--- lisp/utf-2000/update-cdb.el
+++ lisp/utf-2000/update-cdb.el
@@ -1,24 +1,24 @@
 ;;; update-cdb.el --- Update and/or setup character attribute database
 
-;; Copyright (C) 2002 MORIOKA Tomohiko.
+;; Copyright (C) 2002,2003 MORIOKA Tomohiko.
 
 ;; Author: MORIOKA Tomohiko <tomo@kanji.zinbun.kyoto-u.ac.jp>
-;; Keywords: Character, Database, UTF-2000, Unicode, UCS-4, MULE.
+;; Keywords: Character, Database, CHISE, Unicode, UCS-4, MULE.
 
-;; This file is part of XEmacs UTF-2000.
+;; This file is part of XEmacs CHISE.
 
-;; XEmacs UTF-2000 is free software; you can redistribute it and/or
+;; XEmacs CHISE is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
 ;; published by the Free Software Foundation; either version 2, or (at
 ;; your option) any later version.
 
-;; XEmacs UTF-2000 is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; XEmacs CHISE is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with XEmacs UTF-2000; see the file COPYING.  If not, write to
+;; along with XEmacs CHISE; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
@@ -82,7 +82,7 @@
 		(mount-char-attribute-table
 		 (intern (file-name-char-attribute-name file))))
 	      (directory-files
-	       (expand-file-name "system-char-id" ; "character/feature" ;
+	       (expand-file-name "system-char-id" ; "character/feature"
 				 system-char-database-directory)
 	       nil nil t t)))
     (dolist (ccs (charset-list))
