aee6e79678a47009d7f93f15111aa462c21e43cb
tomo <tomo>
2003-10-30 11:29:39 +0000

    This commit was manufactured by cvs2svn to create tag 'r21-4-13'.

--- lisp/utf-2000/auto-autoloads.el
+++
@@ -1,31 +0,0 @@
-;;; DO NOT MODIFY THIS FILE
-(if (featurep 'utf-2000-autoloads) (error "Already loaded"))
-
-;;;### (autoloads (what-char-definition) "char-db-util" "utf-2000/char-db-util.el")
-
-(autoload 'what-char-definition "char-db-util" nil t nil)
-
-;;;***
-
-;;;### (autoloads (ideographic-structure-find-char char-ucs char-daikanwa char-representative-of-daikanwa update-ideograph-radical-table) "ideograph-util" "utf-2000/ideograph-util.el")
-
-(autoload 'update-ideograph-radical-table "ideograph-util" nil t nil)
-
-(autoload 'char-representative-of-daikanwa "ideograph-util" nil nil nil)
-
-(autoload 'char-daikanwa "ideograph-util" nil nil nil)
-
-(autoload 'char-ucs "ideograph-util" nil nil nil)
-
-(autoload 'ideographic-structure-find-char "ideograph-util" nil nil nil)
-
-;;;***
-
-;;;### (autoloads (mapping-table-read-file) "read-maps" "utf-2000/read-maps.el")
-
-(autoload 'mapping-table-read-file "read-maps" "\
-Read mapping table." t nil)
-
-;;;***
-
-(provide 'utf-2000-autoloads)
