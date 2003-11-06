ca03830bb11b082147978c092cc680559dab25ad
tomo <tomo>
2003-11-06 20:30:18 +0000

    (GT-K05506): New character. (BC-8CA9): New character.

--- lisp/utf-2000/Ideograph-R130-Meat.el
+++ lisp/utf-2000/Ideograph-R130-Meat.el
@@ -3475,6 +3475,41 @@
     (=cns11643-6	. #x3422)	; ø»œ«
     ))
 (define-char
+  '((ideographic-radical . 130)	; â¾
+    (ideographic-strokes . 5)
+    (total-strokes	 . 9)
+    (->ideographic-variants
+     ((=big5-cdp	  . #x8CA9)	; ý¢€ˆ²©
+      ))
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW")
+      (=ucs		  . #x2FF1)	; â¿±
+      )
+     ((=ucs		  . #x5DE6)	; å·¦
+      )
+     ((=big5-cdp	  . #x8958)	; ý¢€ˆ¥˜
+      ))
+    (=gt-pj-k1		. #x355F)	; ý¡„–‚
+    (=gt-k		. 05506)	; ý¡„–‚
+    ))
+(define-char
+  '((ideographic-radical . 130)	; â¾
+    (ideographic-strokes . 5)
+    (total-strokes	 . 9)
+    (->ideographic-variants
+     ((=gt-k		  . 05506)	; ý¡„–‚
+      ))
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW")
+      (=ucs		  . #x2FF1)	; â¿±
+      )
+     ((=ucs		  . #x5DE6)	; å·¦
+      )
+     ((=big5-cdp	  . #x8A73)	; ý¢€ˆ©³
+      ))
+    (=big5-cdp		. #x8CA9)	; ý¢€ˆ²©
+    ))
+(define-char
   '((=>ucs		. #x2669D)	; ð¦š
     (ideographic-radical . 130)	; â¾
     (ideographic-strokes . 6)
