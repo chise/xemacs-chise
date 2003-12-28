70c1f4b1a3e1c7f3aa79037fd32af890b70602f9
tomo <tomo>
2003-12-28 17:30:49 +0000

    (U+5841): Use `<-simplified-ideograph*sources' instead of char-ref in `<-simplified-ideograph'. (JX1-2F5E): Unify U+FA3A instead of U+58A8.

--- lisp/utf-2000/Ideograph-R032-Earth.el
+++ lisp/utf-2000/Ideograph-R032-Earth.el
@@ -6909,9 +6909,10 @@
     (ideographic-strokes . 9)
     (total-strokes	 . 12)
     (<-simplified-ideograph
-     (:char	((=daikanwa	     . 05574)	; ø¸—†
-		 )
-      :sources	(morohashi-daikanwa)))
+     ((=daikanwa	  . 05574)	; ø¸—†
+      ))
+    (<-simplified-ideograph*sources
+     daikanwa)
     (=ucs		. #x5841)	; å¡
     (=jis-x0208		. #x4E5D)	; ø½Š¸¾
     (=gt		. 06689)	; ý¡€¨¡
@@ -9947,26 +9948,32 @@
     (=hanziku-2		. #xB475)	; ý¢€«‘µ
     ))
 (define-char
-  '((ideographic-radical . 32)	; â¼Ÿ
+  '((=>ucs*		. #x58A8)	; å¢¨
+    (ideographic-radical . 32)	; â¼Ÿ
     (ideographic-strokes . 12)
     (total-strokes	 . 15)
     (hanyu-dazidian	1 487 3)
-    (=ucs		. #x58A8)	; å¢¨
+    (=ucs		. #xFA3A)	; ï¨º
     (=gb2312		. #x442B)	; ø»„¹¨
     (=ks-x1001		. #x5978)	; ø»‰­³
     (=jis-x0213-1-2000	. #x2F5E)	; ø»¢¨
     (=big5		. #xBEA5)	; ý¢€‹º¥
     (=daikanwa		. 05469)	; ø¸•
     (=jef-china3	. #x47DC)	; øº„Ÿœ
-    (=ucs@jis		. #xFA3A)	; å¢¨
+    (=ucs@gb		. #x58A8)	; ï¨º
+    (=ucs@ks		. #x58A8)	; ï¨º
+    (=ucs@big5		. #x58A8)	; ï¨º
     ))
 (define-char
-  '((=>ucs*		. #x58A8)	; å¢¨
+  '((=>ucs@jis		. #xFA3A)	; ï¨º
     (morohashi-daikanwa	5469 0 0)
     (ideographic-radical . 32)	; â¼Ÿ
     (ideographic-strokes . 12)
     (total-strokes	 . 15)
-    (=ucs		. #xFA3A)	; ï¨º
+    (->identical
+     ((=ucs		  . #xFA3A)	; ï¨º
+      ))
+    (=ucs		. #x58A8)	; å¢¨
     ))
 (define-char
   '((=>ucs		. #x58A8)	; å¢¨
