29442f2a716ee0d346aa6c9d2c74ce31a87ee09c
tomo <tomo>
2003-12-31 18:21:20 +0000

    update.

--- lisp/utf-2000/auto-autoloads.el
+++ lisp/utf-2000/auto-autoloads.el
@@ -7,7 +7,15 @@
 
 ;;;***
 
-;;;### (autoloads (ideographic-structure-find-char char-ucs char-daikanwa char-representative-of-daikanwa update-ideograph-radical-table) "ideograph-util" "utf-2000/ideograph-util.el")
+;;;### (autoloads (ideographic-structure-find-char char-ucs char-daikanwa char-representative-of-daikanwa update-ideograph-radical-table char-total-strokes char-total-strokes-from-domains char-ideographic-strokes char-ideographic-strokes-from-domains) "ideograph-util" "utf-2000/ideograph-util.el")
+
+(autoload 'char-ideographic-strokes-from-domains "ideograph-util" nil nil nil)
+
+(autoload 'char-ideographic-strokes "ideograph-util" nil nil nil)
+
+(autoload 'char-total-strokes-from-domains "ideograph-util" nil nil nil)
+
+(autoload 'char-total-strokes "ideograph-util" nil nil nil)
 
 (autoload 'update-ideograph-radical-table "ideograph-util" nil t nil)
 
