cdc7c19ffca309a7ffb78fafbc42ee41a91b37f8
tomo <tomo>
2003-12-31 13:28:27 +0000

    (U+7691): Add `total-strokes'.

--- lisp/utf-2000/Ideograph-R106-White.el
+++ lisp/utf-2000/Ideograph-R106-White.el
@@ -603,6 +603,7 @@
 (define-char
   '((ideographic-radical . 106)	; â½©
     (ideographic-strokes . 6)
+    (total-strokes	 . 11)
     (=ucs		. #x7691)	; çš‘
     (=gb2312		. #x3028)	; ø»„œ
     ))
