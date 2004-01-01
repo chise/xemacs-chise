bf2a46a4ee1a5d9d74111b47a5cfb727e47ad8c6
tomo <tomo>
2004-01-01 20:14:26 +0000

    (U+7ADE): Add `total-strokes'.

--- lisp/utf-2000/Ideograph-R117-Stand.el
+++ lisp/utf-2000/Ideograph-R117-Stand.el
@@ -617,6 +617,7 @@
 (define-char
   '((ideographic-radical . 117)	; â½´
     (ideographic-strokes . 5)
+    (total-strokes	 . 10)
     (=ucs		. #x7ADE)	; ç«ž
     (=gb2312		. #x3E3A)	; ø»„±ƒ
     ))
