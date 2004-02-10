c3b78def6edd84cec29515645d432f1e8338d5e0
tomo <tomo>
2004-02-10 16:12:22 +0000

    (U+5C56): Use `->subsumptive'.

--- lisp/utf-2000/Ideograph-R044-Corpse.el
+++ lisp/utf-2000/Ideograph-R044-Corpse.el
@@ -1004,13 +1004,18 @@
     (=daikanwa		. 07715)	; ø¸¸£
     ))
 (define-char
-  '((ideographic-radical . 44)	; â¼«
+  '((=>daikanwa		. 07716)	; ý¢€…ª
+    (ideographic-radical . 44)	; â¼«
     (ideographic-strokes . 7)
     (total-strokes	 . 10)
+    (->subsumptive
+     ((=big5		  . #xD16A)	; ý¢€…ª
+      (=daikanwa	  . 07716)	; ø¸¸¤
+      )
+     ((=cns11643-2	  . #x2E6C)	; ø»“ ±
+      (=ucs@unicode	  . #x5C56)	; ø»“ ±
+      ))
     (=ucs		. #x5C56)	; å±–
-    (=cns11643-2	. #x2E6C)	; ø»“ ±
-    (=big5		. #xD16A)	; ý¢€…ª
-    (=daikanwa		. 07716)	; ø¸¸¤
     ))
 (define-char
   '((=>ucs		. #x21C65)	; ð¡±¥
