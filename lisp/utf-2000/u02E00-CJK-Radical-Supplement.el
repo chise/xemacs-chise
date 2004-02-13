2b8d9d72f5d5d99dc6b0fd42f6e6c6c774b711cf
tomo <tomo>
2004-02-13 20:41:49 +0000

    Reformatted.

--- lisp/utf-2000/u02E00-CJK-Radical-Supplement.el
+++ lisp/utf-2000/u02E00-CJK-Radical-Supplement.el
@@ -664,6 +664,10 @@
     (ideographic-radical . 130)	; â¾
     (ideographic-strokes . 0)
     (total-strokes	 . 4)
+    (<-ideographic-component-forms
+     ((=ucs		  . #x8089)	; è‚‰
+      ))
+    (=ucs		. #x2EBC)	; âº¼
     (->subsumptive
      ((=gt		  . 37857)	; ý¡€‰¡
       (=gt-pj-6		  . #x3879)	; ý¡€‰¡
@@ -677,10 +681,6 @@
       (=gt-k		  . 00417)	; ý¡„€†¡
       (=gt-pj-k1	  . #x377D)	; ý¢€ˆ¥˜
       ))
-    (<-ideographic-component-forms
-     ((=ucs		  . #x8089)	; è‚‰
-      ))
-    (=ucs		. #x2EBC)	; âº¼
     ))
 (define-char
   '((name		. "CJK RADICAL MORTAR")
