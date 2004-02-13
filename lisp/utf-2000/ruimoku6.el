82f3e26b172b64931fa47579e639f951efee9da2
tomo <tomo>
2004-02-13 20:40:34 +0000

    Reformatted.

--- lisp/utf-2000/ruimoku6.el
+++ lisp/utf-2000/ruimoku6.el
@@ -83,8 +83,8 @@
     ))
 (define-char
   '((sources		bos=A016374-00004)
-    (=ruimoku-v6	. #xE00A)	; øº€Š
     (=ucs@unicode	. #x29C2C)	; øº€Š
+    (=ruimoku-v6	. #xE00A)	; øº€Š
     ))
 (define-char
   '((sources		bos=A2001-16138-00013)
