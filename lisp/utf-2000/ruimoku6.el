4607e878f041701668c594e8a45a8a4c44023d07
tomo <tomo>
2004-04-09 11:36:21 +0000

    New file.

---
+++ lisp/utf-2000/ruimoku6.el
@@ -0,0 +1,138 @@
+;; -*- coding: utf-8-mcs-er -*-
+(define-char
+  '((script		bones-carapaces Ideographs)
+    (sources		bos=A016070-00002)
+    (=ruimoku-v6	. #xE000)	; &RUI6-E000;
+    ))
+(define-char
+  '((script		bones-carapaces Ideographs)
+    (sources		bos=A016070-00002)
+    (=ruimoku-v6	. #xE001)	; &RUI6-E001;
+    ))
+(define-char
+  '((sources		bos=A015208-00001)
+    (=ruimoku-v6	. #xE002)	; &RUI6-E002;
+    ))
+(define-char
+  '((sources		bos=A015208-00001)
+    (=ruimoku-v6	. #xE003)	; &RUI6-E003;
+    ))
+(define-char
+  '((sources		bos=A015208-00001)
+    (=ruimoku-v6	. #xE004)	; &RUI6-E004;
+    ))
+(define-char
+  '((ideographic-radical . 62)	; ⼽
+    (ideographic-strokes . 5)
+    (total-strokes	 . 9)
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER SURROUND FROM UPPER RIGHT")
+      (=ucs		  . #x2FF9)	; ⿹
+      )
+     ((=ucs		  . #x6208)	; 戈
+      )
+     ((=ucs		  . #x51AC)	; 冬
+      ))
+    (sources		bos=A016481-00008)
+    (=ruimoku-v6	. #xE005)	; &RUI6-E005;
+    ))
+(define-char
+  '((ideographic-radical . 37)	; ⼤
+    (<-unknown
+     ((=ucs		  . #x215DB)	; 𡗛
+      ))
+    (sources		bos=A016462-00003 bos=A016430-00004
+			bos=A016594-00007)
+    (=ruimoku-v6	. #xE006)	; &RUI6-E006;
+    ))
+(define-char
+  '((ideographic-radical . 26)	; ⼙
+    (ideographic-strokes . 4)
+    (total-strokes	 . 6)
+    (<-error@abos/2001
+     ((=ruimoku-v6	  . #xE00F)	; &RUI6-E00F;
+      ))
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER LEFT TO RIGHT")
+      (=ucs		  . #x2FF0)	; ⿰
+      )
+     ((=ucs		  . #x6208)	; 戈
+      )
+     ((=ucs		  . #x5369)	; 卩
+      ))
+    (=ruimoku-v6	. #xE007)	; &RUI6-E007;
+    ))
+(define-char
+  '((ideographic-radical . 46)	; ⼭
+    (ideographic-strokes . 14)
+    (total-strokes	 . 17)
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER LEFT TO RIGHT")
+      (=ucs		  . #x2FF0)	; ⿰
+      )
+     ((=ucs		  . #x5C71)	; 山
+      )
+     ((=ucs		  . #x96A0)	; 隠
+      ))
+    (sources		bos=A015330-00006)
+    (=ruimoku-v6	. #xE008)	; &RUI6-E008;
+    ))
+(define-char
+  '((sources		bos=A015412-00003)
+    (=ruimoku-v6	. #xE009)	; &RUI6-E009;
+    ))
+(define-char
+  '((sources		bos=A016374-00004)
+    (=ruimoku-v6	. #xE00A)	; &RUI6-E00A;
+    (=ucs@unicode	. #x29C2C)	; &RUI6-E00A;
+    ))
+(define-char
+  '((sources		bos=A2001-16138-00013)
+    (=ruimoku-v6	. #xE00B)	; &RUI6-E00B;
+    ))
+(define-char
+  '((ideographic-radical . 75)	; ⽊
+    (ideographic-strokes . 8)
+    (total-strokes	 . 12)
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER LEFT TO RIGHT")
+      (=ucs		  . #x2FF0)	; ⿰
+      )
+     ((=ucs		  . #x6728)	; 木
+      )
+     ((=ucs		  . #x5CB8)	; 岸
+      ))
+    (sources		bos=A016758-00009)
+    (=ruimoku-v6	. #xE00C)	; &RUI6-E00C;
+    ))
+(define-char
+  '((->decomposition	#x0101 #x0301)
+    (sources		bos=C011091-00007)
+    (=ruimoku-v6	. #xE00D)	; &RUI6-E00D;
+    ))
+(define-char
+  '((ideographic-radical . 24)	; ⼗
+    (ideographic-strokes . 3)
+    (total-strokes	 . 5)
+    (<-simplified
+     ((=ucs		  . #x5C70)	; 屰
+      ))
+    (sources		bos=A016219-00020)
+    (=big5-cdp		. #x8B75)	; &RUI6-E00E;
+    (=ruimoku-v6	. #xE00E)	; &I-RUI6-E00E;
+    ))
+(define-char
+  '((ideographic-radical . 26)	; ⼙
+    (ideographic-strokes . 3)
+    (total-strokes	 . 5)
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER LEFT TO RIGHT")
+      (=ucs		  . #x2FF0)	; ⿰
+      )
+     ((=ucs		  . #x5F0B)	; 弋
+      )
+     ((=ucs		  . #x5369)	; 卩
+      ))
+    (sources		bos=A016728-00002)
+    (=ruimoku-v6	. #xE00F)	; &RUI6-E00F;
+    ))
