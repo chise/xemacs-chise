ee10bc21f0c13707743d20773e17fe917810f744
tomo <tomo>
2004-04-26 11:40:11 +0000

    (Q_vulgar): New variable. (Q_vulgar_of): New variable. (Fput_char_attribute): Convert char-refs in value of `{->|<-}vulgar' to characters and put reverse links. (Fsave_char_attribute_table): Use `Fchar_refs_simplify_char_specs' for `{->|<-}vulgar'. (syms_of_chartab): Add new symbols `{->|<-}vulgar'.

--- src/chartab.c
+++ src/chartab.c
@@ -1146,6 +1146,8 @@ Lisp_Object Q_component;
 Lisp_Object Q_component_of;
 Lisp_Object Q_same;
 Lisp_Object Q_same_of;
+Lisp_Object Q_vulgar;
+Lisp_Object Q_vulgar_of;
 Lisp_Object Qto_ucs;
 Lisp_Object Q_ucs_unified;
 Lisp_Object Qcompat;
@@ -3516,6 +3518,8 @@ Store CHARACTER's ATTRIBUTE with VALUE.
 	    EQ (attribute, Q_component_of) ||
 	    EQ (attribute, Q_same) ||
 	    EQ (attribute, Q_same_of) ||
+	    EQ (attribute, Q_vulgar) ||
+	    EQ (attribute, Q_vulgar_of) ||
 	    !NILP (Fstring_match (build_string ("^<-simplified[^*]*$"),
 				  Fsymbol_name (attribute),
 				  Qnil, Qnil)) )
@@ -3546,6 +3550,10 @@ Store CHARACTER's ATTRIBUTE with VALUE.
 	rev_feature = Q_same_of;
       else if (EQ (attribute, Q_same_of))
 	rev_feature = Q_same;
+      else if (EQ (attribute, Q_vulgar))
+	rev_feature = Q_vulgar_of;
+      else if (EQ (attribute, Q_vulgar_of))
+	rev_feature = Q_vulgar;
       else
 	{
 	  Lisp_String* name = symbol_name (XSYMBOL (attribute));
@@ -4611,6 +4619,8 @@ syms_of_chartab (void)
   defsymbol (&Q_component_of,		"<-ideographic-component-forms");
   defsymbol (&Q_same,			"->same");
   defsymbol (&Q_same_of,		"<-same");
+  defsymbol (&Q_vulgar,			"->vulgar");
+  defsymbol (&Q_vulgar_of,		"<-vulgar");
   defsymbol (&Qcomposition,		"composition");
   defsymbol (&Q_decomposition,		"->decomposition");
   defsymbol (&Qcompat,			"compat");
