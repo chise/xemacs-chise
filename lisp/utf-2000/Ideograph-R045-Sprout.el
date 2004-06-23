866755fc150ad8fbfdce8a242e60ba4b0b95563f
tomo <tomo>
2004-06-23 01:12:50 +0000

    Reformatted.

--- lisp/utf-2000/Ideograph-R045-Sprout.el
+++ lisp/utf-2000/Ideograph-R045-Sprout.el
@@ -296,7 +296,9 @@
 (define-char
   '((total-strokes	 . 10)
     (<-original
-     ((=daikanwa	  . 09176)	; &I-M-09176;
+     ((=jis-x0208	  . #x392C)	; &GT-11561; [25-12]
+      (=ks-x1001	  . #x7A39)	; &I-K0-7A39; [90-25]
+      (=daikanwa	  . 09176)	; &I-M-09176;
       ))
     (<-original*sources
      zhengzi-tong zhonghua-dazidian daikanwa)
