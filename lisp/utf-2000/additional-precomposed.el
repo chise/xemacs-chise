7218570c34b8cec50bdb25ecacd74faddd05d8ef
tomo <tomo>
2004-11-11 13:33:38 +0000

    Omit `bidi-category' and `mirrored'.

--- lisp/utf-2000/additional-precomposed.el
+++ lisp/utf-2000/additional-precomposed.el
@@ -2,16 +2,12 @@
 (define-char
   '((name*		. "latin capital letter m with ring below")
     (general-category	letter uppercase) ; Normative Category
-    (bidi-category	. "L")
-    (mirrored		. nil)
     (->decomposition	#x004D #x0325)
     (=jef-china3	. #x8AF0)	; &JC3-8AF0;
     ))
 (define-char
   '((name*		. "latin small letter a with macron and acute")
     (general-category	letter lowercase) ; Normative Category
-    (bidi-category	. "L")
-    (mirrored		. nil)
     (->decomposition	#x0101 #x0301)
     (sources		bos=C011091-00007)
     (=ruimoku-v6	. #xE00D)	; &RUI6-E00D;
