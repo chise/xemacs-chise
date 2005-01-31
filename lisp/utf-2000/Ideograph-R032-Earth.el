2db02bbd216fe5088a3a3c54ab13156786ca783a
tomo <tomo>
2005-01-31 07:31:22 +0000

    (U+585E): Use `->subsumptive'.

--- lisp/utf-2000/Ideograph-R032-Earth.el
+++ lisp/utf-2000/Ideograph-R032-Earth.el
@@ -7855,30 +7855,26 @@
     (=cns11643-6	. #x5B62)	; &C6-5B62; [59-66]
     ))
 (define-char
-  '((=>ucs		. #x585E)	; 塞
-    (ideographic-radical . 32)	; ⼟
-    (ideographic-strokes . 10)
-    (total-strokes	 . 13)
-    (shinjigen-2	. 1464)
-    (=jis-x0208		. #x3A49)	; &GT-06725; [26-41]
-    (=ks-x1001		. #x5F5D)	; &I-K0-5F5D; [63-61]
-    (=big5		. #xB6EB)	; &I-B-B6EB;
-    (=gt		. 06725)	; &I-GT-06725;
-    (=gt-k		. 10208)	; &I-GT-K10208;
-    (=gt-pj-1		. #x3A49)	; &I-GT-06725; [26-41]
-    (=daikanwa		. 05349)	; &I-M-05349;
-    (=ucs@jis		. #x585E)	; &GT-06725;
-    (=ucs@big5		. #x585E)	; &GT-06725;
-    ))
-(define-char
-  '((morohashi-daikanwa	5349 0 0)
-    (ideographic-radical . 32)	; ⼟
+  '((ideographic-radical . 32)	; ⼟
     (ideographic-strokes . 10)
     (total-strokes	 . 13)
     (hanyu-dazidian	1 475 1)
     (=ucs		. #x585E)	; 塞
-    (=gb2312		. #x487B)	; &I-G0-487B; [40-91]
-    (=cns11643-1	. #x6352)	; &I-C1-6352; [67-50]
+    (=big5		. #xB6EB)	; &I-B-B6EB;
+    (->subsumptive
+     ((shinjigen-2	  . 1464)
+      (=jis-x0208	  . #x3A49)	; &GT-06725; [26-41]
+      (=ks-x1001	  . #x5F5D)	; &I-K0-5F5D; [63-61]
+      (=gt		  . 06725)	; &I-GT-06725;
+      (=gt-k		  . 10208)	; &I-GT-K10208;
+      (=gt-pj-1		  . #x3A49)	; &I-GT-06725; [26-41]
+      (=daikanwa	  . 05349)	; &I-M-05349;
+      (=ucs@jis		  . #x585E)	; &GT-06725;
+      )
+     ((=gb2312		  . #x487B)	; &C1-6352; [40-91]
+      (=cns11643-1	  . #x6352)	; &I-C1-6352; [67-50]
+      (=ucs@unicode	  . #x585E)	; &C1-6352;
+      ))
     ))
 (define-char
   '((ideographic-radical . 32)	; ⼟
