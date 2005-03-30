55574137fd14ac2a2633e301dc12e2da5b8d2b3e
tomo <tomo>
2005-03-30 22:10:14 +0000

    (M-03049): Map to U-00020AC3. (U-00020AC3): Use `->subsumptive'. (M-03050): Map to U-00020AC4. (U-00020AC4): Use `->subsumptive'. (U+9F8E): Add `ideographic-radical@ucs*sources'. (M-48819): Add `ideographic-{radical|strokes}'. (UU+9F8E): Likewise. (C3-5755): Likewise.

--- lisp/utf-2000/Ideograph-R027-Cliff.el
+++ lisp/utf-2000/Ideograph-R027-Cliff.el
@@ -3062,28 +3062,50 @@
   '((ideographic-radical . 27)	; ⼚
     (ideographic-strokes . 16)
     (total-strokes	 . 18)
-    (=daikanwa		. 03049)	; &M-03049;
+    (=ucs		. #x20AC3)	; 𠫃
+    (->subsumptive
+     ((=daikanwa	  . 03049)	; &M-03049;
+      )
+     ((hanyu-dazidian	  1 80 19)
+      (=hanziku-1	  . #xB4A4)	; &HZK01-B4A4;
+      )
+     ((=ucs@iso		  . #x20AC3)	; &UU+20AC3;
+      ))
     ))
 (define-char
   '((ideographic-radical . 27)	; ⼚
     (ideographic-strokes . 16)
     (total-strokes	 . 18)
-    (=daikanwa		. 03050)	; &M-03050;
+    (=ucs		. #x20AC4)	; 𠫄
+    (->subsumptive
+     ((=daikanwa	  . 03050)	; &M-03050;
+      )
+     ((hanyu-dazidian	  1 80 20)
+      (=hanziku-1	  . #xB4A5)	; &HZK01-B4A5;
+      (=ucs@iso		  . #x20AC4)	; &HZK01-B4A5;
+      ))
     ))
 (define-char
-  '((=>daikanwa		. 48819)	; &M-48819;
-    (ideographic-radical@ucs . 212)	; ⿓
+  '((ideographic-radical@ucs . 212)	; ⿓
     (ideographic-strokes@ucs . 2)
+    (ideographic-radical@ucs*sources
+     daikanwa ucs)
     (ideographic-radical@cns . 27)	; ⼚
     (ideographic-strokes@cns . 16)
     (total-strokes	 . 18)
     (=ucs		. #x9F8E)	; 龎
     (->subsumptive
-     ((=daikanwa	  . 48819)	; &M-48819;
+     ((ideographic-radical . 212)	; ⿓
+      (ideographic-strokes . 2)
+      (=daikanwa	  . 48819)	; &M-48819;
       )
-     ((=ucs@unicode	  . #x9F8E)	; &UU+9F8E;
+     ((ideographic-radical . 212)	; ⿓
+      (ideographic-strokes . 2)
+      (=ucs@unicode	  . #x9F8E)	; &UU+9F8E;
       )
-     ((=cns11643-3	  . #x5755)	; &C3-5755; [55-53]
+     ((ideographic-radical . 27)	; ⼚
+      (ideographic-strokes . 16)
+      (=cns11643-3	  . #x5755)	; &C3-5755; [55-53]
       ))
     ))
 (define-char
@@ -3386,18 +3408,6 @@
     (hanyu-dazidian	8 7 24)
     (=ucs		. #x20ABF)	; 𠪿
     (=hanziku-12	. #xEB57)	; &I-HZK12-EB57;
-    ))
-(define-char
-  '((ideographic-radical . 27)	; ⼚
-    (hanyu-dazidian	1 80 19)
-    (=ucs		. #x20AC3)	; 𠫃
-    (=hanziku-1		. #xB4A4)	; &I-HZK01-B4A4;
-    ))
-(define-char
-  '((ideographic-radical . 27)	; ⼚
-    (hanyu-dazidian	1 80 20)
-    (=ucs		. #x20AC4)	; 𠫄
-    (=hanziku-1		. #xB4A5)	; &I-HZK01-B4A5;
     ))
 (define-char
   '((ideographic-radical . 27)	; ⼚
