bb55adbf7c893b92a34c2388bdeb31d501c4406f
tomo <tomo>
2005-04-05 12:54:51 +0000

    (U+4FEE): Copied from Ideograph-R009-Man.el.

--- lisp/utf-2000/Ideograph-R059-Bristle.el
+++ lisp/utf-2000/Ideograph-R059-Bristle.el
@@ -287,6 +287,35 @@
     (=gb2312		. #x506B)	; &I-G0-506B; [48-75]
     ))
 (define-char
+  '((ideographic-radical@cns . 9)	; ⼈
+    (ideographic-radical@cns*sources
+     daikanwa cns11643 ucs)
+    (ideographic-radical@shinjigen . 59)	; ⼺
+    (hanyu-dazidian	1 159 2)
+    (=ucs		. #x4FEE)	; 修
+    (->subsumptive
+     ((ideographic-strokes@cns . 8)
+      (ideographic-strokes@shinjigen . 7)
+      (total-strokes	   . 10)
+      (shinjigen-1	  . 2315)
+      (shinjigen-2	  . 2315)
+      (=jis-x0208	  . #x3D24)	; &GT-00982; [29-04]
+      (=ks-x1001	  . #x6173)	; &I-K0-6173; [65-83]
+      (=cns11643-1	  . #x543A)	; &I-C1-543A; [52-26]
+      (=big5		  . #xADD7)	; &I-B-ADD7;
+      (=gt		  . 00982)	; &I-GT-00982;
+      (=gt-k		  . 06456)	; &I-GT-K06456;
+      (=gt-pj-1		  . #x3D24)	; &I-GT-00982; [29-04]
+      (=daikanwa	  . 00721)	; &I-M-00721;
+      (=ucs@jis		  . #x4FEE)	; &GT-00982;
+      )
+     ((ideographic-strokes . 7)
+      (total-strokes	   . 9)
+      (=gb2312		  . #x505E)	; &G0-505E; [48-62]
+      (=ucs@unicode	  . #x4FEE)	; &G0-505E;
+      ))
+    ))
+(define-char
   '((ideographic-radical . 59)	; ⼺
     (ideographic-strokes . 7)
     (total-strokes	 . 10)
