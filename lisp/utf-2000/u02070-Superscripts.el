3ca090103580af741a862371c5419d4a1eeeeaab
tomo <tomo>
2006-01-26 00:07:22 +0000

    Reformatted.

--- lisp/utf-2000/u02070-Superscripts.el
+++ lisp/utf-2000/u02070-Superscripts.el
@@ -7,11 +7,11 @@
     (decimal-digit-value . 0)
     (digit-value	 . 0)
     (numeric-value	 . 0)
+    (=ucs		. #x2070)	; ⁰
     (<-superscript
      ((name		  . "DIGIT ZERO")
       (=ucs		  . #x0030)	; 0
       ))
-    (=ucs		. #x2070)	; ⁰
     ))
 (define-char
   '((name		. "SUPERSCRIPT FOUR")
@@ -21,22 +21,22 @@
     (decimal-digit-value . 4)
     (digit-value	 . 4)
     (numeric-value	 . 4)
+    (=ucs		. #x2074)	; ⁴
     (<-superscript
      ((name		  . "DIGIT FOUR")
       (=ucs		  . #x0034)	; 4
       ))
-    (=ucs		. #x2074)	; ⁴
     ))
 (define-char
   '((<-denotational
      ((name		  . "SUPERSCRIPT FOUR")
       (=ucs		  . #x2074)	; ⁴
       ))
+    (=ks-x1001		. #x2979)	; &K0-2979; [09-89]
     (<-fullwidth
      ((name		  . "SUPERSCRIPT FOUR")
       (=ucs		  . #x2074)	; ⁴
       ))
-    (=ks-x1001		. #x2979)	; &K0-2979; [09-89]
     ))
 (define-char
   '((name		. "SUPERSCRIPT FIVE")
@@ -46,11 +46,11 @@
     (decimal-digit-value . 5)
     (digit-value	 . 5)
     (numeric-value	 . 5)
+    (=ucs		. #x2075)	; ⁵
     (<-superscript
      ((name		  . "DIGIT FIVE")
       (=ucs		  . #x0035)	; 5
       ))
-    (=ucs		. #x2075)	; ⁵
     ))
 (define-char
   '((name		. "SUPERSCRIPT SIX")
@@ -60,11 +60,11 @@
     (decimal-digit-value . 6)
     (digit-value	 . 6)
     (numeric-value	 . 6)
+    (=ucs		. #x2076)	; ⁶
     (<-superscript
      ((name		  . "DIGIT SIX")
       (=ucs		  . #x0036)	; 6
       ))
-    (=ucs		. #x2076)	; ⁶
     ))
 (define-char
   '((name		. "SUPERSCRIPT SEVEN")
@@ -74,11 +74,11 @@
     (decimal-digit-value . 7)
     (digit-value	 . 7)
     (numeric-value	 . 7)
+    (=ucs		. #x2077)	; ⁷
     (<-superscript
      ((name		  . "DIGIT SEVEN")
       (=ucs		  . #x0037)	; 7
       ))
-    (=ucs		. #x2077)	; ⁷
     ))
 (define-char
   '((name		. "SUPERSCRIPT EIGHT")
@@ -88,11 +88,11 @@
     (decimal-digit-value . 8)
     (digit-value	 . 8)
     (numeric-value	 . 8)
+    (=ucs		. #x2078)	; ⁸
     (<-superscript
      ((name		  . "DIGIT EIGHT")
       (=ucs		  . #x0038)	; 8
       ))
-    (=ucs		. #x2078)	; ⁸
     ))
 (define-char
   '((name		. "SUPERSCRIPT NINE")
@@ -102,86 +102,86 @@
     (decimal-digit-value . 9)
     (digit-value	 . 9)
     (numeric-value	 . 9)
+    (=ucs		. #x2079)	; ⁹
     (<-superscript
      ((name		  . "DIGIT NINE")
       (=ucs		  . #x0039)	; 9
       ))
-    (=ucs		. #x2079)	; ⁹
     ))
 (define-char
   '((name		. "SUPERSCRIPT PLUS SIGN")
     (general-category	symbol math) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (=ucs		. #x207A)	; ⁺
     (<-superscript
      ((name		  . "PLUS SIGN")
       (=ucs		  . #x002B)	; +
       ))
-    (=ucs		. #x207A)	; ⁺
     ))
 (define-char
   '((name		. "SUPERSCRIPT MINUS")
     (general-category	symbol math) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (=ucs		. #x207B)	; ⁻
     (<-superscript
      ((name		  . "MINUS SIGN")
       (=ucs		  . #x2212)	; −
       ))
-    (=ucs		. #x207B)	; ⁻
     ))
 (define-char
   '((name		. "SUPERSCRIPT EQUALS SIGN")
     (general-category	symbol math) ; Informative Category
     (bidi-category	. "ON")
     (mirrored		. nil)
+    (=ucs		. #x207C)	; ⁼
     (<-superscript
      ((name		  . "EQUALS SIGN")
       (=ucs		  . #x003D)	; =
       ))
-    (=ucs		. #x207C)	; ⁼
     ))
 (define-char
   '((name		. "SUPERSCRIPT LEFT PARENTHESIS")
     (general-category	punctuation open) ; Informative Category
     (bidi-category	. "ON")
     (mirrored		. t)
+    (=ucs		. #x207D)	; ⁽
     (<-superscript
      ((name		  . "LEFT PARENTHESIS")
       (=ucs		  . #x0028)	; (
       ))
-    (=ucs		. #x207D)	; ⁽
     ))
 (define-char
   '((name		. "SUPERSCRIPT RIGHT PARENTHESIS")
     (general-category	punctuation close) ; Informative Category
     (bidi-category	. "ON")
     (mirrored		. t)
+    (=ucs		. #x207E)	; ⁾
     (<-superscript
      ((name		  . "RIGHT PARENTHESIS")
       (=ucs		  . #x0029)	; )
       ))
-    (=ucs		. #x207E)	; ⁾
     ))
 (define-char
   '((name		. "SUPERSCRIPT LATIN SMALL LETTER N")
     (general-category	letter lowercase) ; Normative Category
     (bidi-category	. "L")
     (mirrored		. nil)
+    (=ucs		. #x207F)	; ⁿ
     (<-superscript
      ((name		  . "LATIN SMALL LETTER N")
       (=ucs		  . #x006E)	; n
       ))
-    (=ucs		. #x207F)	; ⁿ
     ))
 (define-char
   '((<-denotational
      ((name		  . "SUPERSCRIPT LATIN SMALL LETTER N")
       (=ucs		  . #x207F)	; ⁿ
       ))
+    (=ks-x1001		. #x297A)	; &K0-297A; [09-90]
     (<-fullwidth
      ((name		  . "SUPERSCRIPT LATIN SMALL LETTER N")
       (=ucs		  . #x207F)	; ⁿ
       ))
-    (=ks-x1001		. #x297A)	; &K0-297A; [09-90]
     ))
--- lisp/utf-2000/u02080-Subscripts.el
+++ lisp/utf-2000/u02080-Subscripts.el
@@ -7,11 +7,11 @@
     (decimal-digit-value . 0)
     (digit-value	 . 0)
     (numeric-value	 . 0)
+    (=ucs		. #x2080)	; ₀
     (<-subscript
      ((name		  . "DIGIT ZERO")
       (=ucs		  . #x0030)	; 0
       ))
-    (=ucs		. #x2080)	; ₀
     ))
 (define-char
   '((name		. "SUBSCRIPT ONE")
@@ -21,22 +21,22 @@
     (decimal-digit-value . 1)
     (digit-value	 . 1)
     (numeric-value	 . 1)
+    (=ucs		. #x2081)	; ₁
     (<-subscript
      ((name		  . "DIGIT ONE")
       (=ucs		  . #x0031)	; 1
       ))
-    (=ucs		. #x2081)	; ₁
     ))
 (define-char
   '((<-denotational
      ((name		  . "SUBSCRIPT ONE")
       (=ucs		  . #x2081)	; ₁
       ))
+    (=ks-x1001		. #x297B)	; &K0-297B; [09-91]
     (<-fullwidth
      ((name		  . "SUBSCRIPT ONE")
       (=ucs		  . #x2081)	; ₁
       ))
-    (=ks-x1001		. #x297B)	; &K0-297B; [09-91]
     ))
 (define-char
   '((name		. "SUBSCRIPT TWO")
@@ -46,22 +46,22 @@
     (decimal-digit-value . 2)
     (digit-value	 . 2)
     (numeric-value	 . 2)
+    (=ucs		. #x2082)	; ₂
     (<-subscript
      ((name		  . "DIGIT TWO")
       (=ucs		  . #x0032)	; 2
       ))
-    (=ucs		. #x2082)	; ₂
     ))
 (define-char
   '((<-denotational
      ((name		  . "SUBSCRIPT TWO")
       (=ucs		  . #x2082)	; ₂
       ))
+    (=ks-x1001		. #x297C)	; &K0-297C; [09-92]
     (<-fullwidth
      ((name		  . "SUBSCRIPT TWO")
       (=ucs		  . #x2082)	; ₂
       ))
-    (=ks-x1001		. #x297C)	; &K0-297C; [09-92]
     ))
 (define-char
   '((name		. "SUBSCRIPT THREE")
@@ -71,22 +71,22 @@
     (decimal-digit-value . 3)
     (digit-value	 . 3)
     (numeric-value	 . 3)
+    (=ucs		. #x2083)	; ₃
     (<-subscript
      ((name		  . "DIGIT THREE")
       (=ucs		  . #x0033)	; 3
       ))
-    (=ucs		. #x2083)	; ₃
     ))
 (define-char
   '((<-denotational
      ((name		  . "SUBSCRIPT THREE")
       (=ucs		  . #x2083)	; ₃
       ))
+    (=ks-x1001		. #x297D)	; &K0-297D; [09-93]
     (<-fullwidth
      ((name		  . "SUBSCRIPT THREE")
       (=ucs		  . #x2083)	; ₃
       ))
-    (=ks-x1001		. #x297D)	; &K0-297D; [09-93]
     ))
 (define-char
   '((name		. "SUBSCRIPT FOUR")
@@ -96,22 +96,22 @@
     (decimal-digit-value . 4)
     (digit-value	 . 4)
     (numeric-value	 . 4)
+    (=ucs		. #x2084)	; ₄
     (<-subscript
      ((name		  . "DIGIT FOUR")
       (=ucs		  . #x0034)	; 4
       ))
-    (=ucs		. #x2084)	; ₄
     ))
 (define-char
   '((<-denotational
      ((name		  . "SUBSCRIPT FOUR")
       (=ucs		  . #x2084)	; ₄
       ))
+    (=ks-x1001		. #x297E)	; &K0-297E; [09-94]
     (<-fullwidth
      ((name		  . "SUBSCRIPT FOUR")
       (=ucs		  . #x2084)	; ₄
       ))
-    (=ks-x1001		. #x297E)	; &K0-297E; [09-94]
     ))
 (define-char
   '((name		. "SUBSCRIPT FIVE")
@@ -121,11 +121,11 @@
     (decimal-digit-value . 5)
     (digit-value	 . 5)
     (numeric-value	 . 5)
+    (=ucs		. #x2085)	; ₅
     (<-subscript
      ((name		  . "DIGIT FIVE")
       (=ucs		  . #x0035)	; 5
       ))
-    (=ucs		. #x2085)	; ₅
     ))
 (define-char
   '((name		. "SUBSCRIPT SIX")
@@ -135,11 +135,11 @@
     (decimal-digit-value . 6)
     (digit-value	 . 6)
     (numeric-value	 . 6)
+    (=ucs		. #x2086)	; ₆
     (<-subscript
      ((name		  . "DIGIT SIX")
       (=ucs		  . #x0036)	; 6
       ))
-    (=ucs		. #x2086)	; ₆
     ))
 (define-char
   '((name		. "SUBSCRIPT SEVEN")
@@ -149,11 +149,11 @@
     (decimal-digit-value . 7)
     (digit-value	 . 7)
     (numeric-value	 . 7)
+    (=ucs		. #x2087)	; ₇
     (<-subscript
      ((name		  . "DIGIT SEVEN")
       (=ucs		  . #x0037)	; 7
       ))
-    (=ucs		. #x2087)	; ₇
     ))
 (define-char
   '((name		. "SUBSCRIPT EIGHT")
@@ -163,11 +163,11 @@
     (decimal-digit-value . 8)
     (digit-value	 . 8)
     (numeric-value	 . 8)
+    (=ucs		. #x2088)	; ₈
     (<-subscript
      ((name		  . "DIGIT EIGHT")
       (=ucs		  . #x0038)	; 8
       ))
-    (=ucs		. #x2088)	; ₈
     ))
 (define-char
   '((name		. "SUBSCRIPT NINE")
@@ -177,64 +177,64 @@
     (decimal-digit-value . 9)
     (digit-value	 . 9)
     (numeric-value	 . 9)
+    (=ucs		. #x2089)	; ₉
     (<-subscript
      ((name		  . "DIGIT NINE")
       (=ucs		  . #x0039)	; 9
       ))
-    (=ucs		. #x2089)	; ₉
     ))
 (define-char
   '((name		. "SUBSCRIPT PLUS SIGN")
     (general-category	symbol math) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (=ucs		. #x208A)	; ₊
     (<-subscript
      ((name		  . "PLUS SIGN")
       (=ucs		  . #x002B)	; +
       ))
-    (=ucs		. #x208A)	; ₊
     ))
 (define-char
   '((name		. "SUBSCRIPT MINUS")
     (general-category	symbol math) ; Informative Category
     (bidi-category	. "ET")
     (mirrored		. nil)
+    (=ucs		. #x208B)	; ₋
     (<-subscript
      ((name		  . "MINUS SIGN")
       (=ucs		  . #x2212)	; −
       ))
-    (=ucs		. #x208B)	; ₋
     ))
 (define-char
   '((name		. "SUBSCRIPT EQUALS SIGN")
     (general-category	symbol math) ; Informative Category
     (bidi-category	. "ON")
     (mirrored		. nil)
+    (=ucs		. #x208C)	; ₌
     (<-subscript
      ((name		  . "EQUALS SIGN")
       (=ucs		  . #x003D)	; =
       ))
-    (=ucs		. #x208C)	; ₌
     ))
 (define-char
   '((name		. "SUBSCRIPT LEFT PARENTHESIS")
     (general-category	punctuation open) ; Informative Category
     (bidi-category	. "ON")
     (mirrored		. t)
+    (=ucs		. #x208D)	; ₍
     (<-subscript
      ((name		  . "LEFT PARENTHESIS")
       (=ucs		  . #x0028)	; (
       ))
-    (=ucs		. #x208D)	; ₍
     ))
 (define-char
   '((name		. "SUBSCRIPT RIGHT PARENTHESIS")
     (general-category	punctuation close) ; Informative Category
     (bidi-category	. "ON")
     (mirrored		. t)
+    (=ucs		. #x208E)	; ₎
     (<-subscript
      ((name		  . "RIGHT PARENTHESIS")
       (=ucs		  . #x0029)	; )
       ))
-    (=ucs		. #x208E)	; ₎
     ))
