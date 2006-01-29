18577214dc09c3423e8be81fda32a053412ec856
tomo <tomo>
2006-01-29 21:21:03 +0000

    Reformatted.

--- lisp/utf-2000/u02580-Block-Elements.el
+++ lisp/utf-2000/u02580-Block-Elements.el
@@ -18,14 +18,14 @@
      ((name		  . "LOWER ONE EIGHTH BLOCK")
       (=ucs		  . #x2581)	; ▁
       ))
-    (<-fullwidth
-     ((name		  . "LOWER ONE EIGHTH BLOCK")
-      (=ucs		  . #x2581)	; ▁
-      ))
     (=cns11643-1	. #x2324)	; &C1-2324; [03-04]
     (=big5		. #xA262)	; &I-B-A262;
     (=ucs@cns		. #x2581)	; &C1-2324;
     (=ucs@big5		. #x2581)	; &C1-2324;
+    (<-fullwidth
+     ((name		  . "LOWER ONE EIGHTH BLOCK")
+      (=ucs		  . #x2581)	; ▁
+      ))
     ))
 (define-char
   '((name		. "LOWER ONE QUARTER BLOCK")
@@ -39,14 +39,14 @@
      ((name		  . "LOWER ONE QUARTER BLOCK")
       (=ucs		  . #x2582)	; ▂
       ))
-    (<-fullwidth
-     ((name		  . "LOWER ONE QUARTER BLOCK")
-      (=ucs		  . #x2582)	; ▂
-      ))
     (=cns11643-1	. #x2325)	; &C1-2325; [03-05]
     (=big5		. #xA263)	; &I-B-A263;
     (=ucs@cns		. #x2582)	; &C1-2325;
     (=ucs@big5		. #x2582)	; &C1-2325;
+    (<-fullwidth
+     ((name		  . "LOWER ONE QUARTER BLOCK")
+      (=ucs		  . #x2582)	; ▂
+      ))
     ))
 (define-char
   '((name		. "LOWER THREE EIGHTHS BLOCK")
@@ -60,14 +60,14 @@
      ((name		  . "LOWER THREE EIGHTHS BLOCK")
       (=ucs		  . #x2583)	; ▃
       ))
-    (<-fullwidth
-     ((name		  . "LOWER THREE EIGHTHS BLOCK")
-      (=ucs		  . #x2583)	; ▃
-      ))
     (=cns11643-1	. #x2326)	; &C1-2326; [03-06]
     (=big5		. #xA264)	; &I-B-A264;
     (=ucs@cns		. #x2583)	; &C1-2326;
     (=ucs@big5		. #x2583)	; &C1-2326;
+    (<-fullwidth
+     ((name		  . "LOWER THREE EIGHTHS BLOCK")
+      (=ucs		  . #x2583)	; ▃
+      ))
     ))
 (define-char
   '((name		. "LOWER HALF BLOCK")
@@ -81,14 +81,14 @@
      ((name		  . "LOWER HALF BLOCK")
       (=ucs		  . #x2584)	; ▄
       ))
-    (<-fullwidth
-     ((name		  . "LOWER HALF BLOCK")
-      (=ucs		  . #x2584)	; ▄
-      ))
     (=cns11643-1	. #x2327)	; &C1-2327; [03-07]
     (=big5		. #xA265)	; &I-B-A265;
     (=ucs@cns		. #x2584)	; &C1-2327;
     (=ucs@big5		. #x2584)	; &C1-2327;
+    (<-fullwidth
+     ((name		  . "LOWER HALF BLOCK")
+      (=ucs		  . #x2584)	; ▄
+      ))
     ))
 (define-char
   '((name		. "LOWER FIVE EIGHTHS BLOCK")
@@ -102,14 +102,14 @@
      ((name		  . "LOWER FIVE EIGHTHS BLOCK")
       (=ucs		  . #x2585)	; ▅
       ))
-    (<-fullwidth
-     ((name		  . "LOWER FIVE EIGHTHS BLOCK")
-      (=ucs		  . #x2585)	; ▅
-      ))
     (=cns11643-1	. #x2328)	; &C1-2328; [03-08]
     (=big5		. #xA266)	; &I-B-A266;
     (=ucs@cns		. #x2585)	; &C1-2328;
     (=ucs@big5		. #x2585)	; &C1-2328;
+    (<-fullwidth
+     ((name		  . "LOWER FIVE EIGHTHS BLOCK")
+      (=ucs		  . #x2585)	; ▅
+      ))
     ))
 (define-char
   '((name		. "LOWER THREE QUARTERS BLOCK")
@@ -123,14 +123,14 @@
      ((name		  . "LOWER THREE QUARTERS BLOCK")
       (=ucs		  . #x2586)	; ▆
       ))
-    (<-fullwidth
-     ((name		  . "LOWER THREE QUARTERS BLOCK")
-      (=ucs		  . #x2586)	; ▆
-      ))
     (=cns11643-1	. #x2329)	; &C1-2329; [03-09]
     (=big5		. #xA267)	; &I-B-A267;
     (=ucs@cns		. #x2586)	; &C1-2329;
     (=ucs@big5		. #x2586)	; &C1-2329;
+    (<-fullwidth
+     ((name		  . "LOWER THREE QUARTERS BLOCK")
+      (=ucs		  . #x2586)	; ▆
+      ))
     ))
 (define-char
   '((name		. "LOWER SEVEN EIGHTHS BLOCK")
@@ -144,14 +144,14 @@
      ((name		  . "LOWER SEVEN EIGHTHS BLOCK")
       (=ucs		  . #x2587)	; ▇
       ))
-    (<-fullwidth
-     ((name		  . "LOWER SEVEN EIGHTHS BLOCK")
-      (=ucs		  . #x2587)	; ▇
-      ))
     (=cns11643-1	. #x232A)	; &C1-232A; [03-10]
     (=big5		. #xA268)	; &I-B-A268;
     (=ucs@cns		. #x2587)	; &C1-232A;
     (=ucs@big5		. #x2587)	; &C1-232A;
+    (<-fullwidth
+     ((name		  . "LOWER SEVEN EIGHTHS BLOCK")
+      (=ucs		  . #x2587)	; ▇
+      ))
     ))
 (define-char
   '((name		. "FULL BLOCK")
@@ -165,14 +165,14 @@
      ((name		  . "FULL BLOCK")
       (=ucs		  . #x2588)	; █
       ))
-    (<-fullwidth
-     ((name		  . "FULL BLOCK")
-      (=ucs		  . #x2588)	; █
-      ))
     (=cns11643-1	. #x232B)	; &C1-232B; [03-11]
     (=big5		. #xA269)	; &I-B-A269;
     (=ucs@cns		. #x2588)	; &C1-232B;
     (=ucs@big5		. #x2588)	; &C1-232B;
+    (<-fullwidth
+     ((name		  . "FULL BLOCK")
+      (=ucs		  . #x2588)	; █
+      ))
     ))
 (define-char
   '((name		. "LEFT SEVEN EIGHTHS BLOCK")
@@ -186,14 +186,14 @@
      ((name		  . "LEFT SEVEN EIGHTHS BLOCK")
       (=ucs		  . #x2589)	; ▉
       ))
-    (<-fullwidth
-     ((name		  . "LEFT SEVEN EIGHTHS BLOCK")
-      (=ucs		  . #x2589)	; ▉
-      ))
     (=cns11643-1	. #x2332)	; &C1-2332; [03-18]
     (=big5		. #xA270)	; &I-B-A270;
     (=ucs@cns		. #x2589)	; &C1-2332;
     (=ucs@big5		. #x2589)	; &C1-2332;
+    (<-fullwidth
+     ((name		  . "LEFT SEVEN EIGHTHS BLOCK")
+      (=ucs		  . #x2589)	; ▉
+      ))
     ))
 (define-char
   '((name		. "LEFT THREE QUARTERS BLOCK")
@@ -207,14 +207,14 @@
      ((name		  . "LEFT THREE QUARTERS BLOCK")
       (=ucs		  . #x258A)	; ▊
       ))
-    (<-fullwidth
-     ((name		  . "LEFT THREE QUARTERS BLOCK")
-      (=ucs		  . #x258A)	; ▊
-      ))
     (=cns11643-1	. #x2331)	; &C1-2331; [03-17]
     (=big5		. #xA26F)	; &I-B-A26F;
     (=ucs@cns		. #x258A)	; &C1-2331;
     (=ucs@big5		. #x258A)	; &C1-2331;
+    (<-fullwidth
+     ((name		  . "LEFT THREE QUARTERS BLOCK")
+      (=ucs		  . #x258A)	; ▊
+      ))
     ))
 (define-char
   '((name		. "LEFT FIVE EIGHTHS BLOCK")
@@ -228,14 +228,14 @@
      ((name		  . "LEFT FIVE EIGHTHS BLOCK")
       (=ucs		  . #x258B)	; ▋
       ))
-    (<-fullwidth
-     ((name		  . "LEFT FIVE EIGHTHS BLOCK")
-      (=ucs		  . #x258B)	; ▋
-      ))
     (=cns11643-1	. #x2330)	; &C1-2330; [03-16]
     (=big5		. #xA26E)	; &I-B-A26E;
     (=ucs@cns		. #x258B)	; &C1-2330;
     (=ucs@big5		. #x258B)	; &C1-2330;
+    (<-fullwidth
+     ((name		  . "LEFT FIVE EIGHTHS BLOCK")
+      (=ucs		  . #x258B)	; ▋
+      ))
     ))
 (define-char
   '((name		. "LEFT HALF BLOCK")
@@ -249,14 +249,14 @@
      ((name		  . "LEFT HALF BLOCK")
       (=ucs		  . #x258C)	; ▌
       ))
-    (<-fullwidth
-     ((name		  . "LEFT HALF BLOCK")
-      (=ucs		  . #x258C)	; ▌
-      ))
     (=cns11643-1	. #x232F)	; &C1-232F; [03-15]
     (=big5		. #xA26D)	; &I-B-A26D;
     (=ucs@cns		. #x258C)	; &C1-232F;
     (=ucs@big5		. #x258C)	; &C1-232F;
+    (<-fullwidth
+     ((name		  . "LEFT HALF BLOCK")
+      (=ucs		  . #x258C)	; ▌
+      ))
     ))
 (define-char
   '((name		. "LEFT THREE EIGHTHS BLOCK")
@@ -270,14 +270,14 @@
      ((name		  . "LEFT THREE EIGHTHS BLOCK")
       (=ucs		  . #x258D)	; ▍
       ))
-    (<-fullwidth
-     ((name		  . "LEFT THREE EIGHTHS BLOCK")
-      (=ucs		  . #x258D)	; ▍
-      ))
     (=cns11643-1	. #x232E)	; &C1-232E; [03-14]
     (=big5		. #xA26C)	; &I-B-A26C;
     (=ucs@cns		. #x258D)	; &C1-232E;
     (=ucs@big5		. #x258D)	; &C1-232E;
+    (<-fullwidth
+     ((name		  . "LEFT THREE EIGHTHS BLOCK")
+      (=ucs		  . #x258D)	; ▍
+      ))
     ))
 (define-char
   '((name		. "LEFT ONE QUARTER BLOCK")
@@ -291,14 +291,14 @@
      ((name		  . "LEFT ONE QUARTER BLOCK")
       (=ucs		  . #x258E)	; ▎
       ))
-    (<-fullwidth
-     ((name		  . "LEFT ONE QUARTER BLOCK")
-      (=ucs		  . #x258E)	; ▎
-      ))
     (=cns11643-1	. #x232D)	; &C1-232D; [03-13]
     (=big5		. #xA26B)	; &I-B-A26B;
     (=ucs@cns		. #x258E)	; &C1-232D;
     (=ucs@big5		. #x258E)	; &C1-232D;
+    (<-fullwidth
+     ((name		  . "LEFT ONE QUARTER BLOCK")
+      (=ucs		  . #x258E)	; ▎
+      ))
     ))
 (define-char
   '((name		. "LEFT ONE EIGHTH BLOCK")
@@ -312,14 +312,14 @@
      ((name		  . "LEFT ONE EIGHTH BLOCK")
       (=ucs		  . #x258F)	; ▏
       ))
-    (<-fullwidth
-     ((name		  . "LEFT ONE EIGHTH BLOCK")
-      (=ucs		  . #x258F)	; ▏
-      ))
     (=cns11643-1	. #x232C)	; &C1-232C; [03-12]
     (=big5		. #xA26A)	; &I-B-A26A;
     (=ucs@cns		. #x258F)	; &C1-232C;
     (=ucs@big5		. #x258F)	; &C1-232C;
+    (<-fullwidth
+     ((name		  . "LEFT ONE EIGHTH BLOCK")
+      (=ucs		  . #x258F)	; ▏
+      ))
     ))
 (define-char
   '((name		. "RIGHT HALF BLOCK")
@@ -347,12 +347,12 @@
      ((name		  . "MEDIUM SHADE")
       (=ucs		  . #x2592)	; ▒
       ))
+    (=ks-x1001		. #x2246)	; &K0-2246; [02-38]
+    (=ucs@ks		. #x2592)	; &K0-2246;
     (<-fullwidth
      ((name		  . "MEDIUM SHADE")
       (=ucs		  . #x2592)	; ▒
       ))
-    (=ks-x1001		. #x2246)	; &K0-2246; [02-38]
-    (=ucs@ks		. #x2592)	; &K0-2246;
     ))
 (define-char
   '((name		. "DARK SHADE")
@@ -366,12 +366,12 @@
      ((name		  . "DARK SHADE")
       (=ucs		  . #x2593)	; ▓
       ))
+    (=big5-eten		. #xF9FE)	; &B-F9FE;
+    (=ucs@big5		. #x2593)	; &B-F9FE;
     (<-fullwidth
      ((name		  . "DARK SHADE")
       (=ucs		  . #x2593)	; ▓
       ))
-    (=big5-eten		. #xF9FE)	; &B-F9FE;
-    (=ucs@big5		. #x2593)	; &B-F9FE;
     ))
 (define-char
   '((name		. "UPPER ONE EIGHTH BLOCK")
@@ -385,14 +385,14 @@
      ((name		  . "UPPER ONE EIGHTH BLOCK")
       (=ucs		  . #x2594)	; ▔
       ))
-    (<-fullwidth
-     ((name		  . "UPPER ONE EIGHTH BLOCK")
-      (=ucs		  . #x2594)	; ▔
-      ))
     (=cns11643-1	. #x2338)	; &C1-2338; [03-24]
     (=big5		. #xA276)	; &I-B-A276;
     (=ucs@cns		. #x2594)	; &C1-2338;
     (=ucs@big5		. #x2594)	; &C1-2338;
+    (<-fullwidth
+     ((name		  . "UPPER ONE EIGHTH BLOCK")
+      (=ucs		  . #x2594)	; ▔
+      ))
     ))
 (define-char
   '((name		. "RIGHT ONE EIGHTH BLOCK")
@@ -406,12 +406,12 @@
      ((name		  . "RIGHT ONE EIGHTH BLOCK")
       (=ucs		  . #x2595)	; ▕
       ))
-    (<-fullwidth
-     ((name		  . "RIGHT ONE EIGHTH BLOCK")
-      (=ucs		  . #x2595)	; ▕
-      ))
     (=cns11643-1	. #x233B)	; &C1-233B; [03-27]
     (=big5		. #xA279)	; &I-B-A279;
     (=ucs@cns		. #x2595)	; &C1-233B;
     (=ucs@big5		. #x2595)	; &C1-233B;
+    (<-fullwidth
+     ((name		  . "RIGHT ONE EIGHTH BLOCK")
+      (=ucs		  . #x2595)	; ▕
+      ))
     ))
