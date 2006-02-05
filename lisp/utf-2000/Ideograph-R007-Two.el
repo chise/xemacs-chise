fec03136dc601c892338a81cc864920d032bb833
tomo <tomo>
2006-02-05 01:40:14 +0000

    (U+5F00): Unify AJ-16854. (U+4E9D): Unify AJ-17245.

--- lisp/utf-2000/Ideograph-R007-Two.el
+++ lisp/utf-2000/Ideograph-R007-Two.el
@@ -386,6 +386,7 @@
     (=cns11643-4	. #x214D)	; &I-C4-214D; [01-45]
     (=jis-x0213-1-2000	. #x7431)	; &I-JX1-7431; [84-17]
     (=big5-cdp		. #x8B6F)	; &I-CDP-8B6F;
+    (=adobe-japan1-5	. 16854)	; 开
     ))
 (define-char
   '((ideographic-radical . 7)	; ⼆
@@ -705,6 +706,7 @@
     (=daikanwa		. 00273)	; &I-M-00273;
     (=hanziku-1		. #xF57D)	; &I-HZK01-F57D;
     (=jef-china3	. #x41D1)	; &I-JC3-41D1;
+    (=adobe-japan1-5	. 17245)	; 亝
     ))
 (define-char
   '((ideographic-radical . 7)	; ⼆
