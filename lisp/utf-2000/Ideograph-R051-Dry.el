a9e548b060c2ecbcf5fb145578c57512e4b91ee1
tomo <tomo>
2006-02-08 11:16:46 +0000

    (U+5F00): Unify AJ1-16854.

--- lisp/utf-2000/Ideograph-R051-Dry.el
+++ lisp/utf-2000/Ideograph-R051-Dry.el
@@ -34,6 +34,7 @@
     (=cns11643-4	. #x214D)	; &I-C4-214D; [01-45]
     (=jis-x0213-1-2000	. #x7431)	; &I-JX1-7431; [84-17]
     (=big5-cdp		. #x8B6F)	; &I-CDP-8B6F;
+    (=adobe-japan1-5	. 16854)	; 开
     ))
 (define-char
   '((ideographic-radical . 51)	; ⼲
