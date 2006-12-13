9e36a586b0ed581cc9655bdb71f80b507dc86dc3
tomo <tomo>
2006-12-13 06:58:44 +0000

    (C6-2131): Unify U-0002F801.

--- lisp/utf-2000/Ideograph-R003-Dot.el
+++ lisp/utf-2000/Ideograph-R003-Dot.el
@@ -80,12 +80,14 @@
   '((<-denotational
      ((=ucs		  . #x4E38)	; 丸
       ))
+    (=>ucs*		. #x4E38)	; 丸
     (ideographic-radical . 3)	; ⼂
     (ideographic-strokes . 2)
     (total-strokes	 . 3)
     (shinjigen-1	. 39)
     (shinjigen-2	. 39)
-    (=jis-x0208		. #x345D)	; &GT-00131; [20-61]
+    (=ucs		. #x2F801)	; 丸
+    (=jis-x0208		. #x345D)	; &I-J90-345D; [20-61]
     (=gb2312		. #x4D68)	; &I-G0-4D68; [45-72]
     (=ks-x1001		. #x7C2F)	; &I-K0-7C2F; [92-15]
     (=cns11643-6	. #x2131)	; &I-C6-2131; [01-17]
@@ -93,7 +95,8 @@
     (=gt-k		. 00161)	; &I-GT-K00161;
     (=gt-pj-1		. #x345D)	; &I-GT-00131; [20-61]
     (=daikanwa		. 00094)	; &I-M-00094;
-    (=ucs@unicode	. #x4E38)	; &GT-00131;
+    (=ucs@unicode	. #x4E38)	; 丸
+    (=ucs@cns		. #x2F801)	; 丸
     ))
 (define-char
   '((<-denotational
@@ -124,10 +127,8 @@
     (=daikanwa		. 00096)	; &I-M-00096;
     (<-original
      ((shinjigen-1	  . 39)
-      (=jis-x0208	  . #x345D)	; &GT-00131; [20-61]
-      (=gb2312		  . #x4D68)	; &I-G0-4D68; [45-72]
-      (=ks-x1001	  . #x7C2F)	; &I-K0-7C2F; [92-15]
-      (=cns11643-6	  . #x2131)	; &I-C6-2131; [01-17]
+      (=ucs		  . #x2F801)	; 丸
+      (=jis-x0208	  . #x345D)	; &I-J90-345D; [20-61]
       (=daikanwa	  . 00094)	; &I-M-00094;
       ))
     (<-original*sources
@@ -430,8 +431,7 @@
     (<-ancient*sources
      daikanwa)
     (<-same
-     ((=cns11643-6	  . #x2936)	; &GT-00087; [09-22]
-      (=daikanwa	  . 00059)	; &I-M-00059;
+     ((=ucs		  . #x2F800)	; 丽
       ))
     ))
 (define-char
