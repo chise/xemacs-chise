fd59705426099c4d3213a82072bbd92d4c02f78b
tomo <tomo>
2010-02-09 08:23:04 +0000

    (U+7F72): Use `=>jis-x0208@1997' instead of `=jis-x0208@1997'.

--- lisp/utf-2000/Ideograph-R122-Net.el
+++ lisp/utf-2000/Ideograph-R122-Net.el
@@ -1586,8 +1586,8 @@
 (define-char
   '((ideographic-radical . 122)	; ⽹
     (=ucs		. #x7F72)	; 署
+    (=>jis-x0208@1997	. #x3D70)	; 署 [29-80]
     (=big5		. #xB870)	; &I-B-B870;
-    (=jis-x0208@1997	. #x3D70)	; 署 [29-80]
     ))
 (define-char
   '((<-denotational
