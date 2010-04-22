f7591049fb8f211a31bff81b6b63266ef0586a85
tomo <tomo>
2010-04-22 05:44:14 +0000

    (coded-charset-entity-reference-alist): Add settings for &I-AJ1-ddddd; and &AJ1-ddddd;.

--- lisp/mule/mule-coding.el
+++ lisp/mule/mule-coding.el
@@ -182,7 +182,8 @@
 
 (when (featurep 'utf-2000)
   (setq coded-charset-entity-reference-alist
-	'(((=gt			. isolated)     "I-GT-" 5 d)
+	'(((=adobe-japan1-base	. isolated)    "I-AJ1-" 5 d)
+	  ((=gt			. isolated)     "I-GT-" 5 d)
 	  ((=ruimoku-v6		. isolated)   "I-RUI6-" 4 X)
 	  ((chinese-big5-cdp	. isolated)    "I-CDP-" 4 X)
 	  ((=gt-k		. isolated)    "I-GT-K" 5 d)
@@ -239,6 +240,8 @@
 	  ((arabic-digit	. isolated) "I-MULE-ARBD-" 2 X)
 	  ((ipa			. isolated) "I-MULE-IPA-" 2 X)
 	  ((china3-jef		. isolated)    "I-JC3-" 4 X)
+	  ( =adobe-japan1-6			 "AJ1-" 5 d)
+	  ( =adobe-japan1-base		       "I-AJ1-" 5 d)
 	  ( =gt					  "GT-" 5 d)
 	  (=>>gt				"G-GT-" 5 d)
 	  (=>>gt				 "aGT-" 5 d)
