bf79f1bde5bbe330a5bb8a454688744e87d2d9a0
tomo <tomo>
2011-02-11 12:09:06 +0000

    (<CJK RADICAL MEAT>): Use `<-denotational' for A-GT-37857; add `<-identical@component/connect-right' for GT-37857. (GT-K0758): Omitted. (BC-88F0): Omitted.

--- lisp/utf-2000/u02E00-CJK-Radical-Supplement.el
+++ lisp/utf-2000/u02E00-CJK-Radical-Supplement.el
@@ -971,7 +971,10 @@
       ))
     ))
 (define-char
-  '((name		. "CJK RADICAL MEAT")
+  '((<-denotational
+     ((=>gt		  . 37857)	; &A-GT-37857;
+      ))
+    (name		. "CJK RADICAL MEAT")
     (general-category	symbol other) ; Informative Category
     (bidi-category	. "ON")
     (mirrored		. nil)
@@ -982,54 +985,9 @@
     (<-formed@component/connect-right
      ((=ucs		  . #x8089)	; 肉
       ))
-    (->subsumptive
+    (<-identical@component/connect-right
      ((=gt		  . 37857)	; &GT-37857;
-      (=gt-pj-6		  . #x3879)	; &GT-37857; [24-89]
       (=daikanwa	  . 29237)	; &I-M-29237;
-      (<-formed@component/connect-right
-       ((=ucs		    . #x8089)	; 肉
-	(=daikanwa	    . 29236)	; &I-M-29236;
-	))
-      (<-formed@component/connect-right*sources
-       zihui/char-index zhengzitong daikanwa)
-      )
-     ((=ucs@unicode	  . #x2EBC)	; &UU+2EBC;
-      )
-     ((=big5-cdp	  . #x8A73)	; &CDP-8A73;
-      )
-     ((=big5-cdp	  . #x8958)	; &CDP-8958;
-      (=gt-k		  . 00417)	; &I-GT-K00417;
-      (=gt-pj-k1	  . #x377D)	; &CDP-8958; [23-93]
-      (<-formed@component/connect-above
-       ((=ucs		    . #x8089)	; 肉
-	))
-      ))
-    ))
-(define-char
-  '((<-denotational
-     ((name		  . "CJK RADICAL MEAT")
-      (=ucs		  . #x2EBC)	; ⺼
-      ))
-    (ideographic-radical . 130)	; ⾁
-    (ideographic-strokes . 0)
-    (total-strokes	 . 4)
-    (=gt-k		. 00758)	; &GT-K00758;
-    (=gt-pj-k1		. #x362F)	; &GT-K00758; [22-15]
-    (<-formed@component
-     ((=ucs		  . #x8089)	; 肉
-      ))
-    ))
-(define-char
-  '((<-denotational
-     ((name		  . "CJK RADICAL MEAT")
-      (=ucs		  . #x2EBC)	; ⺼
-      ))
-    (ideographic-radical . 130)	; ⾁
-    (ideographic-strokes . 0)
-    (total-strokes	 . 4)
-    (=big5-cdp		. #x88F0)	; &CDP-88F0;
-    (<-formed@component
-     ((=ucs		  . #x8089)	; 肉
       ))
     ))
 (define-char
