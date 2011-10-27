7af4ca5c2da2ef6dd806fe281e857eb0bf22e103
tomo <tomo>
2011-10-27 09:08:31 +0000

    Use `=+>ucs@cns' instead of `=>ucs@cns'. (o-JU+7926): Unify o-J78-395C.

--- lisp/utf-2000/Ideograph-R112-Stone.el
+++ lisp/utf-2000/Ideograph-R112-Stone.el
@@ -7754,9 +7754,10 @@
     (ideographic-radical . 112)	; ⽯
     (ideographic-strokes . 15)
     (total-strokes	 . 20)
-    (=+>ucs@jis		. #x7926)	; &A-CU+7926;
-    (=>ucs@cns		. #x7926)	; &A-CU+7926;
-    (=big5		. #xC471)	; &A-CU+7926;
+    (=+>ucs@jis		. #x7926)	; &o-J78-395C;
+    (=+>ucs@cns		. #x7926)	; &o-J78-395C;
+    (=+>jis-x0208@1978	. #x395C)	; &o-J78-395C; [25-60]
+    (=big5		. #xC471)	; &o-J78-395C;
     (->subsumptive
      ((=ucs@jis		  . #x7926)	; &GT-31404;
       (=jis-x0208@1978	  . #x395C)	; &GT-31404; [25-60]
