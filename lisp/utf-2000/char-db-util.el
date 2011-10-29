af0b0bb1f86e629d13b96e8ea170656457a9bb77
tomo <tomo>
2011-10-29 09:16:40 +0000

    (char-db-coded-charset-priority-list): Add `=+>gt'. (char-db-insert-ccs-feature): Add setting for `=+>gt'.

--- lisp/utf-2000/char-db-util.el
+++ lisp/utf-2000/char-db-util.el
@@ -218,6 +218,7 @@
     =ucs@unicode
     =>>big5-cdp
     =>>gt-k
+    =+>gt
     =>gt
     =>big5-cdp
     =>daikanwa
@@ -484,7 +485,7 @@
        ((or (memq name '(=daikanwa =>>daikanwa =>daikanwa
 			 =daikanwa@rev1 =daikanwa@rev2
 			 =daikanwa/+p =daikanwa/+2p
-			 =gt =>>>gt =>>gt =>gt
+			 =gt =>>>gt =>>gt =+>gt =>gt
 			 =gt-k =>>gt-k =>gt-k
 			 =>>adobe-japan1
 			 =cbeta =>>cbeta
