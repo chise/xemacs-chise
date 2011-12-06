2fdb19662577c5dc07704d09ce08c4cb5e4db808
tomo <tomo>
2011-12-06 06:20:44 +0000

    Reformatted.

--- lisp/utf-2000/Ideograph-R075-Tree.el
+++ lisp/utf-2000/Ideograph-R075-Tree.el
@@ -29154,8 +29154,8 @@
      ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW")
       (=ucs		  . #x2FF1)	; ⿱
       )
-     ((=ucs@unicode	  . #x96B1)	; &HD-FT-3A61;
-      (=gb12345		  . #x527E)	; &HD-FT-3A61; [50-94]
+     ((=ucs@unicode	  . #x96B1)	; 隱󠄂
+      (=gb12345		  . #x527E)	; 隱󠄂 [50-94]
       )
      ((=ucs		  . #x6728)	; 木
       ))
