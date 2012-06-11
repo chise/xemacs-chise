b0d0d086bd69a7c16d281f733fe96f85bf9bf603
MORIOKA Tomohiko <tomo.git@chise.org>
2012-06-11 23:00:25 +0900

    Reformatted.

--- lisp/utf-2000/Ideograph-R048-Work.el
+++ lisp/utf-2000/Ideograph-R048-Work.el
@@ -133,9 +133,9 @@
 	)
        ((=ucs		    . #xE0103)	; 󠄃
 	))
-      (=cns11643-1	  . #x4648)	; 巨󠄃 [38-40]
+      (=cns11643-1	  . #x4648)	; &HD-KS-098730; [38-40]
       (=daikanwa	  . 08722)	; &I-M-08722;
-      (=hanyo-denshi/ks	  . 098730)	; 巨󠄃
+      (=hanyo-denshi/ks	  . 098730)	; &HD-KS-098730;
       (=hanyo-denshi/ks/mf . #x2691)	; &I-KSMF-09873;
       )
      ((=ucs@unicode	  . #x5DE8)	; &GT-03331;
@@ -220,9 +220,9 @@
       )
      ((=ucs		  . #xE0102)	; 󠄂
       ))
-    (=ucs@unicode	. #x5DE9)	; 巩󠄂
-    (=gb2312		. #x392E)	; 巩󠄂 [25-14]
-    (=hanyo-denshi/ks	. 098790)	; 巩󠄂
+    (=ucs@unicode	. #x5DE9)	; &HD-KS-098790;
+    (=gb2312		. #x392E)	; &HD-KS-098790; [25-14]
+    (=hanyo-denshi/ks	. 098790)	; &HD-KS-098790;
     (=hanyo-denshi/ks/mf . #x2697)	; &I-KSMF-09879;
     ))
 (define-char
