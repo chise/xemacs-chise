b103c9095942bcf3f5584a0ae2df2f1362c40ddb
MORIOKA Tomohiko <tomo.git@chise.org>
2013-10-02 02:18:10 +0900

    Apply new glyph-image conventions for `ucs@*', `adobe-japan1-*', `jis-x0208@*', `gb2312', `iso-ir165', `jis-x0213-*', `hanyo-denshi/*', `gt', `gt-k', `gt-pj-*' and `jef-china3'.

--- lisp/utf-2000/Ideograph-R011-Enter.el
+++ lisp/utf-2000/Ideograph-R011-Enter.el
@@ -906,11 +906,11 @@
 	)
        ((=ucs		    . #x65E5)	; 日
 	))
-      (=>>>ucs@iso	  . #x204E4)	; &g2-IU+204E4;
+      (=ucs@iso		  . #x204E4)	; &U-000204E4;
       (->subsumptive
        ((=daikanwa	    . 01431)	; &M-01431;
 	)
-       ((=ucs@iso	    . #x204E4)	; &U-000204E4;
+       ((==ucs@iso	    . #x204E4)	; &R-U-000204E4;
 	)
        ((=hanziku-1	    . #xBE55)	; &HZK01-BE55;
 	))
