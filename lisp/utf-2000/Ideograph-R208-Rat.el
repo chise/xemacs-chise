765d844dd1b5d0152a9020cb04fb21432cfeab66
MORIOKA Tomohiko <tomo.git@chise.org>
2013-10-16 18:54:33 +0900

    Apply new glyph-image conventions for `ucs@*', `adobe-japan1-*', `jis-x0208', `jis-x0208@*', `gb2312', `iso-ir165', `jis-x0213-*', `jis-x0213-1@*', `hanyo-denshi/*', `gt', `gt-k', `gt-pj-*' and `jef-china3'.

--- lisp/utf-2000/Ideograph-R208-Rat.el
+++ lisp/utf-2000/Ideograph-R208-Rat.el
@@ -355,12 +355,12 @@
     (ideographic-strokes . 4)
     (total-strokes	 . 17)
     (=>>ucs@iso		. #x2A543)	; &G-IU+2A543;
-    (=>>>ucs@iso	. #x2A543)	; &G-IU+2A543;
+    (=ucs@iso		. #x2A543)	; &G-IU+2A543;
     (=>>daikanwa	. 48405)	; &G-IU+2A543;
     (->subsumptive
      ((=daikanwa	  . 48405)	; &M-48405;
       )
-     ((=ucs@iso		  . #x2A543)	; &U-0002A543;
+     ((==ucs@iso	  . #x2A543)	; &R-U-0002A543;
       ))
     ))
 (define-char
@@ -1304,12 +1304,12 @@
     (ideographic-strokes . 8)
     (total-strokes	 . 21)
     (=>>ucs@unicode	. #x4D86)	; &G-UU+4D86;
-    (=>>>ucs@unicode	. #x4D86)	; &G-UU+4D86;
+    (=ucs@unicode	. #x4D86)	; &G-UU+4D86;
     (=>>daikanwa	. 48451)	; &G-UU+4D86;
     (->subsumptive
      ((=daikanwa	  . 48451)	; &M-48451;
       )
-     ((=ucs@unicode	  . #x4D86)	; &UU+4D86;
+     ((==ucs@unicode	  . #x4D86)	; &R-UU+4D86;
       ))
     ))
 (define-char
