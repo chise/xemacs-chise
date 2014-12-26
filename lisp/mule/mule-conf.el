61fab379632a7e455fcd43fa9f72433f1dd56d7b
MORIOKA Tomohiko <tomo.git@chise.org>
2014-12-27 00:45:30 +0900

    (===hanyo-denshi/ip): New coded-charset for XEmacs CHISE.

--- lisp/mule/mule-conf.el
+++ lisp/mule/mule-conf.el
@@ -2020,6 +2020,17 @@
 	       registry "HanyoDenshi\\.HG-0$"
 	       mother ==hanyo-denshi/hg))
   (make-charset
+   '===hanyo-denshi/ip
+   "Han'you-Denshi/IP representative glyph-images."
+   '(long-name "Han'you-Denshi/IP glyph-images"
+	       chars 256
+	       dimension 2
+	       columns 2
+	       graphic 2
+	       direction l2r
+	       registry "HanyoDenshi\\.IP-0$"
+	       mother ==hanyo-denshi/ip))
+  (make-charset
    '===hanyo-denshi/jt
    "JT (Juuki Touitsu Moji) detailed glyph-images."
    '(long-name "Han'you-Denshi/JT glyph-images"
