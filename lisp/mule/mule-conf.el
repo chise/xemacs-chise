e63a3fc2cc2eb57ca4faa3708d3c8ca14a84900a
MORIOKA Tomohiko <tomo.git@chise.org>
2015-09-28 21:32:36 +0900

    (===hng-smk): New coded-charset for XEmacs CHISE.

--- lisp/mule/mule-conf.el
+++ lisp/mule/mule-conf.el
@@ -2941,6 +2941,14 @@
 		  graphic	2
 		  columns	2
 		  direction	l2r))
+  (make-charset '===hng-smk "HNG:西夏版妙法蓮華經卷一"
+		'(long-name "HNG/西夏法華"
+		  chars		256
+		  dimension	2
+		  registry	"hng-74$"
+		  graphic	2
+		  columns	2
+		  direction	l2r))
 
   (make-charset '=cbeta "CBETA private characters"
 		'(long-name	"CBETA PUA"
