6cb85e4d40d34c585b848027cdc474113e6c4b1c
MORIOKA Tomohiko <tomo.git@chise.org>
2015-10-01 14:49:25 +0900

    (default-coded-charset-priority-list): Add `===hng-mam'.

--- lisp/mule/mule-charset.el
+++ lisp/mule/mule-charset.el
@@ -458,6 +458,7 @@ DESCRIPTION (string) is the description string of the charset."
 	  ===hng-jou ; 01
 	  ===hng-keg ; 02
 	  ===hng-dng ; 03
+	  ===hng-mam ; 05
 	  ===hng-khi ; 10
 	  ===hng-khm ; 11
 	  ===hng-kak ; 17
