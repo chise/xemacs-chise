5f3f3ee226a22820fba74c3f2a280e444a1096ef
MORIOKA Tomohiko <tomo.git@chise.org>
2015-10-08 13:17:23 +0900

    (default-coded-charset-priority-list): Add `===hng-zkd'.

--- lisp/mule/mule-charset.el
+++ lisp/mule/mule-charset.el
@@ -484,6 +484,7 @@ DESCRIPTION (string) is the description string of the charset."
 	  ===hng-wan ; 49
 	  ===hng-okd ; 50
 	  ===hng-kmi ; 55
+	  ===hng-zkd ; 56
 	  ===hng-kss ; 64
 	  ===hng-kyo ; 66
 	  ===hng-smk ; 74
