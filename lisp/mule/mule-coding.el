b42a1863604edfae8b5007def671c9b2498eddb0
MORIOKA Tomohiko <tomo.git@chise.org>
2015-10-11 15:04:19 +0900

    (coded-charset-entity-reference-alist): Add setting for `===hng-kyd'.

--- lisp/mule/mule-coding.el
+++ lisp/mule/mule-coding.el
@@ -372,6 +372,7 @@
 	  (===hng-khi			      "HNG010-" 5 d)
 	  (===hng-khm			      "HNG011-" 5 d)
 	  (===hng-hok			      "HNG013-" 5 d)
+	  (===hng-kyd			      "HNG014-" 5 d)
 	  (===hng-sok			      "HNG015-" 5 d)
 	  (===hng-yhk			      "HNG016-" 5 d)
 	  (===hng-kak			      "HNG017-" 5 d)
