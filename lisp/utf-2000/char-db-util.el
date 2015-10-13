0c7ec305ab4467dc334fa968ba751a54d5fb6d96
MORIOKA Tomohiko <tomo.git@chise.org>
2015-10-13 10:42:10 +0900

    (char-db-insert-ccs-feature): Format `===hng-kae' as decimal.

--- lisp/utf-2000/char-db-util.el
+++ lisp/utf-2000/char-db-util.el
@@ -684,7 +684,7 @@
 		 ===hng-jou ===hng-keg ===hng-dng ===hng-mam
 		 ===hng-drt ===hng-kgk ===hng-myz ===hng-kda
 		 ===hng-khi ===hng-khm ===hng-hok ===hng-kyd ===hng-sok
-		 ===hng-yhk ===hng-kak ===hng-kar
+		 ===hng-yhk ===hng-kak ===hng-kar ===hng-kae
 		 ===hng-sys ===hng-tsu
 		 ===hng-hod ===hng-gok ===hng-nto
 		 ===hng-nkm ===hng-k24
