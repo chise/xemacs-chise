4ac0d2abcd358234ddca702bdaef453a506bcc0a
MORIOKA Tomohiko <tomo.git@chise.org>
2015-10-15 12:22:02 +0900

    (default-coded-charset-priority-list): Add `===hng-hos'.

--- lisp/mule/mule-charset.el
+++ lisp/mule/mule-charset.el
@@ -475,6 +475,7 @@ DESCRIPTION (string) is the description string of the charset."
 	  ===hng-sys ; 22
 	  ===hng-tsu ; 24
 	  ===hng-tzj ; 25
+	  ===hng-hos ; 26
 	  ===hng-hod ; 30
 	  ===hng-gok ; 31
 	  ===hng-nto ; 34
