616974b8e81a24a4771aff0cfa0dcf6e520d02cf
MORIOKA Tomohiko <tomo.git@chise.org>
2016-03-01 04:38:39 +0900

    Reformatted.

--- lisp/utf-2000/Ideograph-R015-Ice.el
+++ lisp/utf-2000/Ideograph-R015-Ice.el
@@ -581,10 +581,14 @@
     (=shinjigen		.  4081)	; 决
     (=cbeta		. 00804)	; &I-CB00804;
     (<-vulgar
-     ((=ucs		  . #x6C7A)	; 決
-      (=jis-x0208	  . #x3768)	; &I-J90-3768; [23-72]
+     ((=>ucs@iwds-1	  . #x6C7A)	; &GT-21966;
+      (=ucs@unicode	  . #x6C7A)	; &GT-21966;
+      (=jis-x0208	  . #x3768)	; &GT-21966; [23-72]
+      (=ks-x1001	  . #x4C3D)	; &I-K0-4C3D; [44-29]
+      (=cns11643-1	  . #x4A6E)	; &I-C1-4A6E; [42-78]
+      (=jis-x0213-1	  . #x3768)	; &I-JX1-3768; [23-72]
       (=daikanwa	  . 17174)	; &I-M-17174;
-      (=shinjigen	  .  4080)	; 決
+      (=shinjigen	  .  4080)	; &GT-21966;
       ))
     (<-vulgar*sources
      yupian daikanwa shinjigen@1ed)
