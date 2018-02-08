f8e8d09357d5c9e3b8cc4c96521e9a7614655768
MORIOKA Tomohiko <tomo.git@chise.org>
2018-02-08 23:51:06 +0900

    (BUCS+4E1F): Separate B-A5E1. (B-A5E1): Separate BUCS+4E1F; add `<-denotational' for BUCS+4E1F; unify A-IWDSU+4E1F. (U+4E1F): Add representative nodes. (U+4E22): Apply new conventions for glyph granularity.

--- lisp/utf-2000/Ideograph-R001-One.el
+++ lisp/utf-2000/Ideograph-R001-One.el
@@ -2893,9 +2893,8 @@
       )
      ((=ucs		  . #x4E00)	; 一
       )
-     ((==>ucs@bucs	  . #x5167)	; &J97-4662;
+     ((=>ucs@iwds-1	  . #x5167)	; &J97-4662;
       (=>ucs@jis	  . #x5185)	; &J97-4662;
-      (=ucs@big5	  . #x5167)	; &J97-4662;
       (=>jis-x0208	  . #x4662)	; &J97-4662; [38-66]
       ))
     (sound@ja/kun	"hinoe")
@@ -3485,13 +3484,20 @@
 (define-char
   '((ideographic-radical . 1)	; ⼀
     (==>ucs@bucs	. #x4E1F)	; &BUCS+4E1F;
-    (=ucs@big5		. #x4E1F)	; &BUCS+4E1F;
-    (=big5		. #xA5E1)	; &BUCS+4E1F;
     ))
 (define-char
   '((<-denotational
      ((==>ucs@bucs	  . #x4E1F)	; &BUCS+4E1F;
-      (=ucs@big5	  . #x4E1F)	; &BUCS+4E1F;
+      ))
+    (ideographic-radical . 1)	; ⼀
+    (=>ucs@iwds-1	. #x4E1F)	; &A-IWDSU+4E1F;
+    (=ucs@big5		. #x4E1F)	; &A-IWDSU+4E1F;
+    (=big5		. #xA5E1)	; &A-IWDSU+4E1F;
+    ))
+(define-char
+  '((<-denotational
+     ((=>ucs@iwds-1	  . #x4E1F)	; &A-IWDSU+4E1F;
+      (=big5		  . #xA5E1)	; &A-IWDSU+4E1F;
       ))
     (ideographic-radical . 1)	; ⼀
     (ideographic-strokes . 5)
@@ -3513,6 +3519,29 @@
     (=gt-k		. 02153)	; &I-GT-K02153;
     (=gt-pj-2		. #x303B)	; &I-GT-00061; [16-27]
     (=daikanwa		. 00041)	; &I-M-00041;
+    (->subsumptive
+     ((==ucs@unicode	  . #x4E1F)	; &g2-AJ1-14298;
+      (==adobe-japan1-4	  . 14298)	; &g2-AJ1-14298;
+      (==jis-x0212	  . #x3026)	; &g2-AJ1-14298; [16-06]
+      (==cns11643-1	  . #x4723)	; &g2-AJ1-14298; [39-03]
+      (==gt		  . 00061)	; &g2-AJ1-14298;
+      (==gt-k		  . 02153)	; &g2-AJ1-14298;
+      (==daikanwa	  . 00041)	; &g2-AJ1-14298;
+      (->subsumptive
+       ((===daikanwa	    . 00041)	; &R-M-00041;
+	)
+       ((===jis-x0212	    . #x3026)	; &R-JSP-3026; [16-06]
+	)
+       ((===adobe-japan1-4  . 14298)	; &R-AJ1-14298;
+	)
+       ((===cns11643-1	    . #x4723)	; &R-C1-4723; [39-03]
+	)
+       ((===gt		    . 00061)	; &R-GT-00061;
+	(===gt-k	    . 02153)	; &R-GT-00061;
+	)
+       ((===ucs@unicode	    . #x4E1F)	; &R-UU+4E1F;
+	))
+      ))
     ))
 (define-char
   '((ideographic-radical . 1)	; ⼀
@@ -3702,8 +3731,8 @@
     ))
 (define-char
   '((<-denotational
-     ((==>ucs@bucs	  . #x4E1F)	; &BUCS+4E1F;
-      (=ucs@big5	  . #x4E1F)	; &BUCS+4E1F;
+     ((=>ucs@iwds-1	  . #x4E1F)	; &A-IWDSU+4E1F;
+      (=big5		  . #xA5E1)	; &A-IWDSU+4E1F;
       ))
     (ideographic-radical@ucs . 1)	; ⼀
     (ideographic-radical@cns . 4)	; ⼃
@@ -3725,8 +3754,30 @@
     (=gt-k		. 02131)	; &I-GT-K02131;
     (=gt-pj-2		. #x3132)	; &I-GT-00193; [17-18]
     (=daikanwa		. 00144)	; &I-M-00144;
-    (=hanziku-1		. #xF56D)	; &I-HZK01-F56D;
     (=jef-china3	. #x41BA)	; &I-JC3-41BA;
+    (->subsumptive
+     ((==ucs@unicode	  . #x4E22)	; &g2-GT-00193;
+      (==gb2312		  . #x362A)	; &g2-GT-00193; [22-10]
+      (==cns11643-3	  . #x2263)	; &g2-GT-00193; [02-67]
+      (==gt		  . 00193)	; &g2-GT-00193;
+      (==gt-k		  . 02131)	; &g2-GT-00193;
+      (==daikanwa	  . 00144)	; &g2-GT-00193;
+      (==jef-china3	  . #x41BA)	; &g2-GT-00193;
+      (->subsumptive
+       ((===daikanwa	    . 00144)	; &R-M-00144;
+	)
+       ((===cns11643-3	    . #x2263)	; &R-C3-2263; [02-67]
+	)
+       ((===gt		    . 00193)	; &R-GT-00193;
+	(===gt-k	    . 02131)	; &R-GT-00193;
+	)
+       ((===jef-china3	    . #x41BA)	; &R-JC3-41BA;
+	)
+       ((===ucs@unicode	    . #x4E22)	; &R-UU+4E22;
+	)
+       ((=hanziku-1	    . #xF56D)	; &HZK01-F56D;
+	))
+      ))
     ))
 (define-char
   '((ideographic-radical@ucs . 1)	; ⼀
