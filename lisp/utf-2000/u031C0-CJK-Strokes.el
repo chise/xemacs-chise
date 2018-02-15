c3cdc86b5d30f317c4e6f020c3625599eef34cde
MORIOKA Tomohiko <tomo.git@chise.org>
2018-02-15 20:55:54 +0900

    New file.

---
+++ lisp/utf-2000/u031C0-CJK-Strokes.el
@@ -0,0 +1,253 @@
+;; -*- coding: utf-8-mcs-er -*-
+(define-char
+  '((name		. "CJK STROKE T")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31C0)	; ㇀
+    ))
+(define-char
+  '((name		. "CJK STROKE WG")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31C1)	; ㇁
+    ))
+(define-char
+  '((name		. "CJK STROKE XG")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31C2)	; ㇂
+    ))
+(define-char
+  '((name		. "CJK STROKE BXG")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31C3)	; ㇃
+    ))
+(define-char
+  '((name		. "CJK STROKE SW")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31C4)	; ㇄
+    ))
+(define-char
+  '((name		. "CJK STROKE HZZ")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31C5)	; ㇅
+    ))
+(define-char
+  '((name		. "CJK STROKE HZG")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31C6)	; ㇆
+    ))
+(define-char
+  '((name		. "CJK STROKE HP")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31C7)	; ㇇
+    ))
+(define-char
+  '((name		. "CJK STROKE HZWG")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31C8)	; ㇈
+    ))
+(define-char
+  '((name		. "CJK STROKE SZWG")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31C9)	; ㇉
+    ))
+(define-char
+  '((name		. "CJK STROKE HZT")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31CA)	; ㇊
+    ))
+(define-char
+  '((name		. "CJK STROKE HZZP")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31CB)	; ㇋
+    ))
+(define-char
+  '((name		. "CJK STROKE HPWG")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31CC)	; ㇌
+    ))
+(define-char
+  '((name		. "CJK STROKE HZW")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31CD)	; ㇍
+    ))
+(define-char
+  '((name		. "CJK STROKE HZZZ")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31CE)	; ㇎
+    ))
+(define-char
+  '((name		. "CJK STROKE N")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31CF)	; ㇏
+    ))
+(define-char
+  '((name		. "CJK STROKE H")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31D0)	; ㇐
+    ))
+(define-char
+  '((name		. "CJK STROKE S")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31D1)	; ㇑
+    ))
+(define-char
+  '((name		. "CJK STROKE P")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31D2)	; ㇒
+    ))
+(define-char
+  '((name		. "CJK STROKE SP")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31D3)	; ㇓
+    ))
+(define-char
+  '((name		. "CJK STROKE D")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31D4)	; ㇔
+    ))
+(define-char
+  '((name		. "CJK STROKE HZ")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31D5)	; ㇕
+    ))
+(define-char
+  '((name		. "CJK STROKE HG")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31D6)	; ㇖
+    ))
+(define-char
+  '((name		. "CJK STROKE SZ")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31D7)	; ㇗
+    ))
+(define-char
+  '((name		. "CJK STROKE SWZ")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31D8)	; ㇘
+    ))
+(define-char
+  '((name		. "CJK STROKE ST")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31D9)	; ㇙
+    ))
+(define-char
+  '((name		. "CJK STROKE SG")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31DA)	; ㇚
+    ))
+(define-char
+  '((name		. "CJK STROKE PD")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31DB)	; ㇛
+    ))
+(define-char
+  '((name		. "CJK STROKE PZ")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31DC)	; ㇜
+    ))
+(define-char
+  '((name		. "CJK STROKE TN")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31DD)	; ㇝
+    ))
+(define-char
+  '((name		. "CJK STROKE SZZ")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31DE)	; ㇞
+    ))
+(define-char
+  '((name		. "CJK STROKE SWG")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31DF)	; ㇟
+    ))
+(define-char
+  '((name		. "CJK STROKE HXWG")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31E0)	; ㇠
+    ))
+(define-char
+  '((name		. "CJK STROKE HZZZG")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31E1)	; ㇡
+    ))
+(define-char
+  '((name		. "CJK STROKE PG")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31E2)	; ㇢
+    ))
+(define-char
+  '((name		. "CJK STROKE Q")
+    (general-category	symbol other) ; Informative Category
+    (bidi-category	. "ON")
+    (mirrored		. nil)
+    (=ucs		. #x31E3)	; ㇣
+    ))
