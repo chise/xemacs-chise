a9cb3bc3ec8463b190f60256ee176a573e12b24a
MORIOKA Tomohiko <tomo.git@chise.org>
2019-01-07 00:11:35 +0900

    Update Copyright header.

--- lisp/mule/mule-charset.el
+++ lisp/mule/mule-charset.el
@@ -4,7 +4,7 @@
 ;; Copyright (C) 1995 Amdahl Corporation.
 ;; Copyright (C) 1996 Sun Microsystems.
 ;; Copyright (C) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2007, 2008,
-;;   2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018
+;;   2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019
 ;;   MORIOKA Tomohiko
 
 ;; Author: Unknown
