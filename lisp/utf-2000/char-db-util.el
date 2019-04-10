b35e1f673814cec1034413c83d0860326aa58279
MORIOKA Tomohiko <tomo.git@chise.org>
2019-04-11 01:32:19 +0900

    (char-db-insert-ccs-feature): Format `===hng-kad' as decimal.

--- lisp/utf-2000/char-db-util.el
+++ lisp/utf-2000/char-db-util.el
@@ -752,7 +752,7 @@
 		 ===hng-skk ===hng-kyu ===hng-ksk ===hng-wan
 		 ===hng-okd ===hng-wad ===hng-kmi
 		 ===hng-zkd ===hng-doh ===hng-jyu ===hng-tzs
-		 ===hng-sai ===hng-kss ===hng-kyo ===hng-ykk
+		 ===hng-sai ===hng-kad ===hng-kss ===hng-kyo ===hng-ykk
 		 ===hng-sab ===hng-wks ===hng-wke ===hng-smk
 		 =shuowen-jiguge ===shuowen-jiguge4 ===shuowen-jiguge5))
 	 ;; (string-match "^=adobe-" (symbol-name name))
