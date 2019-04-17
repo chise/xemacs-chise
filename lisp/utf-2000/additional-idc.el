7ede3ec59dce0105368a2b8f97b1e745a8edea9c
MORIOKA Tomohiko <tomo.git@chise.org>
2019-04-17 16:26:38 +0900

    New file.

---
+++ lisp/utf-2000/additional-idc.el
@@ -0,0 +1,35 @@
+;; -*- coding: utf-8-mcs-er -*-
+(define-char
+  '((=>ucs@iwds-1	. #x2FFA)	; &A-IWDSU+2FFA;
+    (=>iwds-1		.  0307)	; &A-IWDSU+2FFA;
+    ))
+(define-char
+  '((=>ucs@component	. #x2FF0)	; &U-v001+2FF0;
+    (=ucs-var-001	. #x2FF0)	; &U-v001+2FF0;
+    ))
+(define-char
+  '((=>ucs@iwds-1	. #x2FF8)	; &U-v002+2FF1;
+    (=>iwds-1		.  0305)	; &U-v002+2FF1;
+    (=ucs-var-002	. #x2FF1)	; &U-v002+2FF1;
+    ))
+(define-char
+  '((=>ucs@component	. #x2FF5)	; &U-v001+2FF1;
+    (=ucs-var-001	. #x2FF1)	; &U-v001+2FF1;
+    ))
+(define-char
+  '((=ucs-var-003	. #x2FF1)	; &U-v003+2FF1;
+    ))
+(define-char
+  '((=ucs-itaiji-001	. #x2FF6)	; &U-i001+2FF6;
+    (<-denotational@arg-reversed
+     ((=>ucs@component	  . #x2FF0)	; &U-v001+2FF0;
+      (=ucs-var-001	  . #x2FF0)	; &U-v001+2FF0;
+      ))
+    ))
+(define-char
+  '((<-denotational
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER SURROUND FROM UPPER RIGHT")
+      (=ucs		  . #x2FF9)	; ⿹
+      ))
+    (=ucs-itaiji-001	. #x2FF9)	; &U-i001+2FF9;
+    ))
