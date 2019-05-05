a6cec976773feae90733b002e7c43fcde8313b17
MORIOKA Tomohiko <tomo.git@chise.org>
2019-05-05 16:57:05 +0900

    (U-000223A6): Use A-IWDSU+4EBA instead of IWDS1-0146 in `ideographic-structure'.

--- lisp/utf-2000/Ideograph-R057-Bow.el
+++ lisp/utf-2000/Ideograph-R057-Bow.el
@@ -687,8 +687,8 @@
      ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW")
       (=ucs		  . #x2FF1)	; ⿱
       )
-     ((=>ucs@component	  . #x4EBA)	; &A-compU+4EBA;
-      (=>iwds-1		  .  0146)	; &A-compU+4EBA;
+     ((=>ucs@iwds-1	  . #x4EBA)	; &A-IWDSU+4EBA;
+      (=>iwds-1		  .  14623)	; &A-IWDSU+4EBA;
       )
      ((=ucs		  . #x5F13)	; 弓
       ))
