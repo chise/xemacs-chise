0533d7d4eef98174ad4a5da9aca312267ef66de5
MORIOKA Tomohiko <tomo.git@chise.org>
2019-05-09 12:45:07 +0900

    (U-0002D37A): New character.

--- lisp/utf-2000/Ideograph-R032-Earth.el
+++ lisp/utf-2000/Ideograph-R032-Earth.el
@@ -643,6 +643,42 @@
   '((ideographic-radical . 32)	; ⼟
     (ideographic-strokes . 2)
     (total-strokes	 . 5)
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW")
+      (=ucs		  . #x2FF1)	; ⿱
+      )
+     ((=>ucs@iwds-1	  . #x4EBA)	; &A-IWDSU+4EBA;
+      (=>iwds-1		  .  14623)	; &A-IWDSU+4EBA;
+      )
+     ((=>ucs@iwds-1	  . #x571F)	; &A-IWDSU+571F;
+      (=>iwds-1		  .  0312)	; &A-IWDSU+571F;
+      ))
+    (=ucs		. #x2D37A)	; 𭍺
+    ))
+(define-char
+  '((<-denotational
+     ((=ucs		  . #x2D37A)	; 𭍺
+      ))
+    (ideographic-radical . 32)	; ⼟
+    (ideographic-strokes . 2)
+    (total-strokes	 . 5)
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW")
+      (=ucs		  . #x2FF1)	; ⿱
+      )
+     ((=ucs		  . #x5165)	; 入
+      )
+     ((=ucs		  . #x571F)	; 土
+      ))
+    (=ucs@JP/hanazono	. #x2D37A)	; &hanaJU+2D37A;
+    (->subsumptive
+     ((==ucs@JP/hanazono  . #x2D37A)	; &g2-hanaJU+2D37A;
+      ))
+    ))
+(define-char
+  '((ideographic-radical . 32)	; ⼟
+    (ideographic-strokes . 2)
+    (total-strokes	 . 5)
     (=>big5-cdp		. #x8A65)	; &A-CDP-8A65;
     ))
 (define-char
