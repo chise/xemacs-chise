940e354a671314ffce3dae1ff118fd98987b26a1
MORIOKA Tomohiko <tomo.git@chise.org>
2019-12-01 12:48:05 +0900

    (coded-charset-entity-reference-alist): Add setting for `=>ucs-itaiji-002@iwds-1'.

--- lisp/mule/mule-coding.el
+++ lisp/mule/mule-coding.el
@@ -648,6 +648,7 @@
 	  (=>big5-cdp-itaiji-003	  "A-CDP-i003-" 4 X)
 	  (=>big5-cdp-var-001		  "A-CDP-v001-" 4 X)
 	  (=>ucs-itaiji-001@iwds-1	"A-IWDSU-i001+" 4 X)
+	  (=>ucs-itaiji-002@iwds-1	"A-IWDSU-i002+" 4 X)
 	  (=>big5-cdp@iwds-1		   "A-IWDSCDP-" 4 X)
 	  (=>big5-cdp@component		   "A-compCDP-" 4 X)
 	  (=>big5-cdp@cognate		    "A-cgnCDP-" 4 X)
