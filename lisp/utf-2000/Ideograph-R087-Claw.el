167bc19b6c6d4c4b69e7ecd5a71363536ad22209
MORIOKA Tomohiko <tomo.git@chise.org>
2020-02-06 01:27:43 +0900

    (g2-RUI6-E030): Omit `ideographic-structure'. (hanaJU+2C2F4): Separate RUI6-E158.

--- lisp/utf-2000/Ideograph-R087-Claw.el
+++ lisp/utf-2000/Ideograph-R087-Claw.el
@@ -2345,17 +2345,7 @@
       ))
     (=ruimoku-v6	. #xE030)	; &RUI6-E030;
     (->subsumptive
-     ((ideographic-structure
-       ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW")
-	(=ucs		    . #x2FF1)	; ⿱
-	)
-       ((name		    . "CJK COMPATIBILITY IDEOGRAPH-FA49")
-	(=ucs		    . #xFA49)	; 爫
-	)
-       ((==ucs@unicode	    . #x8863)	; &g2-AJ1-01189;
-	(==adobe-japan1-0   . 01189)	; &g2-AJ1-01189;
-	))
-      (==ruimoku-v6	  . #xE030)	; &g2-RUI6-E030;
+     ((==ruimoku-v6	  . #xE030)	; &g2-RUI6-E030;
       ))
     ))
 (define-char
@@ -3876,11 +3866,20 @@
     (ideographic-radical . 87)	; ⽖
     (ideographic-strokes . 12)
     (total-strokes	 . 16)
-    (=ucs@JP/hanazono	. #x2C2F4)	; &RUI6-E158;
-    (=ruimoku-v6	. #xE158)	; &RUI6-E158;
+    (=ucs@JP/hanazono	. #x2C2F4)	; &hanaJU+2C2F4;
     (->subsumptive
      ((==ucs@JP/hanazono  . #x2C2F4)	; &g2-hanaJU+2C2F4;
-      )
+      ))
+    ))
+(define-char
+  '((<-denotational
+     ((=ucs		  . #x2C2F4)	; 𬋴
+      ))
+    (ideographic-radical . 87)	; ⽖
+    (ideographic-strokes . 12)
+    (total-strokes	 . 16)
+    (=ruimoku-v6	. #xE158)	; &RUI6-E158;
+    (->subsumptive
      ((==ruimoku-v6	  . #xE158)	; &g2-RUI6-E158;
       ))
     ))
