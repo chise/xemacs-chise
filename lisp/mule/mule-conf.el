233d4bd8166a2a11006d1fd8b1bba131f31367e9
MORIOKA Tomohiko <tomo.git@chise.org>
2020-07-26 12:24:20 +0900

    (=>ucs-itaiji-003@iwds-1): New coded-charset for XEmacs CHISE. (=>ucs-bmp-itaiji-003@iwds-1): New coded-charset for XEmacs CHISE.

--- lisp/mule/mule-conf.el
+++ lisp/mule/mule-conf.el
@@ -5429,6 +5429,29 @@
 		  mother	==ucs-itaiji-003
 		  min-code #x20000 max-code #x2FFFF
 		  registry "ucs00\\.itaiji003-2"))
+  (make-charset '=>ucs-itaiji-003@iwds-1
+		"uXXXX-itaiji-003's abstract characters based on IWDS-1"
+		'(long-name	"IWDS-1 abstract characters of UCS-itaiji-003"
+		  chars		256
+		  dimension	3
+		  columns	2
+		  graphic	2
+		  direction	l2r
+                  ;; =>iso-ir	177
+		  mother	=>ucs-itaiji-003))
+  (make-charset '=>ucs-bmp-itaiji-003@iwds-1
+		"IWDS-1 abstract characters of UCS-itaiji-003 in BMP"
+		'(long-name	"=>ucs-itaiji-003@iwds-1 in BMP"
+		  chars		256
+		  dimension	2
+		  columns	2
+		  graphic	2
+		  direction	l2r
+                  ;; =>iso-ir	177
+		  mother	=>ucs-itaiji-003@iwds-1
+		  min-code #x2E00
+		  max-code #x9FCC
+		  registry "ucs\\.itaiji003-0"))
 
   (make-charset '=ucs-itaiji-004
 		"uXXXX-itaiji-004 glyphs of GlyphWiki"
