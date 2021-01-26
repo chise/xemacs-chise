c052195f12cca06eb40b498b5f79121533b76aed
MORIOKA Tomohiko <tomo.git@chise.org>
2021-01-26 16:29:20 +0900

    (coded-charset-entity-reference-alist): Add setting for `=>>ruimoku-v6'.

--- lisp/mule/mule-coding.el
+++ lisp/mule/mule-coding.el
@@ -5,8 +5,8 @@
 ;; Copyright (C) 1995 Amdahl Corporation.
 ;; Copyright (C) 1995 Sun Microsystems.
 ;; Copyright (C) 1997, 1999, 2002, 2003, 2004, 2005, 2006, 2008, 2009,
-;;   2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020
-;;   MORIOKA Tomohiko
+;;   2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020,
+;;   2021 MORIOKA Tomohiko
 
 ;; This file is part of XEmacs.
 
@@ -536,6 +536,7 @@
 	  (=>>big5-cdp			       "G-CDP-" 4 X)
 	  (=>>gt-k			       "G-GT-K" 5 d)
 	  (=>>cbeta				 "G-CB" 5 d)
+	  (=>>ruimoku-v6		      "G-RUI6-" 4 X)
 	  (=+>jis-x0208				"o-J0-" 4 X)
 	  (=+>jis-x0213-1		       "o-JX1-" 4 X)
 	  (=+>jis-x0213-2		       "o-JX2-" 4 X)
