2ab9d608a0b35f5ec716d80ba063471ea923127f
MORIOKA Tomohiko <tomo.git@chise.org>
2021-05-30 13:53:01 +0900

    Add `ideographic-structure' for some ShuoWen's characters.

--- lisp/utf-2000/ShuoWen-SR001.el
+++ lisp/utf-2000/ShuoWen-SR001.el
@@ -13,6 +13,14 @@
     ))
 (define-char
   '((shuowen-radical	. 1)	; 一
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER SURROUND FROM UPPER RIGHT")
+      (=ucs		  . #x2FF9)	; ⿹
+      )
+     ((=shuowen-jiguge	  . 43809)	; &SW-JIGUGE-43809;
+      )
+     ((=shuowen-jiguge	  . 00100)	; &SW-JIGUGE-00100;
+      ))
     (=shuowen-jiguge	. 00200)	; &SW-JIGUGE-00200;
     (<-Small-Seal@shuowen
      ((=ucs		  . #x5F0C)	; 弌
@@ -25,6 +33,14 @@
     ))
 (define-char
   '((shuowen-radical	. 1)	; 一
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW")
+      (=ucs		  . #x2FF1)	; ⿱
+      )
+     ((=shuowen-jiguge	  . 00100)	; &SW-JIGUGE-00100;
+      )
+     ((=shuowen-jiguge	  . 28901)	; &SW-JIGUGE-28901;
+      ))
     (=shuowen-jiguge	. 00201)	; &SW-JIGUGE-00201;
     (<-Small-Seal@shuowen
      ((=ucs		  . #x5143)	; 元
@@ -37,6 +53,14 @@
     ))
 (define-char
   '((shuowen-radical	. 1)	; 一
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW")
+      (=ucs		  . #x2FF1)	; ⿱
+      )
+     ((=shuowen-jiguge	  . 00100)	; &SW-JIGUGE-00100;
+      )
+     ((=shuowen-jiguge	  . 35403)	; &SW-JIGUGE-35403;
+      ))
     (=shuowen-jiguge	. 00202)	; &SW-JIGUGE-00202;
     (<-Small-Seal@shuowen
      ((=ucs		  . #x5929)	; 天
@@ -49,6 +73,14 @@
     ))
 (define-char
   '((shuowen-radical	. 1)	; 一
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER SURROUND FROM ABOVE")
+      (=ucs		  . #x2FF5)	; ⿵
+      )
+     ((=shuowen-jiguge	  . 41103)	; &SW-JIGUGE-41103;
+      )
+     ((=shuowen-jiguge	  . 00100)	; &SW-JIGUGE-00100;
+      ))
     (=shuowen-jiguge	. 00203)	; &SW-JIGUGE-00203;
     (<-Small-Seal@shuowen
      ((=ucs		  . #x4E15)	; 丕
@@ -61,6 +93,14 @@
     ))
 (define-char
   '((shuowen-radical	. 1)	; 一
+    (ideographic-structure
+     ((name		  . "IDEOGRAPHIC DESCRIPTION CHARACTER OVERLAID")
+      (=ucs		  . #x2FFB)	; ⿻
+      )
+     ((=shuowen-jiguge	  . 00100)	; &SW-JIGUGE-00100;
+      )
+     ((=shuowen-jiguge	  . 09201)	; &SW-JIGUGE-09201;
+      ))
     (=shuowen-jiguge	. 00204)	; &SW-JIGUGE-00204;
     (<-Small-Seal@shuowen
      ((=ucs		  . #x53D3)	; 叓
