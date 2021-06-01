72e278e1f7a17e3a7c0f86f234f9bbab658e388c
MORIOKA Tomohiko <tomo.git@chise.org>
2021-06-01 09:14:21 +0900

    Add `ideographic-structure' for some ShuoWen's characters. (SW-JIGUGE-01215): Add U+73CF as a corresponding modern character. (SW-JIGUGE-01216): Add `<-formed@shuowen' for SW-JIGUGE-01215.

--- lisp/utf-2000/ShuoWen-SR007.el
+++ lisp/utf-2000/ShuoWen-SR007.el
@@ -1,8 +1,18 @@
 ;; -*- coding: utf-8-mcs-er -*-
 (define-char
   '((shuowen-radical	. 7)	; 玨
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER LEFT TO RIGHT")
+      (=ucs		  . #x2FF0)	; ⿰
+      )
+     ((=shuowen-jiguge	  . 00605)	; &SW-JIGUGE-00605;
+      )
+     ((=shuowen-jiguge	  . 00605)	; &SW-JIGUGE-00605;
+      ))
     (=shuowen-jiguge	. 01215)	; &SW-JIGUGE-01215;
     (<-Small-Seal@shuowen
+     ((=ucs		  . #x73CF)	; 珏
+      )
      ((=ucs		  . #x73A8)	; 玨
       ))
     (->subsumptive
@@ -13,9 +23,22 @@
     ))
 (define-char
   '((shuowen-radical	. 7)	; 玨
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER SURROUND FROM UPPER RIGHT")
+      (=ucs		  . #x2FF9)	; ⿹
+      )
+     ((=shuowen-jiguge	  . 09412)	; &SW-JIGUGE-09412;
+      )
+     ((=shuowen-jiguge	  . 00605)	; &SW-JIGUGE-00605;
+      ))
     (=shuowen-jiguge	. 01216)	; &SW-JIGUGE-01216;
     (<-Small-Seal@shuowen
      ((=ucs		  . #x7474)	; 瑴
+      ))
+    (<-formed@shuowen
+     ((=shuowen-jiguge	  . 01215)	; &SW-JIGUGE-01215;
+      )
+     ((=ucs		  . #x73CF)	; 珏
       ))
     (->subsumptive
      ((===shuowen-jiguge5 . 01216)	; &SW-JIGUGE5-01216;
@@ -25,6 +48,14 @@
     ))
 (define-char
   '((shuowen-radical	. 7)	; 玨
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER FULL SURROUND")
+      (=ucs		  . #x2FF4)	; ⿴
+      )
+     ((=shuowen-jiguge	  . 01215)	; &SW-JIGUGE-01215;
+      )
+     ((=shuowen-jiguge	  . 14010)	; &SW-JIGUGE-14010;
+      ))
     (=shuowen-jiguge	. 01217)	; &SW-JIGUGE-01217;
     (<-Small-Seal@shuowen
      ((=ucs		  . #x73ED)	; 班
@@ -37,6 +68,14 @@
     ))
 (define-char
   '((shuowen-radical	. 7)	; 玨
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER FULL SURROUND")
+      (=ucs		  . #x2FF4)	; ⿴
+      )
+     ((=shuowen-jiguge	  . 01215)	; &SW-JIGUGE-01215;
+      )
+     ((=shuowen-jiguge	  . 49806)	; &SW-JIGUGE-49806;
+      ))
     (=shuowen-jiguge	. 01218)	; &SW-JIGUGE-01218;
     (<-Small-Seal@shuowen
      ((=ucs		  . #x28325)	; 𨌥
