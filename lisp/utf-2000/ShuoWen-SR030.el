5b6b81f0fa3274bb7311dafdaf53d8777531ea43
MORIOKA Tomohiko <tomo.git@chise.org>
2022-10-04 14:18:25 +0900

    Add `===shuowen-jiguge-A30' characters.

--- lisp/utf-2000/ShuoWen-SR030.el
+++ lisp/utf-2000/ShuoWen-SR030.el
@@ -6,6 +6,8 @@
      ((=ucs		  . #x6B64)	; 此
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 04900)	; &SW-JIGUGE30-04900;
+      )
      ((===shuowen-jiguge5 . 05000)	; &SW-JIGUGE5-05000;
       )
      ((===shuowen-jiguge4 . 04900)	; &SW-JIGUGE4-04900;
@@ -18,6 +20,8 @@
      ((=ucs		  . #x5559)	; 啙
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 04901)	; &SW-JIGUGE30-04901;
+      )
      ((===shuowen-jiguge5 . 05001)	; &SW-JIGUGE5-05001;
       )
      ((===shuowen-jiguge4 . 04901)	; &SW-JIGUGE4-04901;
@@ -30,6 +34,8 @@
      ((=ucs		  . #x3B70)	; 㭰
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 04902)	; &SW-JIGUGE30-04902;
+      )
      ((===shuowen-jiguge5 . 05002)	; &SW-JIGUGE5-05002;
       )
      ((===shuowen-jiguge4 . 04902)	; &SW-JIGUGE4-04902;
@@ -42,6 +48,8 @@
      ((=ucs		  . #x4E9B)	; 些
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 04903)	; &SW-JIGUGE30-04903;
+      )
      ((===shuowen-jiguge5 . 05003)	; &SW-JIGUGE5-05003;
       )
      ((===shuowen-jiguge4 . 04903)	; &SW-JIGUGE4-04903;
