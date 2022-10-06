123285b0a39e0cfca2a6b414384dca1143683d22
MORIOKA Tomohiko <tomo.git@chise.org>
2022-10-06 19:32:28 +0900

    Add `===shuowen-jiguge-A30' characters.

--- lisp/utf-2000/ShuoWen-SR090.el
+++ lisp/utf-2000/ShuoWen-SR090.el
@@ -8,6 +8,8 @@
      ((=ucs		  . #x76AE)	; 皮
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 10109)	; &SW-JIGUGE30-10109;
+      )
      ((===shuowen-jiguge5 . 09609)	; &SW-JIGUGE5-09609;
       )
      ((===shuowen-jiguge4 . 09509)	; &SW-JIGUGE4-09509;
@@ -29,6 +31,8 @@
      ((=ucs		  . #x76AE)	; 皮
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 10110)	; &SW-JIGUGE30-10110;
+      )
      ((===shuowen-jiguge5 . 09610)	; &SW-JIGUGE5-09610;
       )
      ((===shuowen-jiguge4 . 09510)	; &SW-JIGUGE4-09510;
@@ -50,6 +54,8 @@
      ((=ucs		  . #x76AE)	; 皮
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 10111)	; &SW-JIGUGE30-10111;
+      )
      ((===shuowen-jiguge5 . 09611)	; &SW-JIGUGE5-09611;
       )
      ((===shuowen-jiguge4 . 09511)	; &SW-JIGUGE4-09511;
@@ -62,6 +68,8 @@
      ((=ucs		  . #x76B0)	; 皰
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 10112)	; &SW-JIGUGE30-10112;
+      )
      ((===shuowen-jiguge5 . 09612)	; &SW-JIGUGE5-09612;
       )
      ((===shuowen-jiguge4 . 09512)	; &SW-JIGUGE4-09512;
@@ -74,6 +82,8 @@
      ((=ucs		  . #x76AF)	; 皯
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 10113)	; &SW-JIGUGE30-10113;
+      )
      ((===shuowen-jiguge5 . 09613)	; &SW-JIGUGE5-09613;
       )
      ((===shuowen-jiguge4 . 09513)	; &SW-JIGUGE4-09513;
@@ -86,6 +96,8 @@
      ((=ucs		  . #x76B8)	; 皸
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 10114)	; &SW-JIGUGE30-10114;
+      )
      ((===shuowen-jiguge5 . 09614)	; &SW-JIGUGE5-09614;
       )
      ((===shuowen-jiguge4 . 09514)	; &SW-JIGUGE4-09514;
@@ -98,6 +110,8 @@
      ((=ucs		  . #x76B4)	; 皴
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 10115)	; &SW-JIGUGE30-10115;
+      )
      ((===shuowen-jiguge5 . 09615)	; &SW-JIGUGE5-09615;
       )
      ((===shuowen-jiguge4 . 09515)	; &SW-JIGUGE4-09515;
