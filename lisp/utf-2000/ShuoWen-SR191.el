1686e46f913632b6dde16d1bae82cf760ca678d3
MORIOKA Tomohiko <tomo.git@chise.org>
2022-10-23 23:06:49 +0900

    Add `===shuowen-jiguge-A30' characters.

--- lisp/utf-2000/ShuoWen-SR191.el
+++ lisp/utf-2000/ShuoWen-SR191.el
@@ -6,6 +6,8 @@
      ((=ucs		  . #x4EAF)	; 亯
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 18100)	; &SW-JIGUGE30-18100;
+      )
      ((===shuowen-jiguge5 . 17600)	; &SW-JIGUGE5-17600;
       )
      ((===shuowen-jiguge4 . 17300)	; &SW-JIGUGE4-17300;
@@ -20,6 +22,8 @@
      ((=ucs		  . #x20156)	; 𠅖
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 18101)	; &SW-JIGUGE30-18101;
+      )
      ((===shuowen-jiguge5 . 17601)	; &SW-JIGUGE5-17601;
       )
      ((===shuowen-jiguge4 . 17301)	; &SW-JIGUGE4-17301;
@@ -36,6 +40,8 @@
      ((=ucs		  . #x4EA8)	; 亨
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 18102)	; &SW-JIGUGE30-18102;
+      )
      ((===shuowen-jiguge5 . 17602)	; &SW-JIGUGE5-17602;
       )
      ((===shuowen-jiguge4 . 17302)	; &SW-JIGUGE4-17302;
@@ -48,6 +54,8 @@
      ((=ucs		  . #x20186)	; 𠆆
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 18103)	; &SW-JIGUGE30-18103;
+      )
      ((===shuowen-jiguge5 . 17603)	; &SW-JIGUGE5-17603;
       )
      ((===shuowen-jiguge4 . 17303)	; &SW-JIGUGE4-17303;
@@ -60,6 +68,8 @@
      ((=ucs		  . #x421E)	; 䈞
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 18104)	; &SW-JIGUGE30-18104;
+      )
      ((===shuowen-jiguge5 . 17604)	; &SW-JIGUGE5-17604;
       )
      ((===shuowen-jiguge4 . 17304)	; &SW-JIGUGE4-17304;
@@ -72,6 +82,8 @@
      ((=ucs		  . #x26918)	; 𦤘
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 18105)	; &SW-JIGUGE30-18105;
+      )
      ((===shuowen-jiguge5 . 17605)	; &SW-JIGUGE5-17605;
       )
      ((===shuowen-jiguge4 . 17305)	; &SW-JIGUGE4-17305;
