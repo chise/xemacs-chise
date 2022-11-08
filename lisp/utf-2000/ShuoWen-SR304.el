11b2867f5f54645abec3a688ad4394e3a46c3b65
MORIOKA Tomohiko <tomo.git@chise.org>
2022-11-08 10:21:20 +0900

    Add `===shuowen-jiguge-A30' characters.

--- lisp/utf-2000/ShuoWen-SR304.el
+++ lisp/utf-2000/ShuoWen-SR304.el
@@ -6,6 +6,8 @@
      ((=ucs		  . #x6BF3)	; 毳
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 28912)	; &SW-JIGUGE30-28912;
+      )
      ((===shuowen-jiguge5 . 28412)	; &SW-JIGUGE5-28412;
       )
      ((===shuowen-jiguge4 . 27912)	; &SW-JIGUGE4-27912;
@@ -18,6 +20,8 @@
      ((=ucs		  . #x4A41)	; 䩁
       ))
     (->subsumptive
+     ((===shuowen-jiguge-A30 . 28913)	; &SW-JIGUGE30-28913;
+      )
      ((===shuowen-jiguge5 . 28413)	; &SW-JIGUGE5-28413;
       )
      ((===shuowen-jiguge4 . 27913)	; &SW-JIGUGE4-27913;
