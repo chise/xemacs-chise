9bf706a1839ab18148b6aa85031fce9501d49b93
MORIOKA Tomohiko <tomo.git@chise.org>
2023-08-05 15:47:32 +0900

    (default-coded-charset-priority-list): Add `=>ucs-sip-itaiji-009'.

--- lisp/mule/mule-charset.el
+++ lisp/mule/mule-charset.el
@@ -664,6 +664,7 @@ DESCRIPTION (string) is the description string of the charset."
 	  =>ucs-bmp-itaiji-008
 	  =>ucs-sip-itaiji-008
 	  =>ucs-bmp-itaiji-009
+	  =>ucs-sip-itaiji-009
 	  =>ucs-bmp-itaiji-010
 	  =>ucs-bmp-itaiji-011
 	  =>ucs-bmp-itaiji-012
