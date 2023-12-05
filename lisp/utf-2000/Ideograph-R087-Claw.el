3f7e294a5fb0fbac0f0fa2ec4714f9e0b6977bbf
MORIOKA Tomohiko <tomo.git@chise.org>
2023-12-05 11:13:00 +0900

    (U-0002DDAF): New character.

--- lisp/utf-2000/Ideograph-R087-Claw.el
+++ lisp/utf-2000/Ideograph-R087-Claw.el
@@ -3049,6 +3049,44 @@
       )
      ((=ucs		  . #x722B)	; 爫
       )
+     ((=ucs		  . #x221B6)	; 𢆶
+      ))
+    (=ucs		. #x2DDAF)	; 𭶯
+    ))
+(define-char
+  '((<-denotational
+     ((=ucs		  . #x2DDAF)	; 𭶯
+      ))
+    (ideographic-radical . 87)	; ⽖
+    (ideographic-strokes . 6)
+    (total-strokes	 . 10)
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW")
+      (=ucs		  . #x2FF1)	; ⿱
+      )
+     ((=ucs@cns		  . #x722B)	; &GT-25225;
+      (=ucs@jis		  . #x722B)	; &GT-25225;
+      (=jis-x0212	  . #x4A48)	; &GT-25225; [42-40]
+      (=cns11643-4	  . #x2151)	; &I-C4-2151; [01-49]
+      (=jis-x0213-2	  . #x702A)	; &I-JX2-702A; [80-10]
+      (=daikanwa	  . 19655)	; &I-M-19655;
+      )
+     ((=ucs@iso		  . #x221B6)	; &C4-2267;
+      (=cns11643-4	  . #x2267)	; &C4-2267; [02-71]
+      (=daikanwa	  . 09196)	; &I-M-09196;
+      ))
+    (=ucs@JP/hanazono	. #x2DDAF)	; &hanaJU+2DDAF;
+    ))
+(define-char
+  '((ideographic-radical . 87)	; ⽖
+    (ideographic-strokes . 6)
+    (total-strokes	 . 10)
+    (ideographic-structure
+     ((name . "IDEOGRAPHIC DESCRIPTION CHARACTER ABOVE TO BELOW")
+      (=ucs		  . #x2FF1)	; ⿱
+      )
+     ((=ucs		  . #x722B)	; 爫
+      )
      ((=>ucs@iwds-1	  . #x7CF8)	; &A-IWDSU+7CF8;
       (=>iwds-1		  .  0046)	; &A-IWDSU+7CF8;
       ))
